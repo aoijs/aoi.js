663d6d49201a708ca50b48d9b51e54c071d85a0c
MustafaAmsy <64412145+MustafaAmsy@users.noreply.github.com>
2021-04-30 13:02:02 +0300

    Update killClient.js

--- package/functions/funcs/killClient.js
+++ package/functions/funcs/killClient.js
@@ -1,7 +1,7 @@
 module.exports = async (d) => {
     return {
       code: d.command.code.replaceLast(
-        `$killClient`, client.destroy())
+        `$killClient`, d.client.destroy())
       ),
     };
   };
