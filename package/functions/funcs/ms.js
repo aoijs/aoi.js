944456720eb4e660057e84f33a538f1e2262ce5f
Rakejs <77444367+Rakejs@users.noreply.github.com>
2021-04-30 16:58:59 +0200

    Create ms.js

---
+++ package/functions/funcs/ms.js
@@ -0,0 +1,18 @@
+const ms = require('ms');
+module.exports = async (d) => {
+  const code = d.command.code;
+  const inside = d.unpack();
+  const err = d.inside(inside);
+  if (err) return d.error(err);
+  const time = inside;
+  try{
+  time = ms(inside);
+  }
+  catch{
+  return d.error(':x: Wrong ms/duration provided')
+  }
+
+  return {
+    code: code.replaceLast(`$ms${inside}`,time),
+  };
+};
