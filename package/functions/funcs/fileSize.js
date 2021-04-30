b9ac369cf6fcc7795da5eb77cf7a3e2eddf9b532
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-04-30 22:58:07 +0530

    Create fileSize.js
    cause why not

---
+++ package/functions/funcs/fileSize.js
@@ -0,0 +1,79 @@
+module.exports = async (d) => {
+
+ //ayaka was here
+
+ const fs = require('fs')
+
+  const code = d.command.code
+
+  const inside = d.unpack()
+
+	const err = d.inside(inside)
+
+	if (err) return d.error(err)
+
+ const [file , size] = inside.splits
+
+let options = ["b","kb","mb","gb"]
+
+ 
+
+ if(size=== undefined) return d.error(`No option given in $filesize[${inside.splits.join(" ")}]`)
+let result;
+try{
+
+ const stats = fs.statSync(file)
+
+   switch (size.toLowerCase()){
+
+           case "b":
+
+       result = stats.size
+
+           break;
+
+           case "kb":
+
+           result = stats.size/1000
+
+           break;
+
+           case "mb":
+
+           result = stats.size/1000000
+
+           break;
+
+           case "gb":
+
+           result = stats.size/1000000000
+
+           break;
+
+           default :
+
+          result = d.error(`❌ invalid size in $filesize[${inside}]`)
+
+           ;
+
+}
+
+}
+
+    catch(e) {
+
+        d.error(`no file like ${file} exist`)}
+
+    
+
+   
+
+  
+
+  return{
+
+    code: code.replaceLast(`$fileSize${inside}`, result)
+
+  } 
+
+}   
