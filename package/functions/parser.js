2cd05f16ac9c81b412ce2fff0e083fcb96fadfe5
XzFirzal <67226790+XzFirzal@users.noreply.github.com>
2021-04-30 17:23:24 +0700

    Update parser.js

--- package/functions/parser.js
+++ package/functions/parser.js
@@ -814,9 +814,9 @@ $spliceTextJoin[$userRoles;,  ; | ;\n;3]
   $leaveVC: "Make the bot leave from voice channel, if any.;$leaveVC",
   //New Update DBD.JS v3.0.7 <3
   $dbPing: "Returns the Database Ping;$dbPing",
-  $killClient: "Stops the sercer process completely.;$killClient",
+  $killClient: "Destroy the client gateway connection.;$killClient",
  $clientToken: "Return the client token;$clientToken",
-  $uri: "Decodes or Encodes a url Example when you encode a url 'hello world' = 'hello%20world' ;$uri[decode/encode;text]",
+  $uri: "Decodes or Encodes a url Example when you encode a url 'hello world' = 'hello%20world';$uri[decode/encode;text]",
   $lerefImages: "Return a random images of Leref;$lerefImages"
  
 };
