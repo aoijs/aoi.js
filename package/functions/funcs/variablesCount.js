33822e8f345fa5c3652d6d1b37a47b953466030a
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-01 17:33:21 +0530

    Update variablesCount.js
    Added an field 
    vars => keys in client.variables
    data => keys in db

--- package/functions/funcs/variablesCount.js
+++ package/functions/funcs/variablesCount.js
@@ -1,7 +1,47 @@
 module.exports = async (d) => {
+
+const inside = d.unpack()
+
+	const err = d.inside(inside)
+
+		if (err) return d.error(err)
+
+    
+
+let res;
+
+    let [type] = inside.splits
+
+if(type == "data"){
+
+res = await d.client.db.all("main") 
+
+res = res.length
+
+}
+
+else if(type =="vars"){
+
+res = Object.keys(d.client.variables).length
+
+}
+
+else {
+
+d.error(`Invalid Type Provided In $variablesCount${inside}`)
+
+}
+
   return {
+
     code: d.command.code.replaceLast(
-      `$variablesCount`,
-      Object.keys(d.client.variables).length),
+
+      `$variablesCount${inside}`,
+
+   res)
+
   };
-};
+
+}
+
+    
