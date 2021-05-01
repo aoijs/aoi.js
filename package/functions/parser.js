3e678fcb5b03ff719f66eaf942b75bf63bf5fe3f
XzFirzal <67226790+XzFirzal@users.noreply.github.com>
2021-05-01 10:25:48 +0700

    Update parser.js

--- package/functions/parser.js
+++ package/functions/parser.js
@@ -816,5 +816,6 @@ $spliceTextJoin[$userRoles;,  ; | ;\n;3]
   $dbPing: "Returns the Database Ping;$dbPing",
   $clientToken: "Returns the client / bot token",
   $uri: "Decodes or Encodes a url Example when you encode a url 'hello world' = 'hello%20world' ;$uri[decode/encode;text]",
+  $defaultMessageNotifications: "Return the guild default message notification level (All or Mentions).;$defaultMessageNotifications or $defaultMessageNotifications[guildID]"
 };
 module.exports = Parser;
