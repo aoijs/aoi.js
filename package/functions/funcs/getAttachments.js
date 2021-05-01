d097fb458fe14ccf06008b07eb06579bb512dd01
XzFirzal <67226790+XzFirzal@users.noreply.github.com>
2021-05-01 09:33:19 +0700

    Update getAttachments.js

--- package/functions/funcs/getAttachments.js
+++ package/functions/funcs/getAttachments.js
@@ -14,23 +14,20 @@ let Options = ["name","id","size","height","width","url"]
 
      const channel = d.client.channels.cache.get(channelid)
 
-         if(!channel) return d.error(`:x: Invalid Channel ID Provided In $getAttachments[${inside.splits.join(" ")}]`)
+         if(!channel) return d.error(`:x: Invalid Channel ID Provided In $getAttachments${inside}`)
 
     if(pos <= 0) return d.error("Position Can't Be Less Than 1")
 
 const msg = await channel.messages.fetch(msgid) 
 
-if(!msg) return d.error(`:x: Invalid Message ID Provided In $getAttachments[${inside.splits.join(" ")}]`)
+if(!msg) return d.error(`:x: Invalid Message ID Provided In $getAttachments${inside}`)
 
     
 
     const option = Options.includes(opt)
 
-    if(!option) return d.error(`:x: Invalid Option Provided In $getAttachments[${inside.splits.join(" ")}]`)
-
-    
-
-   try { 
+    if(!option) return d.error(`:x: Invalid Option Provided In $getAttachments${inside}`)
+ 
 
     let attachment = msg.attachments.map(x=>x) 
 
@@ -87,15 +84,4 @@ return {
     }
 
 }
-
-catch (e){
-
-console.error(e)
-
-}
-
-    
-
-    
-
 }
