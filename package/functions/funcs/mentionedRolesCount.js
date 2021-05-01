24be154bf38c960b50c6fb7a7c52925b3471bf18
XzFirzal <67226790+XzFirzal@users.noreply.github.com>
2021-05-01 09:56:04 +0700

    Update mentionedRolesCount.js

--- package/functions/funcs/mentionedRolesCount.js
+++ package/functions/funcs/mentionedRolesCount.js
@@ -4,7 +4,7 @@ module.exports = async (d) => {
   return {
     code: d.command.code.replaceLast(
       `$mentionedRolesCount`,
-      message ? message.mentions.roles.size : undefined
+      message ? message.mentions.roles.size : 0
     ),
   };
 };
