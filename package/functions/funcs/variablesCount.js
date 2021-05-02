18d6865de994da7dbdd2e7c8e996d6ceb2e4e3b1
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-02 23:34:44 +0530

    Update variablesCount.js
    minimized the code as stated by @kino

--- package/functions/funcs/variablesCount.js
+++ package/functions/funcs/variablesCount.js
@@ -1,47 +1,13 @@
 module.exports = async (d) => {
-
 const inside = d.unpack()
-
 	const err = d.inside(inside)
-
 		if (err) return d.error(err)
-
-    
-
-let res;
-
     let [type] = inside.splits
-
-if(type == "data"){
-
-res = await d.client.db.all("main") 
-
-res = res.length
-
-}
-
-else if(type =="vars"){
-
-res = Object.keys(d.client.variables).length
-
-}
-
-else {
-
-d.error(`Invalid Type Provided In $variablesCount${inside}`)
-
-}
-
-  return {
-
+let data = await d.client.db.all("main")
+     if(!["vars","data"].includes(type)) return d.error(`:x: Invalid Type Provided In $variablesCount${inside}`)
+   return {
     code: d.command.code.replaceLast(
-
-      `$variablesCount${inside}`,
-
-   res)
-
+      `$variablesCount${inside}`, type == "data" ?data.length : Object.keys(d.client.variables).length 
+   )
   };
-
 }
-
-    
