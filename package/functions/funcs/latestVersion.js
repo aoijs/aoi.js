56d1ece9bf65a3d7408c25dd8a5ce26222796568
LightnerDev <79603731+LightnerDev@users.noreply.github.com>
2021-05-02 10:22:59 -0500

    Update latestVersion.js
    incase api is down

--- package/functions/funcs/latestVersion.js
+++ package/functions/funcs/latestVersion.js
@@ -1,12 +1,15 @@
-const axios = require('axios')
-
 module.exports = async d => {
-  axios.get('https://api.leref.ga/package/version')
+  /*axios.get('https://api.leref.ga/package/version')
     .then(function (response) {
       let version = response.data.version
       
       return {
         code: d.command.code.replaceLast(`$latestVersion`, version)
       }
-    })
+    })*/
+  let version = "1.0.1"
+
+  return {
+    code: d.command.code.replaceLast(`$latestVersion`, version)
+  }
 }
