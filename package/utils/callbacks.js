2d7749f24129a2cdfbe2a37faf4063afc91e943f
Leref <Juansantana75@icloud.com>
2021-05-08 09:19:47 -0400

    Removed callbacks not needed.

--- package/utils/callbacks.js
+++ package/utils/callbacks.js
@@ -23,8 +23,6 @@ module.exports = [
   "onMessageUpdate()",
   "onPresenceUpdate()", 
   "onUserUpdate()", 
-  "onMessageDelete()", 
-  "onMusicStart()", 
-  "onMusicEnd()",
+  "onMessageDelete()",
   "onInteractionCreate()"
 ]
