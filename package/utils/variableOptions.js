de2038558243562dd7721772c6c75d9c67a3bea6
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-17 04:07:34 +0530

    Create variableOptions.js

---
+++ package/utils/variableOptions.js
@@ -0,0 +1,9 @@
+module.exports = {
+variable:"returns the main variable",
+key:"returns the key used in db",
+value:"returns the value of Variable",
+for:"returns the id of the thing for which data was stored",
+guild:"guild id for id returned by for",
+type:"type of Variable",
+timestamp:"timestamp of changes in the variable"
+}
