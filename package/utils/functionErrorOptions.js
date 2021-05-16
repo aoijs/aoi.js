0cba3f7e7a2e949c1145e5854b013f94e4866c45
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-17 03:23:00 +0530

    Create FunctionErrorOptions.js

---
+++ package/utils/functionErrorOptions.js
@@ -0,0 +1,5 @@
+module.exports = {
+command: "command that threw the error;.command",
+function: "function that threw the error;.function",
+error: "Error message that was thrown by the function;.error"
+}
