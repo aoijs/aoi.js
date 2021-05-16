8787820a252af785b6ae26e50f773d3f58c3e82f
MasterGuy <73334601+masterguy35@users.noreply.github.com>
2021-05-16 23:19:24 +0530

    Create firstMessageID.js
    $firstMessageID created by Dynamic

---
+++ package/functions/funcs/firstMessageID.js
@@ -0,0 +1,50 @@
+module.exports = async (d) => {
+
+    const code = d.command.code
+
+    const inside = d.unpack()
+
+    if (inside.inside) {
+
+        const channelID = await d.client.channels.cache.get(inside.inside)
+
+        if (!channelID) return d.error(`:x: Invalid channel ID in \`$firstMessageID${inside}\``)
+
+        const fetchMessages = await channelID.messages.fetch({
+
+            after: 1,
+
+            limit: 1
+
+        });
+
+        const fmsg = fetchMessages.first();
+
+        return {
+
+            code: code.replaceLast(`$firstMessageID${inside}`, fmsg.id.removeBrackets())
+
+        }
+
+    } else if(!inside.inside) {
+
+        const fetchMessages = await d.message.channel.messages.fetch({
+
+            after: 1,
+
+            limit: 1
+
+        });
+
+        const fmsg = fetchMessages.first();
+
+        return {
+
+            code: code.replaceLast(`$firstMessageID`, fmsg.id.removeBrackets())
+
+        }
+
+    }
+
+}
+
