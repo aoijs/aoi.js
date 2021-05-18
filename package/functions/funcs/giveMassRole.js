3c8e57b9da267a72c48a2331b5053c750333a3a5
Mr. Catty <73694914+Mr-Catty@users.noreply.github.com>
2021-05-18 17:31:09 +0530

    Create giveMassRole.js

---
+++ package/functions/funcs/giveMassRole.js
@@ -0,0 +1,31 @@
+module.exports = async (d, message) => {
+    const code = d.command.code;
+
+    const r = code.split("$giveMassRole").length - 1;
+
+    const inside = d.unpack();
+
+    const err = d.inside(inside);
+
+    if (err) return d.error(err);
+
+    let option = inside.inside;
+if (!option) return d.error(`:x: Missing role id in \`$giveMassRole${inside}\``);
+    let gmembers = d.message.guild.members.cache.array()
+
+    let role = d.message.guild.roles.cache.get(option)
+
+    if (!role) return d.error(`:x: Invalid role id in \`$giveMassRole${inside}\``)
+
+
+    for (const member of gmembers) {
+      await new Promise(resolve => setTimeout(resolve, 1500))
+
+      member.roles.add(role.id).catch(err => {})
+    }
+
+
+    return {
+      code: d.command.code.replaceLast(`$giveMassRole${inside}`,""),
+    };
+  };
