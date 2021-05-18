f590b6b2a8f4aacf493aae5cadf8d21b105a478c
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-18 13:26:46 +0530

    Update vcSize.js
    final fix

--- package/functions/funcs/vcSize.js
+++ package/functions/funcs/vcSize.js
@@ -7,9 +7,9 @@ module.exports = async d =>{
     let res =0
     if(!["channels","users","songs"].includes(type)) return d.error(`:x: Invalid Type Provided In $vcSize${inside}`) 
     let c = d.client.voice.connections
-
-        if(c.size ==0) return res = 0
-    
+        if(c.size ==0) return {
+      code:  code.replaceLast(`$vcSize${inside}`,0)
+        }
     if(guildID == "all"){
      let i ;
         switch(type){
@@ -22,59 +22,34 @@ module.exports = async d =>{
           }
          break; 
      case "songs" :
-            
-              
       for(i=0;c.size >i;i++){
           res +=d.client.servers.get(c.array()[i].channel.guild.id).songs.length
-        
           }
                 break;
           }
            }
-        
     else{
 if(c.size == 0){res = 0}
         const guild = d.client.guilds.cache.get(guildID) 
-
         if(!guild) return d.error(`:x: Invalid guildID Provided in $vcSize${inside}`) 
-
-        
-
       let oo =  c.filter(x=>x.channel.guild.id == guild.id) 
-
     if(oo.size == 0) { res = 0 }
 else{
     switch (type) {
-
        case "channels" :
-
             res = oo.size 
-
             break;
-
        case "users" : 
-
-           
-
-                
-
+//Ayaka#5057 #aoi.js #dbd.js
             res = oo.channel.members.size 
-
-            
-
             break;
-
        case "songs" :
-
             res = d.client.servers.get(guild.id).songs.length 
-
             break ; 
-
             }
 }
     }
     return {
       code:  code.replaceLast(`$vcSize${inside}`,res || 0)
         }
-        }
-    
+        } 
