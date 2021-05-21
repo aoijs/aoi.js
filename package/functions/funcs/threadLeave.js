8def2ebcb96936a47fe46619ba342509938b9e6b
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-21 17:38:37 +0530

    Update threadLeave.js
    Beta => threads System
    function: $threadLeave
    Usage : $threadLeave or $threadLeave[channelid] 
    description: make the bot Leaves  the thread of the current/provided channel

--- package/functions/funcs/threadLeave.js
+++ package/functions/funcs/threadLeave.js
@@ -4,7 +4,7 @@ const code = d.command.code
 const inside = d.unpack()
 let [channelID = d.channel.id] = inside.splits ;
   let chan = d.client.channels.cache.get(channelID)   
-if(!chan) return d.error(`Invalid ChannelID Provided in ${inside.splits ? `$threadJoin${inside}` : `$threadJoin`}.`);
+if(!chan) return d.error(`Invalid ChannelID Provided in ${inside.splits ? `$threadLeave${inside}` : `$threadLeave`}.`);
     try{
 let req = await axios.delete(d.client._api(`channels/${chan.id}/thread-members/@me`),{
 headers:{
@@ -12,7 +12,7 @@ Authorization:`Bot ${d.client.token}`
 }
 })
 return {
-code:code.replaceLast(inside.splits ? `$threadJoin${inside}` : "$threadJoin", "")
+code:code.replaceLast(inside.splits ? `$threadLeave${inside}` : "$threadLeave", "")
 }
   }
     catch(err){
