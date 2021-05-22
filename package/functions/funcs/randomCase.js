b64a87e78126f26c6024b72380d39ca82a0fedde
MasterGuy <73334601+masterguy35@users.noreply.github.com>
2021-05-22 13:53:48 +0530

    Create randomCase.js
    randomCase.js created by Dynamic

---
+++ package/functions/funcs/randomCase.js
@@ -0,0 +1,30 @@
+module.exports = async (d) => {
+     const code = d.command.code;
+     const inside = d.unpack();
+     const err = d.inside(inside);
+     if (err) return d.error(err);
+
+     let option = inside.inside;
+     if (!option) return d.error(`:x: Missing text in \`$randomCase${inside}\``);
+     const regex = !/[^a-zA-Z0-9 ]+/g.test(option);
+     if (!regex) return d.error(`:x: Invalid text in \`$randomCase${inside}\``);
+     let Cases = [
+  String.prototype.toUpperCase,
+  String.prototype.toLowerCase
+]
+     let str = option.toString();
+     let l = str.length;
+     let result = '';
+     let a;
+     let b;
+     let c;
+
+  for (b = 0; b < l; b++) {
+    c = Number(Math.random() <= 0.5);
+    a = Cases[c];
+    result += a.call(str[b])
+  }
+  return {
+    code: d.command.code.replaceLast(`$randomCase${inside}`, result),
+  }
+}
