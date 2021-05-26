68ba43e21d8d852fdd9c03d89ddf478aece6a0cc
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-26 20:15:42 +0530

    Update deleteGlobalUserVar.js
    added event

--- package/functions/funcs/deleteGlobalUserVar.js
+++ package/functions/funcs/deleteGlobalUserVar.js
@@ -14,9 +14,11 @@ module.exports = async (d) => {
     return d.error(
       `:x: userID field not provided in \`$deleteGlobalUserVar${inside}\``
     );
+let old = await d.client.db.get("main",`${variable}_${userID}`) 
 
+old = old ? old : {key: variable,value:d.client.variables[variable]}
   await d.client.db.delete("main", `${variable}_${userID}`);
-
+d.client.emit("VARIABLE_DELETE",d.client,d.client.db,variable,old.key,old.value,"globalUser",Date.now())
   return {
     code: code.replaceLast(`$deleteGlobalUserVar${inside}`, ""),
   };
