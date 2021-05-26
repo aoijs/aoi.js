0a4c93330c40749ef492fd6c09f91c87eee9aea1
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-05-26 20:16:36 +0530

    Update deleteMessageVar.js
    added event

--- package/functions/funcs/deleteMessageVar.js
+++ package/functions/funcs/deleteMessageVar.js
@@ -14,9 +14,11 @@ module.exports = async (d) => {
     return d.error(
       `:x: messageID field not provided in \`$deleteMessageVr${inside}\``
     );
+let old = await d.client.db.get("main",`${variable}_${messageID}`) 
 
+old = old ? old : {key: variable,value:d.client.variables[variable]}
   await d.client.db.delete("main", `${variable}_${messageID}`);
-
+d.client.emit("VARIABLE_DELETE",d.client,d.client.db,variable,old.key,old.value,"message",Date.now())
   return {
     code: code.replaceLast(`$deleteMessageVar${inside}`, ""),
   };
