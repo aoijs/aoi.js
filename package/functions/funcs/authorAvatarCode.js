c8cf865f80fae1191166e9fca420efabd4c720c3
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-05-28 09:59:35 +0300

    Delete authorAvatarCode.js

--- package/functions/funcs/authorAvatarCode.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (d) => {
-  return {
-    code: d.command.code.replaceLast(
-      `$authorAvatarCode`,
-      d.message.author.avatar
-      })
-    ),
-  };
-}; //@falsisdev uWu
