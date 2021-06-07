798a3b66f14bc9a4b0957673675cfea338bb0982
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-06-08 02:51:45 +0530

    Add files via upload
    apiMessage and buttonCollector

---
+++ package/functions/funcs/testApi.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const ErrorParser = require('../../handlers/errorParser.js')
+const ComponentParser = require('../../handlers/componentParser.js')
+module.exports = async d=>{
+    const inside = d.unpack() 
+    const code = d.command.code 
+    const err = d.inside(inside)
+    if(err) return d.error(err) 
+    const [content,embed = {},components=[],returnID="no"] = inside.splits 
+    if(!content && embed === {} && embed === "") return d.error("Provide Either A Content Or A Embed Structure") 
+   const e = embed === {} ? embed : await ErrorParser(embed) 
+    //console.log(e)
+   const c = components === [] ? [] : await ComponentParser(components) 
+ //  console.log(JSON.stringify(c,null,2))
+ let msg =  await axios.post(d.client._api(`channels/${d.channel.id}/messages`),{
+     content: content ,
+     embed: e,
+     components: c
+         
+  //   message_reference: msgReply 
+     },{
+     headers:{
+         Authorization:`Bot ${d.client.token}`
+         }
+     }).catch(e=>console.log(e.message))
+// console.log(msg)
+ return {
+     code: code.replaceLast(`$testApi${inside}`,returnID === "no" ? "" : msg.data.id )
+ }
+}
