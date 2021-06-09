3e7021461e576044433b9a98d194e050fff13246
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-06-10 03:41:50 +0530

    Update apiMessage.js
    Updated $apiMessage
    Usage:
    $apiMessage[content,embed (optional), components (optional), reference _message_id:mention the replied user(default false); Return id (optional default no)]

--- package/functions/funcs/apiMessage.js
+++ package/functions/funcs/apiMessage.js
@@ -6,24 +6,31 @@ module.exports = async d=>{
     const code = d.command.code 
     const err = d.inside(inside)
     if(err) return d.error(err) 
-    const [content,embed = {},components=[],returnID="no"] = inside.splits 
-    if(!content && embed === {} && embed === "") return d.error("Provide Either A Content Or A Embed Structure") 
-   const e = embed === {} ? embed : await ErrorParser(embed) 
-   
+    const [content,embed = "",components=[],msgReply = "" ,returnID="no"] = inside.splits 
+    if(!content  && embed === "") return d.error("Provide Either A Content Or A Embed Structure") 
+   const e =embed !== "" ? await ErrorParser(embed) : {}
+    //console.log(e)
    const c = components === [] ? [] : await ComponentParser(components) 
+ //  console.log(JSON.stringify(c,null,2))
 
- let msg =  await axios.post(d.client._api(`channels/${d.channel.id}/messages`),{
-     content: content ,
+const data = {  content: content ,
      embed: e,
-     components: c
-         
-  //   message_reference: msgReply
-     },{
+     components: c,
+     allowed_mentions : {parse:d.disabledMentions} 
+}
+if(msgReply !== ""){
+       let [Reply,mention = false] = msgReply.split(":")
+
+  data.message_reference= {message_id : Reply} 
+    data.allowed_mentions.replied_user = mention 
+}
+ let msg =  await axios.post(d.client._api(`channels/${d.channel.id}/messages`),
+  data,{
      headers:{
          Authorization:`Bot ${d.client.token}`
          }
-     }).catch(e=>console.log(e.message))
-// console.log(msg)
+     }).catch(e=>d.error(e.message))
+
  return {
      code: code.replaceLast(`$apiMessage${inside}`,returnID === "no" ? "" : msg.data.id )
  }
