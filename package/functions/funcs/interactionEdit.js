31d68bbe9ead4bdef3d732654ee600636a94374e
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-06-10 03:48:42 +0530

    Create interactionEdit.js
    InteractionEdit
    
    edits and interaction Response 
    
    $interactionEdit[content;embed; components]

---
+++ package/functions/funcs/interactionEdit.js
@@ -0,0 +1,33 @@
+const ComponentParser = require('../../handlers/componentParser.js')
+const embed = require("../../handlers/errors")
+module.exports = async d => {
+    const code = d.command.code 
+    
+    const inside = d.unpack()
+	const err = d.inside(inside)
+
+	if (err) return d.error(err)
+    
+    const fields = inside.splits
+    let fie = fields
+    let [c,e, components=""] = fields
+ 
+    const m = await embed(d, fie.shift(), true) 
+    
+    const embeds = m.embed ? [m.embed.toJSON()] : [] 
+    
+    for (const field of fie) {
+        const m = await embed(d, field, true) 
+        
+        if (m.embed) embeds.push(m.embed.toJSON())
+    }
+    
+    if (d.data.interaction) {
+        const msg = await d.data.interaction.edit(m.message, embeds, components === ""? [] : ComponentParser(components)) 
+        //if (! msg) return d.error(`❌ Failed to reply to slash command!`
+    }
+    
+    return {
+        code: code.replaceLast(`$interactionEdit${inside}`, "")
+    }
+}
