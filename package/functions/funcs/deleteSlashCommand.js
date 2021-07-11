f75aba7d9d7175a993eb0141215cb2ae3bad4978
Rubenennj <Rubenmudarrajurado@hotmail.com>
2021-07-11 03:17:26 +0200

    Changed slash command creation and deletion to use client.api

--- package/functions/funcs/deleteSlashCommand.js
+++ package/functions/funcs/deleteSlashCommand.js
@@ -1,98 +1,77 @@
-const {SnowflakeUtil} = require('discord.js')
+const {
+  SnowflakeUtil
+} = require('discord.js')
 const axios = require("axios");
 const parser = require("../../handlers/slashCommandOptionsParser");
 module.exports = async (d) => {
   const code = d.command.code;
-
-  const r = code.split(`$deleteSlashCommand`).length - 1;
-
+  
+  const r = code.split(`$deleteSlashCommand`)
+      .length - 1;
+  
   const inside = code.split(`$deleteSlashCommand`)[r].after();
-
+  
   const err = d.inside(inside);
-
+  
   if (err) return d.error(err);
-
+  
   const [guildID, option] = inside.splits;
-let command;
+  let command;
   let commands;
-    if(guildID == "global"){
-        if(d.client.aoi.options.applicationCache){
-        commands = d.client.applications.slash.filter(x=>(x.name.toLowerCase() == option.toLowerCase() && x.guild == null) || (x.id == option && x.guild == null)).array()
-            }
-      else{
-        commands = await axios.get(d.client._api(`applications/${d.client.user.id}/commands`),{
-            headers:{
-              Authorization:`Bot ${d.client.token}`
-}
-            })
+  if (guildID == "global") {
+      if (d.client.aoi.options.applicationCache) {
+          commands = d.client.applications.slash.filter(x => (x.name.toLowerCase() == option.toLowerCase() && x.guild == null) || (x.id == option && x.guild == null))
+              .array()
+      } else {
+          commands = await d.client.api.applications(d.client.user.id).commands.get().catch(() => null)
+          
           if (!commands) return d.error(`:x: Failed to fetch guild commands`);
-
-  else commands = commands.data;
-           }
-       
-    }
-    else{
-        d.client.applications.slash.filter(x=>(x.name.toLowerCase() == option.toLowerCase() && x.guild ? x.guild.id == guildID: "")|| (x.id == option && x.guild ? x.guild.id== guildID : "")).array()
-        if(!commands){
-    commands = await axios
-    .get(
-      d.client._api(`/applications/${d.client.user.id}/guilds/${guildID}/commands`),
-      {
-        headers: {
-          Authorization: `Bot ${d.client.token}`,
-        },
       }
-    )
-    .catch((err) => null);
-}
-  if (!commands) return d.error(`:x: Failed to fetch guild commands`);
-  else commands = commands.data;
-}
+      
+  } else {
+      d.client.applications.slash.filter(x => (x.name.toLowerCase() == option.toLowerCase() && x.guild ? x.guild.id == guildID : "") || (x.id == option && x.guild ? x.guild.id == guildID : ""))
+          .array()
+      if (!commands) {
+        commands = await d.client.api.applications(d.client.user.id).guilds(guildID).commands.get().catch(() => null)
+      }
+      if (!commands) return d.error(`:x: Failed to fetch guild commands`);
+  }
   command = commands.find(
-    (c) => c.name.toLowerCase() === option.toLowerCase() || c.id === option
+      (c) => c.name.toLowerCase() === option.toLowerCase() || c.id === option
   );
-
+  
   if (!command)
-    return d.error(`❌ Could not find any command with name/id ${option}`);
-   command = {
-
-       id: command.id,
-
-       name: command.name,
-
-       description:command.description,
-
-       options: command.options || [],
-
-       defaultPermission : command.default_permission ,
-
-       guild : d.client.guilds.cache.get(command.guild_id) || null,
-
-       application : d.client ,
-
-       timestamp : SnowflakeUtil.deconstruct(command.id).timestamp, 
-
-       createdAt: SnowflakeUtil.deconstruct(command.id).date
-
-       } 
-d.client.emit("applicationCommandDelete",d.client,command)
-  const request = axios
-    .delete(
-      d.client._api(
-        `/applications/${d.client.user.id}/guilds/${guildID}/commands/${command.id}`
-      ),
-      {
-        headers: {
-          Authorization: `Bot ${d.client.token}`,
-        },
-      }
-    )
-    .catch((err) => null);
-
+      return d.error(`❌ Could not find any command with name/id ${option}`);
+  command = {
+      
+      id: command.id,
+      
+      name: command.name,
+      
+      description: command.description,
+      
+      options: command.options || [],
+      
+      defaultPermission: command.default_permission,
+      
+      guild: d.client.guilds.cache.get(command.guild_id) || null,
+      
+      application: d.client,
+      
+      timestamp: SnowflakeUtil.deconstruct(command.id)
+          .timestamp,
+      
+      createdAt: SnowflakeUtil.deconstruct(command.id)
+          .date
+      
+  }
+  d.client.emit("applicationCommandDelete", d.client, command)
+  const request = await d.client.api.application(d.client.user.id).guilds(guildID).commands(command.id).delete().catch(() => null) 
+  
   if (!request)
-    return d.error(`❌ Failed to delete slash command ${command.name}`);
-
+      return d.error(`❌ Failed to delete slash command ${command.name}`);
+  
   return {
-    code: code.replaceLast(`$deleteSlashCommand${inside}`, ""),
-  };
+      code: code.replaceLast(`$deleteSlashCommand${inside}`, "")
+  , };
 };
--- package/handlers/errorParser.js
+++ package/handlers/errorParser.js
@@ -1,81 +1,95 @@
-const ErrorParser = (msg) =>{
- const checker = (thingy)  => msg.includes("{"+thingy+":");
+const ErrorParser = (msg) => {
+    const checker = (thingy) => msg.includes("{" + thingy + ":");
     const embed = {}
-//---------------------------------------//
-   if(checker("title")){
-  embed.title = msg.split("{title:")[1].split("}")[0] }
-//---------------------------------------//
-   if( checker("description")){
-     embed.description = msg.split("{description:")[1].split("}")[0] }
-//---------------------------------------//
-   if(checker("footer")){ 
-      const footer = msg.split("{footer:")[1].split("}")[0]
- const fields = footer.split(":") 
-     const  footerText = fields.shift()
-     const  footerURL = fields.join(":")
- embed.footer ={text:footerText,icon_url:footerURL} 
-       }
-//---------------------------------------//
-    if(checker ("author")){
-     const author =   msg.split("{author:")[1].split("}")[0] 
-    const fields = author.split(":") 
-const authorName = fields.shift() 
-const authorURL = fields.pop()
-const authorIcon = fields.join(":")
-    embed.author = {
-        name:authorName.replace(/#COLON#/g,":"),
-        icon_url:authorIcon,
-       url:authorURL.replace(/#COLON#/g,":") 
-        }
-        }
-//---------------------------------------//
- if( checker("image")){
-    embed.image = {
-     url:msg.split("{image:")[1].split("}")[0]
-     }
-     }
-//---------------------------------------//
- if( checker("url")){
-     embed.url = msg.split("{url:")[1].split("}")[0] 
-     }
-//---------------------------------------//
-if( checker("color")){
-    embed.color = Number(parseInt(msg.split("{color:")[1].split("}")[0].replace("#",""),16).toString(10))
+    //---------------------------------------//
+    if (checker("title")) {
+        embed.title = msg.split("{title:")[1].split("}")[0]
     }
-//---------------------------------------//
- if(checker("thumbnail")){
-     embed.thumbnail = {
-     url:msg.split("{thumbnail:")[1].split("}")[0]
+    //---------------------------------------//
+    if (checker("description")) {
+        embed.description = msg.split("{description:")[1].split("}")[0]
+    }
+    //---------------------------------------//
+    if (checker("footer")) {
+        const footer = msg.split("{footer:")[1].split("}")[0]
+        const fields = footer.split(":")
+        const footerText = fields.shift()
+        const footerURL = fields.join(":")
+        embed.footer = {
+            text: footerText
+            , icon_url: footerURL
+        }
+    }
+    //---------------------------------------//
+    if (checker("author")) {
+        const author = msg.split("{author:")[1].split("}")[0]
+        const fields = author.split(":")
+        const authorName = fields.shift()
+        const authorURL = fields.pop()
+        const authorIcon = fields.join(":")
+        embed.author = {
+            name: authorName.replace(/#COLON#/g, ":")
+            , icon_url: authorIcon
+            , url: authorURL.replace(/#COLON#/g, ":")
+        }
+    }
+    //---------------------------------------//
+    if (checker("image")) {
+        embed.image = {
+            url: msg.split("{image:")[1].split("}")[0]
+        }
+    }
+    //---------------------------------------//
+    if (checker("url")) {
+        embed.url = msg.split("{url:")[1].split("}")[0]
+    }
+    //---------------------------------------//
+    if (checker("color")) {
+        embed.color = Number(parseInt(msg.split("{color:")[1].split("}")[0].replace("#", ""), 16)
+            .toString(10))
+    }
+    //---------------------------------------//
+    if (checker("thumbnail")) {
+        embed.thumbnail = {
+            url: msg.split("{thumbnail:")[1].split("}")[0]
+        }
+    }
+    //---------------------------------------//
+    if (msg.includes("{timestamp")) {
+        let timestamp = msg.split("{timestamp")[1].split("}")[0].replace(":", "")
+        
+        if (timestamp === "" || timestamp === "ms") {
+            timestamp = Date.now()
+        }
+        embed.timestamp = new Date(Number(timestamp))
+    }
+    //---------------------------------------//
+    if (checker("field")) {
+        const fields = msg.split("{field:")
+            .slice(1)
+        const res = []
+        fields.forEach(x => {
+            const y = x.split("}")[0]
+            const field = y.split(":")
+            const name = field.shift()
+            const inline = field.pop()
+            const value = field.join(":")
+            
+            
+            res.push({
+                name: name.replace(/#COLON#/g, ":")
+                , value: value.replace(/#COLON#/g, ":")
+                , incline: inline.replace("yes", true)
+                    .replace("no", false)
+            })
+        })
+        embed.fields = res
+    }
+    
+    
+    //---------------------------------------//
+    
+    
+    return embed
 }
-     }
-//---------------------------------------//
-if(msg.includes("{timestamp")){
-    let timestamp = msg.split("{timestamp")[1].split("}")[0].replace(":","")
-
-    if(timestamp==="" || timestamp === "ms"){timestamp = Date.now()} 
-    embed.timestamp = new Date(Number(timestamp))
-  }
-//---------------------------------------//
- if( checker("field")){
-     const fields = msg.split("{field:").slice(1)
- const res = [] 
- fields.forEach(x=>{
-     const y = x.split("}")[0]
-    const field = y.split(":")
-    const name = field.shift() 
-    const inline = field.pop()
-    const value = field.join(":")
-
- 
-    res.push({name:name.replace(/#COLON#/g,":"),value:value.replace(/#COLON#/g,":"),incline:inline.replace("yes",true).replace("no",false)}) 
-     })
-  embed.fields = res 
-     }
-    
-
-//---------------------------------------//
- 
-    
- return embed 
-    }
-module.exports = ErrorParser ;
+module.exports = ErrorParser;
