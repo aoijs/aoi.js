c26ee4b5e357133bebd27c81b19e90695ab5dbe0
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-07-28 23:35:58 +0530

    Update customEmoji.js
    changed var to let because var is discouraged and removed spaces

--- package/functions/funcs/customEmoji.js
+++ package/functions/funcs/customEmoji.js
@@ -1,29 +1,21 @@
 module.exports = async (d) => {
   const code = d.command.code;
-
   const inside = d.unpack();
-
   const options = [
     emoji,
     guildID="global"
   ] = inside.splits
-
-  var find;
-
+  let find;
   if (guildID !== "global") {
     let guild = d.client.guilds.cache.get(guildID)
     if (!guild) return d.error(`\`${d.func}: Invalid guild ID in ${inside}\``)
     find = guild.emojis.cache.find((e) => e.name.toLowerCase() === emoji.toLowerCase())
-
    // if (!find) return d.error(`:x: Invalid emoji name in \`$customEmoji${inside}\``)
   } else {
     find = d.client.emojis.cache.find((e) => e.name.toLowerCase() === emoji.toLowerCase())
-
     //if (!find) return d.error(`:x: Invalid emoji name in \`$customEmoji${inside}\``)
   }
-
   //if (!find) return d.error(`\`CustomEmojiError: Failed to find custom emoji\``);
-
   return {
     code: code.replaceLast(
       `$customEmoji${inside}`,
