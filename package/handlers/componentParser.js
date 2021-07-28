9ecaed8e703d96665779be4aeab3926547e2ca8a
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-07-28 20:57:20 +0530

    Update componentParser.js
    fixed disbaled

--- package/handlers/componentParser.js
+++ package/handlers/componentParser.js
@@ -16,19 +16,19 @@ c = await mustEscape(c)
         //console.log("inside:"+inside)
         let buttons = [] 
         inside.forEach(z=>{
-        let [label,type,style,cus,emoji=undefined,disabled=false] = z.split(",") 
+        let [label,type,style,cus,emoji=undefined,disabled="no"] = z.split(",") 
 //     console.log(z)
        
     //   console.log(emoji)
        if(style == 5){
-      data ={label:label.addBrackets(),type:Number(type),style:Number(style),url:cus.addBrackets(),disabled:disabled}
+      data ={label:label.addBrackets(),type:Number(type),style:Number(style),url:cus.addBrackets(),disabled:disabled.replace("yes",true).replace("no",false)}
            if(emoji){
                const [ename,eid=0,eani=false] = emoji.split("|")
                data.emoji = {name:ename.addBrackets(),id:eid.addBrackets(), animated:eani.addBrackets()}
            }
            }
            else {
-               data = {label:label.addBrackets(),type:Number(type),style:Number(style),custom_id:cus.addBrackets(), disabled:disabled}
+               data = {label:label.addBrackets(),type:Number(type),style:Number(style),custom_id:cus.addBrackets(), disabled:disabled.replace("yes",true).replace("no",false)}
                if(emoji){
                const [ename,eid=0,eani=false] = emoji.split("|")
                data.emoji = {name:ename.addBrackets(),id:eid.addBrackets(), animated:eani.addBrackets()}
