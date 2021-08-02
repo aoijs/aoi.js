58f76b126dd892bdeda76d154508f1354677167c
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-08-02 19:01:37 +0530

    Update forEachChannel.js
    fixed forEachChannel

--- package/functions/funcs/forEachChannel.js
+++ package/functions/funcs/forEachChannel.js
@@ -1,24 +1,21 @@
 module.exports = async (d) => {
   const code = d.command.code;
-
-  const r = code.split("$forEachChannel").length - 1;
-
-  const inside = code.split("$forEachChannel")[r].after();
-
+  const inside = d.unpack();
   for (const channel of d.client.channels.cache.array()) {
     for (const command of inside.splits) {
-      const m = Object.assign(Object.create(d.message), d.message);
-
-      m.channel = channel;
-
       const cmd = d.client.awaited_commands.find((e) => e.name === command);
-
       if (!cmd)
         return d.error(
           `\`${d.func}: Invalid awaited command ${command} in ${inside}\``
         );
-
-      d.interpreter(d.client, m, d.args, cmd);
+      d.interpreter(d.client,{
+     message:d.message,
+     channel: channel,
+     guild: channel.guild,
+     author:d.author,
+     member:d.member,
+     client:d.client
+      }, d.args, cmd);
     }
   }
 
