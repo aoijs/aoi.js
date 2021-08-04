f45eeb48df0a3cfc3ab7df8adc2f7501c3e9a9cc
Leref <12578483+Leref@users.noreply.github.com>
2021-08-03 22:16:54 -0400

    Update checkGlobalSlashCreate.js

--- package/handlers/checkGlobalSlashCreate.js
+++ package/handlers/checkGlobalSlashCreate.js
@@ -16,24 +16,18 @@ module.exports = async (type,name, client ) =>{
         
     await client.emit(res,command)
       },2000)
-                   
-                      }
+   }
     else {
-        
+
 setTimeout(async ()=>{
   d = await axios.get(client._api(`/applications/${client.user.id}/guilds/${type}/commands`),{
-
           headers:{
-
               Authorization:`Bot ${client.token}`
-
               }
-
           })
-
   command = d.data.find(x=>x.name == name) 
 if(!command){res = "e"}
      await client.emit(res,command)
         },2000) 
     }
-    }
+  }
