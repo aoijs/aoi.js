41ceb35ad3b9476c901cf463926bd064673dea32
Leref <12578483+Leref@users.noreply.github.com>
2021-08-03 22:17:14 -0400

    Update channelUpdateCommands.js

--- package/handlers/channelUpdateCommands.js
+++ package/handlers/channelUpdateCommands.js
@@ -7,7 +7,7 @@ module.exports = async (client, oldc, newc) => {
         const data = {
             guild: newc.guild,
             channel: newc.channel 
-    }
+                    }
     
     const id = await interpreter(client, data, [], {
       channel: command.channel,
@@ -16,8 +16,6 @@ module.exports = async (client, oldc, newc) => {
 
     const channel = client.channels.cache.get(id)
 
-    //if (!channel) return console.error(`channel with ID ${id} does not exist`)
-    
     data.channel = channel || data.channel
 
     await interpreter(client, data, [], command, undefined, undefined, undefined, {
