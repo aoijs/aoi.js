fc261670aa8b110b6427377f6786904fd6b0b1fb
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-09-04 01:19:03 +0530

    Add files via upload

---
+++ package/CacheHandler/index (1).js
@@ -0,0 +1,8 @@
+const Group = require('./group.js')
+const LimitGroup = require('./limitGroup.js')
+const SuperSet =  require('./superSet.js')
+module.exports = {
+cache:Group,
+limitedCache:LimitGroup,
+setCache:SuperSet,
+}
