d3f9144c20ad6317f386e40fc942a305fba5ed3b
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-09-05 15:57:39 +0530

    Update color.js

--- package/functions/funcs/color.js
+++ package/functions/funcs/color.js
@@ -1,3 +1,4 @@
+const {MessageEmbed} = require("discord.js") 
 const color = (d) => {
   
   const inside = d.unpack();
@@ -5,9 +6,8 @@ const color = (d) => {
   if(err) return d.error(err) 
   let [index=1,color] = inside.splits; 
     if(isNaN(Number(index-1))) d.error("$color: Invalid Index")
-    d.embed.setColor(color)
-    d.embeds[index-1].color = d.embed.color 
-
+    if(!d.embeds[index-1]) d.embeds[index-1] = new MessageEmbed ()
+   d.embeds[index-1].setColor(color) 
   return {
     code: d.command.code.replaceLast(`$color${inside}`, ""),
     embeds: d.embeds 
