3012728c711c9d500daef655efe876f72439a21b
BeastBoyADI <agarwaladitya381@gmail.com>
2021-10-20 17:03:34 +0530

    Fixed maxRam returning wrong info. The code i used returns accurate ram that the bot's container have
    You try it and i have tested it many times and it does returns correct max ram that was allowed to your bot's container/server

--- package/functions/funcs/maxRam.js
+++ package/functions/funcs/maxRam.js
@@ -1,11 +1,7 @@
-const os = require('os')
-
 module.exports = async d => {
-	let ram = os.totalmem() / 1024 / 1024
-
-	ram = ram.toFixed(2)
+	let ram = `${process.env.SERVER_MEMORY} MB`
 
 	return {
-		code: d.command.code.replaceLast(`$maxRam${d.unpack()}`, String(ram))
+		code: d.command.code.replaceLast(`$maxRam${d.unpack()}`, ram)
 	}
 }
