48ae10e831e5d9c11e8db6d4c3622168cb34a9dc
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-10-21 19:11:53 +0530

    Update addButton.js

--- package/functions/funcs/addButton.js
+++ package/functions/funcs/addButton.js
@@ -1,29 +1,26 @@
-const {MessageButton} = require('discord.js') 
-const {ButtonStyleOptions} = require('../../Utils/Constants.js')
 module.exports = async d =>{
-    const {code} = d.command 
-    const inside = d.unpack() 
-    const err = d.inside(inside) 
-    if(err) return d.error(err) 
-    let [index=1,label,style,cus,disabled=false,emoji] = inside.splits
-    index = Number(index)-1
-    label = label.addBrackets()
-const components = new MessageButton()
-    cus=cus.addBrackets()
-    disabled = disabled ? disabled.replace("yes",true).replace("true",true).replace("no",false).replace("false",false): disabled
-    style = ButtonStyleOptions[style]||Number(style)
-  //  if(!style || style <1 || style > 5 ||isNaN(style)||!["PRIMARY","SECONDARY","DANGER","SUCCESS","LINK"].includes(style)) return d.error(`${d.func}:Inavlid Style Provided In ${inside}`)
-    if(!cus) return d.error(`${d.func}:${style === 5 ? "URL" : "CustomId"} Not Provided In ${inside}`)
-    d.components[index] = d.components[index]||{type:1, components:[]}
-    components.setLabel(label)
-        .setStyle(style) 
-   if(style === 5 || style === "LINK") components.setUrl(cus)
-    else components.setCustomId(cus)
-    components.setDisabled(disabled)
-    if(emoji) components.setEmoji(emoji)
-    d.components[index].components.push(components)
+const {code} = d.command 
+const inside = d.unpack() 
+const err = d.inside(inside)
+if(err) return d.error(err) 
+let [index,label,style,cus,disabled="no",emoji]=inside.splits;
+    if(isNaN(index) || Number(index)<1) d.aoiError.fnError(d,"custom",{inside},"Invalid Index Provided In") 
+    index = Number(index) -1 
+    style = isNaN(style) ? d.util.constants.ButtonStyleOptions[style] : Number(style) 
+    disabled = disabled === "yes" 
+    if(!style || style > 5 || style < 1) d.aoiError.fnError(d,"custom",{inside},"Invalid Style Provided In") 
+    if(!d.components[index]) d.components[index] = {type:1, components:[]}
+    const button = {
+        label.addBrackets(),
+        type:2,
+        style,
+        disabled,
+        emoji 
+    }
+    button[style === 5? "url" : "customId"] = cus 
+    d.components[index].components.push(button)
     return {
-        code: code.replaceLast(`$addButton${inside}`,""),
+        code: d.util.setCode({function:d.func,inside,code,result:""}) ,
         components:d.components 
     }
 }
