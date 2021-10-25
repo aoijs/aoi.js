030c48da704c1da57ba562c3d90d9ad54145a538
Leref <12578483+Leref@users.noreply.github.com>
2021-10-25 15:48:55 -0400

    Update variableOptions.js

--- package/utils/variableOptions.js
+++ package/utils/variableOptions.js
@@ -1,8 +1,8 @@
 module.exports = {
-    variable: "returns the main variable",
-    key: "returns the key used in db",
-    value: "returns the value of Variable",
-    for: "returns the id of the thing for which data was stored",
+    variable: "Returns the main variable",
+    key: "Returns the key used in db",
+    value: "Returns the value of Variable",
+    for: "Returns the id of the thing for which data was stored",
     guild: "guild id for id returned by for",
     type: "type of Variable",
     timestamp: "timestamp of changes in the variable"
