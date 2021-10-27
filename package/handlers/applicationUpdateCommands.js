470a1e1e4166d8376b8f66fd5878ea4dc732cb1b
Leref <12578483+Leref@users.noreply.github.com>
2021-10-27 18:49:12 -0400

    Update applicationUpdateCommands.js

--- package/handlers/applicationUpdateCommands.js
+++ package/handlers/applicationUpdateCommands.js
@@ -8,35 +8,22 @@ let data = {
     oldData : oldData,
     newData : newData 
     }
-    
       //gets the  info of application that was created 
   
 //channel: part 
     const id =
-
       commands.channel && commands.channel.includes("$")
-
         ? await interpreter(
-
             client,
-
             data,
-
             [],
-
             {
               channel: commands.channel,
-
               code: commands.channel,
-
             },
-
             undefined,
-
             true
-
           )
-
         : commands.channel;
 //verify if its exist 
     const ch = client.channels.cache.get(id);
@@ -44,31 +31,19 @@ data.channel = ch
     //if (!channel) return console.error(`channel with ID ${id} does not exist`)
      let msg = data 
 
-    
 //interpreter does it's job 
     await interpreter(
-
       client,
-
       msg,
-
-     [],
-
+      [],
       commands,
-
-     undefined,
-
-      false,
-
       undefined,
-
+      false,
+      undefined,
       {
-oldData: oldData,
-newData: newData
+        oldData: oldData,
+        newData: newData
       }
-
     );
-
   }
-
 };
