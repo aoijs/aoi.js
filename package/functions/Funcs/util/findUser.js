aed12851f0e1f8e6137895b5a7236549b205ed15
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-11-02 04:02:16 +0530

    Update findUser.js

--- package/functions/Funcs/util/findUser.js
+++ package/functions/Funcs/util/findUser.js
@@ -3,7 +3,7 @@ module.exports = async d => {
     if(data.err) return d.error(data.err);
     
     let [ userResolver,returnSelf = "yes" ] = data.inside.splits;
-    userResolver = userResolver.addBrackets();
+    userResolver = userResolver.addBrackets().replace(/[\\<>@!]/g,"").trim();;
     
 
     data.result = d.client.users.cache.find( x => x.username.toLowerCase() === userResolver.toLowerCase() || x.tag.toLowerCase() === userResolver.toLowerCase() || x.id === userResolver || x.toString() === userResolver )
