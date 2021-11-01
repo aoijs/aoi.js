465d1bfbbf5078c327d4cc85872340bffbeab6f8
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-11-02 02:25:56 +0530

    Fix no 2

---
+++ package/functions/parser.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const maps = {};
+const functions = fs.readdirSync(__dirname+'/Funcs').map( x => fs.readdirSync(__dirname+`/Funcs/${x}`) ).map( x => x.join(',') ).join(',').split(',').map( x => '$'+x.split('.js')[0] );
+
+require('fs').readdirSync(__dirname+'/Funcs').map(x => maps[x] = fs.readdirSync(__dirname+'/Funcs/' + x).map(y => y.split(".js")[0]));
+
+module.exports = {functions,maps }
