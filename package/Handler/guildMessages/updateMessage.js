411c57e3279bd5b92abdc0330ae77ec86944a3f5
Leref <12578483+Leref@users.noreply.github.com>
2021-10-31 21:50:33 -0400

    some fixes of assign

--- package/Handler/guildMessages/updateMessage.js
+++ package/Handler/guildMessages/updateMessage.js
@@ -3,7 +3,7 @@ module.exports = async (oldm,newm, client)=>{
     let chan;
 for(const cmd of client.cmd.messageUpdate.allValues()){
   const id = cmd.channel.includes("$")?await Interpreter(client,newm,[],{name:"channelParser",code:cmd.channel},client.db,true):cmd.channel 
-const channel = client.channels.cache.get(id) 
+let channel = client.channels.cache.get(id)
 if(!channel) channel = newm.channel 
     chan = channel
   await Interpreter(client,newm,newm?. content?.split(" "),cmd, client.db,false, undefined,{oldm:oldm},chan) 
