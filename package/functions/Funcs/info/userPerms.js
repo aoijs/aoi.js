6566c0fc0bbc9a6e3b29e0da84a7dd53bd106fb4
Ayaka <user.satoshi69@gmail.com>
2021-11-02 23:37:54 +0530

    more fixes

--- package/functions/Funcs/info/userPerms.js
+++ package/functions/Funcs/info/userPerms.js
@@ -2,9 +2,8 @@ const { Perms } = require('../../../Utils/Constants.js');
 
 module.exports = async d => {
     const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
 
-    const [userId, sep = ' , ', guildId = d.guild?.id] = data.inside.splits;
+    const [userId = d.author?.id, sep = ' , ', guildId = d.guild?.id] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildId);
     if (!guild) return d.aoiError.fnError(d, 'guild', { inside: data.inside });
