7e5ef2b45c9ceb4e5c9650ed24c8ab8b3395566a
Ayaka <user.satoshi69@gmail.com>
2021-11-02 20:44:32 +0530

    voice fixes

--- package/functions/Funcs/info/voiceId.js
+++ package/functions/Funcs/info/voiceId.js
@@ -1,7 +1,7 @@
 module.exports = async d => {
     const data = d.util.openFunc(d);
 
-    const [userId = d.author?.id, guildId = screen.guild?.id] = data.inside.splits;
+    const [userId = d.author?.id, guildId = d.guild?.id] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildId);
     if (!guild) return d.aoiError.fnError(d, 'guild', { inside: data.inside });
