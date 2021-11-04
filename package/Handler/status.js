90b339826e1e01653c7fade458ced4a4d11a9b35
Ayaka <user.satoshi69@gmail.com>
2021-11-05 02:13:47 +0530

    fixed  Unexpected identifier error

--- package/Handler/status.js
+++ package/Handler/status.js
@@ -2,12 +2,13 @@ module.exports = (statuses, client) => {
   if (statuses.size !== 0) {
     let y = 0
 
-    let status = statuses.allValues()
+    let status = statuses.allValues();
     const f = async () => {
 
       if (!status[y]) { y = 0 }
-      setTimeout(() => {
-        status[y].activity.name = status[y].activity.name.includes('$') ? (await client.functionManager.interpreter(client, {}, [], { code: status[y].activity.name }, client.db, true))?.code : status[y].activity.name;
+      setTimeout(async () => {
+        status[y].activity.name = status[y].activity.name.includes('$') ? ((await client.functionManager.interpreter(client, {}, [], { code: status[y].activity.name }, client.db, true))?.code) : (status[y].activity.name);
+
         client.user.setPresence({
           status: status[y].status,
           activities: Array.isArray(status[y].activity) ? status[y].activity : [status[y].activity],
