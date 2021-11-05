74f7cdcd9997184f6664ea60359ed328ddc82311
Ayaka <user.satoshi69@gmail.com>
2021-11-05 22:55:53 +0530

    fixed

--- package/functions/Funcs/info/activity.js
+++ package/functions/Funcs/info/activity.js
@@ -1,10 +1,14 @@
 module.exports = async (d) => {
   const code = d.command.code;
   const inside = d.unpack()
-  const [id = d.author?.id] = inside.splits; 
-    let u = await d.util.getUser(d,id)
-    if (!u) return d.aoiError.fnError(d,"user",{inside}) 
-    return {
-      code: d.util.setCode({function:d.func,code,inside,result:u.presence?.activities?.join(", ")?.deleteBrackets() || "none"})
-}
-    };
+  const [id = d.author?.id, guildId = d.guild?.id] = inside.splits;
+
+  const guild = await d.util.getGuild(d, guildId);
+  if (!guild) return d.aoiError.fnError(d, 'guild', { inside: DataTransfer.inside });
+
+  let u = await d.util.getMember(guild, id)
+  if (!u) return d.aoiError.fnError(d, "member", { inside })
+  return {
+    code: d.util.setCode({ function: d.func, code, inside, result: u.presence?.activities?.join(", ")?.deleteBrackets() || "none" })
+  }
+};
