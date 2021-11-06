2b88e5352403ed8fd823f57136c9d6a63a2b5f43
dbthesecond <84148386+dbthesecond@users.noreply.github.com>
2021-11-07 01:07:15 +0530

    Create isSelfDeafened.js

---
+++ package/functions/Funcs/util/isSelfDeafened.js
@@ -0,0 +1,14 @@
+module.exports = async d => {
+    const data = d.util.openFunc( d );
+    if( data.err ) return d.error( data.err );
+    const [ userId = d.member?.id, guildId = d.guild?.id ] = data.inside.splits;
+    const guild = d.util.getGuild(d,guildId);
+    if(!guild) return d.aoiError.fnError( d,'guild',{ inside : data.inside });
+    const user = guild.members?.cache?.get(userId)
+    if(!user) return d.aoiError.fnError( d,'user',{ inside : data.inside });
+    data.result = user.voice.selfDeaf
+    return {
+        code: d.util.setCode(data)
+    }
+}
+//Usage `$isSelfDeafened[user's id(optional); server's id(optional)]`
