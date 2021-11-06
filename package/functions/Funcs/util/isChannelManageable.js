eee6bcb1a888111349c477d69a4944c945e242c7
dbthesecond <84148386+dbthesecond@users.noreply.github.com>
2021-11-07 01:57:13 +0530

    Added aupport for guild

--- package/functions/Funcs/util/isChannelManageable.js
+++ package/functions/Funcs/util/isChannelManageable.js
@@ -1,11 +1,14 @@
 module.exports = async d => {
   const data = d.util.openFunc(d)
   if( data.err ) return d.error( data.err );
-  const [ id ] = data.inside.splits;
-  const channel = id ? d.guild?.channels?.cache?.get(id)?.id : d.channel?.id;
+  const [ channelId = d.channel?.id, guildId = d.guild?.id ] = data.inside.splits;
+  const guild = d.util.getGuild(d,guildId);
+  if(!guild) return d.aoiError.fnError( d,'guild',{ inside : data.inside });
+  const channel = guild.channels?.cache?.get(channelId)?.id
   if(!channel) return d.aoiError.fnError(d, 'channel', { inside: data.inside });
   data.result = channel.manageable
   return {
       code : d.util.setCode( data )
    }
 }
+//Usage: $isChannelManageable[channel id(optional); guild id(optional)]
