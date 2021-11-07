6fdfcf83e55ab8a88f422db7072450f389cf4823
dbthesecond <84148386+dbthesecond@users.noreply.github.com>
2021-11-07 13:03:10 +0530

    Made channel id for global support so no need of guild id

--- package/functions/Funcs/util/isChannelManageable.js
+++ package/functions/Funcs/util/isChannelManageable.js
@@ -1,14 +1,12 @@
 module.exports = async d => {
   const data = d.util.openFunc(d)
   if( data.err ) return d.error( data.err );
-  const [ channelId = d.channel?.id, guildId = d.guild?.id ] = data.inside.splits;
-  const guild = d.util.getGuild(d,guildId);
-  if(!guild) return d.aoiError.fnError( d,'guild',{ inside : data.inside });
-  const channel = guild.channels?.cache?.get(channelId)?.id
+  const [ channelId = d.channel?.id ] = data.inside.splits;
+  const channel = d.util.getChannel(d, channelId)
   if(!channel) return d.aoiError.fnError(d, 'channel', { inside: data.inside });
   data.result = channel.manageable
   return {
       code : d.util.setCode( data )
    }
 }
-//Usage: $isChannelManageable[channel id(optional); guild id(optional)]
+//Usage: $isChannelManageable[channel id(optional)]
