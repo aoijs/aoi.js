f84a7d798f9ab243626a4fcb246c439f1dd1d71c
dbthesecond <84148386+dbthesecond@users.noreply.github.com>
2021-11-07 13:19:55 +0530

    Done the change as per ayaka

--- package/functions/Funcs/calling/setRolePosition.js
+++ package/functions/Funcs/calling/setRolePosition.js
@@ -4,7 +4,7 @@ module.exports = async d => {
     const [ roleId,newPosition,guildId = d.guild?.id ] = data.inside.splits;
     const guild = await d.util.getGuild(d,guildId);
     if(!guild) return d.aoiError.fnError( d,'guild',{ inside : data.inside });
-    const role = guild.roles?.cache?.get(roleID);
+    const role = await guild.roles.fetch(roleId);
     if(!role) return d.aoiError.fnError( d,'role',{ inside : data.inside });
 
     await role.setPosition(newPosition).catch((err) => {
