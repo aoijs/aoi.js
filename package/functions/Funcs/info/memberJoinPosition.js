6e3c390c45ab7bc76995602e3d628e6da96ca305
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2021-11-07 20:05:03 +0530

    Update memberJoinPosition.js

--- package/functions/Funcs/info/memberJoinPosition.js
+++ package/functions/Funcs/info/memberJoinPosition.js
@@ -6,7 +6,7 @@ module.exports = async d => {
     const guild = await d.util.getGuild(d, guildId);
     if (!guild) return d.aoiError.fnError(d, 'custom', { inside: data.inside });
 
-    data.result = guild.members.cache.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp).findIndex(x => x.id === userId) + 1;
+    data.result = [...guild.members.cache.sort((a, b) => a.joinedTimestamp - b.joinedTimestamp).values()].findIndex(x => x.id === userId) + 1;
 
     return {
         code: d.util.setCode(data)
