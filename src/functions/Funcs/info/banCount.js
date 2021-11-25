dbc5d5c974e61d26b8b0786b27ed16e3ef1cf366
Leref <12578483+Leref@users.noreply.github.com>
2021-11-25 08:50:07 -0500

    Fixed $banCount

--- src/functions/Funcs/info/banCount.js
+++ src/functions/Funcs/info/banCount.js
@@ -4,7 +4,7 @@ module.exports = async d => {
     const [guildId = d.guild.id, user] = inside.splits;
     const guild = await d.util.getGuild(d, guildId)
     let result;
-    result = await guild.bans.fetch({user}).catch(err => {
+    result = await guild.bans.fetch().catch(err => {
         result = 0;
         d.aoiError.fnError(d, "custom", {}, "Failed To Fetch Bans");
     })
