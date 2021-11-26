7f5e6d91c4cc84d44db08d920c0792dd191811a9
Ayaka <user.satoshi69@gmail.com>
2021-11-26 23:32:17 +0530

    minor fixes

--- src/functions/Funcs/util/get.js
+++ src/functions/Funcs/util/get.js
@@ -2,7 +2,7 @@ module.exports = async d => {
     const data = d.util.openFunc(d);
     const [name] = data.inside.splits;
 
-    data.result = d.vars[name?.addBrackets()];
+    data.result = d.data.vars[name?.addBrackets()];
 
     return {
         code: d.util.setCode(data)
