8b29d36a7ee4b2a3fbcc42212473e65ab3da53c7
Ayaka <user.satoshi69@gmail.com>
2021-11-26 23:18:55 +0530

    fix

--- src/handler/custom/loop.js
+++ src/handler/custom/loop.js
@@ -3,56 +3,56 @@ const Interpreter = require("../../interpreter.js");
  * @param  {import('../../classes/Bot.js')} client
  */
 module.exports = async (client) => {
-    const cmds = client.cmd.loop.allValues();
-    let chan;
-    let data = {
-        client: client,
-        channel: undefined,
-        guild: undefined,
-    };
-    for (const cmd of cmds) {
-        if (cmd.channel?.includes("$")) {
-            const id = await Interpreter(
-                client,
-                data,
-                [],
-                {name: "ChannelParser", code: cmd.channel},
-                client.db,
-                true,
-            );
-            chan = client.channels.cache.get(id?.code);
-            data.channel = chan;
-            data.guild = chan?.guild;
-        } else {
-            chan = client.channels.cache.get(cmd.channel);
-            data.channel = chan;
-            data.guild = chan?.guild;
-        }
-        setInterval(async () => {
-            await Interpreter(
-                client,
-                data,
-                [],
-                cmd,
-                client.db,
-                false,
-                chan?.id,
-                {},
-                chan,
-            );
-        }, cmd.every || 60000);
+	const cmds = client.cmd.loop.allValues();
+	let chan;
+	let data = {
+		client: client,
+		channel: undefined,
+		guild: undefined,
+	};
+	for (const cmd of cmds) {
+		if (cmd.channel?.includes("$")) {
+			const id = await Interpreter(
+				client,
+				data,
+				[],
+				{ name: "ChannelParser", code: cmd.channel },
+				client.db,
+				true,
+			);
+			chan = await client.channels.fetch(id?.code);
+			data.channel = chan;
+			data.guild = chan?.guild;
+		} else {
+			chan = await client.channels.fetch(cmd.channel);
+			data.channel = chan;
+			data.guild = chan?.guild;
+		}
+		setInterval(async () => {
+			await Interpreter(
+				client,
+				data,
+				[],
+				cmd,
+				client.db,
+				false,
+				chan?.id,
+				{},
+				chan,
+			);
+		}, cmd.every || 60000);
 
-        if (cmd.executeOnStartup)
-            await Interpreter(
-                client,
-                data,
-                [],
-                cmd,
-                client.db,
-                false,
-                chan?.id,
-                {},
-                chan,
-            );
-    }
+		if (cmd.executeOnStartup)
+			await Interpreter(
+				client,
+				data,
+				[],
+				cmd,
+				client.db,
+				false,
+				chan?.id,
+				{},
+				chan,
+			);
+	}
 };
