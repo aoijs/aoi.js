e70ee8e990eec925e18ed06cc4dd759d100a1bb7
Ayaka <user.satoshi69@gmail.com>
2021-11-27 23:38:40 +0530

    fixed presense in membersConur

--- src/functions/Funcs/info/membersCount.js
+++ src/functions/Funcs/info/membersCount.js
@@ -7,7 +7,7 @@ module.exports = async d => {
     if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
     if (presence === '' && countBot === 'yes') data.result = guild.memberCount;
     else {
-        const mems = guild.members.cache.filter(x => (x.presence?.status?.toLowerCase() === presence.toLowerCase()) && (countBot === 'yes' ? true : !x.user.bot))
+        const mems = guild.members.cache.filter(x => ( presence === '' || presence === 'all'  ? true : x.presence?.status?.toLowerCase() === presence.toLowerCase()) && (countBot === 'yes' ? true : !x.user.bot))
 
         data.result = mems.size;
     }
