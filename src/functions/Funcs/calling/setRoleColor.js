4e6685b998e2c1ed1035af5c9c9628c0824f203e
Ayaka <user.satoshi69@gmail.com>
2021-11-27 18:00:04 +0530

    fixed setRoleColor

--- src/functions/Funcs/calling/setRoleColor.js
+++ src/functions/Funcs/calling/setRoleColor.js
@@ -5,13 +5,13 @@ module.exports = async (d) => {
     const [roleID, color] = data.inside.splits;
 
     const role = d.guild.roles.cache.get(roleID);
-    if (!role) return d.error(`${d.func}: Invalid role ID in ${inside}`);
+    if (!role) return d.error(`${d.func}: Invalid role ID in ${data.inside}`);
 
     const re = await role.setColor(color).catch((err) => {
     });
     if (!re) return d.error(`${d.func}: Failed to change ${role.name} color to ${color}!`);
 
     return {
-        code: code.replaceLast(`${d.func}${inside}`, ""),
+        code: code.replaceLast(`${d.func}${data.inside}`, ""),
     };
 };
