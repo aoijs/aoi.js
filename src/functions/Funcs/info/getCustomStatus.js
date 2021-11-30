06f95cb234d198dc371ae36b876b58c0ae544a25
Ayaka <user.satoshi69@gmail.com>
2021-11-30 15:01:40 +0530

    added ?. after presence

--- src/functions/Funcs/info/getCustomStatus.js
+++ src/functions/Funcs/info/getCustomStatus.js
@@ -9,7 +9,7 @@ module.exports = async d => {
     const user = await d.util.getMember(d.guild, userId);
     if (!user) return d.aoiError.fnError(d, 'member', {inside: data.inside});
 
-    const status = user.presence.activities?.find(x => x.type === 'CUSTOM');
+    const status = user.presence?.activities?.find(x => x.type === 'CUSTOM');
     data.result = status?.[type];
 
     return {
