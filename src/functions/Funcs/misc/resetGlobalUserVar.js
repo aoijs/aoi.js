8e634385e40c68fde98db3767e47c4957da58949
Ayaka <user.satoshi69@gmail.com>
2021-12-03 01:23:42 +0530

    stuffs

--- src/functions/Funcs/misc/resetGlobalUserVar.js
+++ src/functions/Funcs/misc/resetGlobalUserVar.js
@@ -1,16 +1,24 @@
-module.exports = async d => {
-    const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
+module.exports = async (d) => {
+	const data = d.util.openFunc(d);
+	if (data.err) return d.error(data.err);
 
-    const [varname, table = d.client.db.tables[0]] = data.inside.splits;
+	const [varname, table = d.client.db.tables[0]] = data.inside.splits;
 
-    if (!d.client.variableManager.cache.has(varname.addBrackets())) return d.aoiError.fnError(d, 'custom', {}, `Variable ${varname.addBrackets()} Doesn't Exist!`);
+	if (!d.client.variableManager.cache.has(varname.addBrackets()))
+		return d.aoiError.fnError(
+			d,
+			"custom",
+			{},
+			`Variable ${varname.addBrackets()} Doesn't Exist!`,
+		);
 
-    const all = await d.client.db.all(table, x => x.key.split('_').length < 3 && x.key.startsWith(varname.addBrackets()) && !d.util.getGuild(d, x.key.split('_')[1]) && !d.util.getChannel(d, x.key.split('_')[1]))
+	const all = await d.client.db.all(table, varname.addBrackets(), 2, [
+		0,
+		guildId,
+	]);
+	await Promise.all(all.forEach((x) => d.client.db.delete(table, x.key)));
 
-    await Promise.all(all.forEach(x => d.client.db.delete(table, x.key)));
-
-    return {
-        code: d.util.setCode(data)
-    }
-}
+	return {
+		code: d.util.setCode(data),
+	};
+};
--- src/functions/Funcs/misc/resetUserVar.js
+++ src/functions/Funcs/misc/resetUserVar.js
@@ -1,19 +1,29 @@
-module.exports = async d => {
-    const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
+module.exports = async (d) => {
+	const data = d.util.openFunc(d);
+	if (data.err) return d.error(data.err);
 
-    const [varname, guildId = d.guild?.id, table = d.client.db.tables[0]] = data.inside.splits;
+	const [varname, guildId = d.guild?.id, table = d.client.db.tables[0]] =
+		data.inside.splits;
 
-    if (!d.client.variableManager.cache.has(varname.addBrackets())) return d.aoiError.fnError(d, 'custom', {}, `Variable ${varname.addBrackets()} Doesn't Exist!`);
+	if (!d.client.variableManager.cache.has(varname.addBrackets()))
+		return d.aoiError.fnError(
+			d,
+			"custom",
+			{},
+			`Variable ${varname.addBrackets()} Doesn't Exist!`,
+		);
 
-    const guild = d.util.getGuild(d, guildId);
-    if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
+	const guild = d.util.getGuild(d, guildId);
+	if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
 
-    const all = await d.client.db.all(table, x => x.key.split('_').length < 4 && x.key.startsWith(varname.addBrackets()) && guild.id === x.key.split('_')[2])
+	const all = await d.client.db.all(table, varname.addBrackets(), 3, [
+		1,
+		guildId,
+	]);
 
-    await Promise.all(all.forEach(x => d.client.db.delete(table, x.key)));
+	await Promise.all(all.forEach((x) => d.client.db.delete(table, x.key)));
 
-    return {
-        code: d.util.setCode(data)
-    }
-}
+	return {
+		code: d.util.setCode(data),
+	};
+};
