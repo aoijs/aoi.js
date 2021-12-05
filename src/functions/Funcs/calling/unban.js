31857b689c80c177013465359108f8ce0ab24efd
Ayaka <user.satoshi69@gmail.com>
2021-12-05 20:53:07 +0530

    fixed unban

--- src/functions/Funcs/calling/unban.js
+++ src/functions/Funcs/calling/unban.js
@@ -1,20 +1,23 @@
-const {Data} = require("dbdjs.db");
+module.exports = async (d) => {
+	const { code } = d.command;
+	const inside = d.unpack();
+	const err = d.inside(inside);
+	if (err) return d.error(err);
+	let [userId, guildId = d.guild?.id] = inside.splits;
 
-module.exports = async d => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    const err = d.inside(inside);
-    if (err) return d.error(err);
-    let [userId, guildId = d.guild?.id] = inside.splits;
+	const guild = await d.util.getGuild(d, guildId);
+	if (!guild) return d.aoiError.fnError(d, "guild", { inside: inside });
 
-    const guild = await d.util.getGuild(d, guildId);
-    if (!guild) return d.aoiError.fnError(d, 'guild', {inside: Data.inside});
+	guild.members.unban(userId).catch((err) => {
+		d.aoiError.fnError(
+			d,
+			"custom",
+			{},
+			`Failed To Unban ${userId} With Reason: ` + err,
+		);
+	});
 
-    guild.members.unban(userId).catch(err => {
-        d.aoiError.fnError(d, 'custom', {}, `Failed To Unban ${userId} With Reason: ` + err);
-    });
-
-    return {
-        code: d.util.setCode({function: d.func, code, inside})
-    }
-}
+	return {
+		code: d.util.setCode({ function: d.func, code, inside }),
+	};
+};
