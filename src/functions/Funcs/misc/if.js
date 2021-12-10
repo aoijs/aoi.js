d92ca23edc92215bb3d914fa74ad43bd49b9a53f
Leref <12578483+Leref@users.noreply.github.com>
2021-12-09 22:23:58 -0500

    Update if.js

--- src/functions/Funcs/misc/if.js
+++ src/functions/Funcs/misc/if.js
@@ -1,53 +1,53 @@
-const { CheckCondition } = require("../../../utils/helpers/checkCondition.js");
-const { mustEscape } = require("../../../utils/helpers/mustEscape.js");
+const {CheckCondition} = require("../../../utils/helpers/checkCondition.js");
+const {mustEscape} = require("../../../utils/helpers/mustEscape.js");
 module.exports = async (d) => {
-	const data = d.util.openFunc(d);
-	if (data.err) return d.error(data.err);
+    const data = d.util.openFunc(d);
+    if (data.err) return d.error(data.err);
 
-	const [condition, trueawait, falseawait = ""] = data.inside.splits;
+    const [condition, trueawait, falseawait = ""] = data.inside.splits;
 
-	data.result = eval(CheckCondition.solve(mustEscape(condition)))
-		? trueawait.addBrackets()
-		: falseawait.addBrackets();
+    data.result = eval(CheckCondition.solve(mustEscape(condition)))
+        ? trueawait.addBrackets()
+        : falseawait.addBrackets();
 
-	if (data.result.includes("{execute:")) {
-		const cmd = d.client.cmd.awaited.find(
-			(x) =>
-				x.name.toLowerCase() ===
-				data.result
-					.addBrackets()
-					.split("{execute:")[1]
-					.split("}")[0]
-					.toLowerCase(),
-		);
-		if (!cmd)
-			return d.aoiError.fnError(
-				d,
-				"custom",
-				{},
-				`Invalid Awaited Command: '${ data.result.addBrackets().split( "{execute:" )[ 1 ].split( "}" )[ 0 ] }' Provided`,
-			);
-		await d.interpreter(
-			d.client,
-			d.message,
-			d.args,
-			cmd,
-			d.client.db,
-			false,
-			undefined,
-			{ data: d.data },
-		);
-		data.result = data.result
-			.addBrackets()
-			.replace(
-				`{execute:${
-					data.result.addBrackets().split("{execute:")[1].split("}")[0]
-				}}`,
-				"",
-			);
-	} else data.result = data.result.addBrackets();
+    if (data.result.includes("{execute:")) {
+        const cmd = d.client.cmd.awaited.find(
+            (x) =>
+                x.name.toLowerCase() ===
+                data.result
+                    .addBrackets()
+                    .split("{execute:")[1]
+                    .split("}")[0]
+                    .toLowerCase(),
+        );
+        if (!cmd)
+            return d.aoiError.fnError(
+                d,
+                "custom",
+                {},
+                `Invalid Awaited Command: '${data.result.addBrackets().split("{execute:")[1].split("}")[0]}' Provided`,
+            );
+        await d.interpreter(
+            d.client,
+            d.message,
+            d.args,
+            cmd,
+            d.client.db,
+            false,
+            undefined,
+            {data: d.data},
+        );
+        data.result = data.result
+            .addBrackets()
+            .replace(
+                `{execute:${
+                    data.result.addBrackets().split("{execute:")[1].split("}")[0]
+                }}`,
+                "",
+            );
+    } else data.result = data.result.addBrackets();
 
-	return {
-		code: d.util.setCode(data),
-	};
+    return {
+        code: d.util.setCode(data),
+    };
 };
