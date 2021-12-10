bc53005c47ba8a01f9dd5a5c8edd8b340161394e
Leref <12578483+Leref@users.noreply.github.com>
2021-12-09 22:23:33 -0500

    Update slashOption.js

--- src/functions/Funcs/callback/slashOption.js
+++ src/functions/Funcs/callback/slashOption.js
@@ -1,12 +1,12 @@
 module.exports = async (d) => {
-	const data = d.util.openFunc(d);
-	if (data.err) return d.error(data.err);
+    const data = d.util.openFunc(d);
+    if (data.err) return d.error(data.err);
 
-	const option = data.inside.inside;
+    const option = data.inside.inside;
 
-	data.result = d.data.interaction.options.get(option.addBrackets())?.value;
+    data.result = d.data.interaction.options.get(option.addBrackets())?.value;
 
-	return {
-		code: d.util.setCode(data),
-	};
+    return {
+        code: d.util.setCode(data),
+    };
 };
