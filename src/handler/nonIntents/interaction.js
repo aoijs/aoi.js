c386eb1efc379a8ee1feed86f1f770f73e126ada
Leref <12578483+Leref@users.noreply.github.com>
2021-12-09 22:24:53 -0500

    Update interaction.js

--- src/handler/nonIntents/interaction.js
+++ src/handler/nonIntents/interaction.js
@@ -1,78 +1,78 @@
 const {
-	Interaction,
-	ButtonInteraction,
-	SelectMenuInteraction,
-	ContextMenuInteraction,
-	CommandInteraction,
+    Interaction,
+    ButtonInteraction,
+    SelectMenuInteraction,
+    ContextMenuInteraction,
+    CommandInteraction,
 } = require("discord.js");
 const Interpreter = require("../../interpreter.js");
 const {
-	InteractionTypes,
-	MessageComponentTypes,
+    InteractionTypes,
+    MessageComponentTypes,
 } = require("../../utils/InteractionConstants.js");
 /**
  * @param  {Interaction | ButtonInteraction | SelectMenuInteraction | ContextMenuInteraction | CommandInteraction } interaction
  * @param  {import('../../classes/Bot.js')} client
  */
 module.exports = async (interaction, client) => {
-	client.interactionManager.resolve(interaction);
-	if (interaction.isMessageComponent()) {
-		client.interactionManager.emit("messageComponentInteraction", interaction);
-	}
+    client.interactionManager.resolve(interaction);
+    if (interaction.isMessageComponent()) {
+        client.interactionManager.emit("messageComponentInteraction", interaction);
+    }
 
-	let cmds;
-	if (InteractionTypes[interaction.type] === "component") {
-		cmds = client.cmd.interaction[
-			MessageComponentTypes[interaction.componentType]
-		]
-			.filter((x) =>
-				x.name
-					? Array.isArray(x.name)
-						? x.name.includes(interaction.customId)
-						: x.name === interaction.customId
-					: !x.name,
-			)
-			.allValues();
-	} else {
-		cmds = client.cmd.interaction.slash
-			.filter(
-				(x) => x.name.toLowerCase() === interaction.commandName.toLowerCase(),
-			)
-			.allValues();
-	}
+    let cmds;
+    if (InteractionTypes[interaction.type] === "component") {
+        cmds = client.cmd.interaction[
+            MessageComponentTypes[interaction.componentType]
+            ]
+            .filter((x) =>
+                x.name
+                    ? Array.isArray(x.name)
+                        ? x.name.includes(interaction.customId)
+                        : x.name === interaction.customId
+                    : !x.name,
+            )
+            .allValues();
+    } else {
+        cmds = client.cmd.interaction.slash
+            .filter(
+                (x) => x.name.toLowerCase() === interaction.commandName.toLowerCase(),
+            )
+            .allValues();
+    }
 
-	if (!cmds.length) return;
-	const data = {
-		client: client,
-		guild: interaction.guild,
-		message: interaction?.message,
-		channel: interaction.channel,
-		author: interaction.author,
-		member: interaction.member,
-	};
-	for (const cmd of cmds) {
-		if (cmd.name.includes("$")) {
-			cmd.name = (
-				await Interpreter(
-					client,
-					data,
-					[],
-					{ code: cmd.name, name: "NameParser" },
-					client.db,
-					true,
-				)
-			)?.code;
-		}
-		await Interpreter(
-			client,
-			data,
-			interaction.values || interaction._hoistedOptions.map((x) => x.value),
-			cmd,
-			client.db,
-			false,
-			undefined,
-			{ interaction: interaction },
-			undefined,
-		);
-	}
+    if (!cmds.length) return;
+    const data = {
+        client: client,
+        guild: interaction.guild,
+        message: interaction?.message,
+        channel: interaction.channel,
+        author: interaction.author,
+        member: interaction.member,
+    };
+    for (const cmd of cmds) {
+        if (cmd.name.includes("$")) {
+            cmd.name = (
+                await Interpreter(
+                    client,
+                    data,
+                    [],
+                    {code: cmd.name, name: "NameParser"},
+                    client.db,
+                    true,
+                )
+            )?.code;
+        }
+        await Interpreter(
+            client,
+            data,
+            interaction.values || interaction._hoistedOptions.map((x) => x.value),
+            cmd,
+            client.db,
+            false,
+            undefined,
+            {interaction: interaction},
+            undefined,
+        );
+    }
 };
