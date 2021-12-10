398b535da48ecbc9268853deb84b0c7126ed1485
Leref <12578483+Leref@users.noreply.github.com>
2021-12-09 22:23:39 -0500

    Update unban.js

--- src/functions/Funcs/calling/unban.js
+++ src/functions/Funcs/calling/unban.js
@@ -1,23 +1,23 @@
 module.exports = async (d) => {
-	const { code } = d.command;
-	const inside = d.unpack();
-	const err = d.inside(inside);
-	if (err) return d.error(err);
-	let [userId, guildId = d.guild?.id] = inside.splits;
+    const {code} = d.command;
+    const inside = d.unpack();
+    const err = d.inside(inside);
+    if (err) return d.error(err);
+    let [userId, guildId = d.guild?.id] = inside.splits;
 
-	const guild = await d.util.getGuild(d, guildId);
-	if (!guild) return d.aoiError.fnError(d, "guild", { inside: inside });
+    const guild = await d.util.getGuild(d, guildId);
+    if (!guild) return d.aoiError.fnError(d, "guild", {inside: inside});
 
-	guild.members.unban(userId).catch((err) => {
-		d.aoiError.fnError(
-			d,
-			"custom",
-			{},
-			`Failed To Unban ${userId} With Reason: ` + err,
-		);
-	});
+    guild.members.unban(userId).catch((err) => {
+        d.aoiError.fnError(
+            d,
+            "custom",
+            {},
+            `Failed To Unban ${userId} With Reason: ` + err,
+        );
+    });
 
-	return {
-		code: d.util.setCode({ function: d.func, code, inside }),
-	};
+    return {
+        code: d.util.setCode({function: d.func, code, inside}),
+    };
 };
