14ce2574d5c4f7745d1534983424ebf6bd8964ff
Ayaka <user.satoshi69@gmail.com>
2021-12-14 23:25:01 +0530

    fixed memberExist return true for empty args

--- src/functions/Funcs/util/memberExists.js
+++ src/functions/Funcs/util/memberExists.js
@@ -7,7 +7,7 @@ module.exports = async d => {
     const guild = await d.util.getGuild(d, guildId);
     if (!guild) return d.aoiError.fnError(d, 'custom', {inside: data.inside});
 
-    data.result = await d.util.getMember(guild, userId) ? true : false;
+    data.result = userId.trim() === '' ? false : await d.util.getMember(guild, userId) ? true : false;
 
     return {
         code: d.util.setCode(data)
