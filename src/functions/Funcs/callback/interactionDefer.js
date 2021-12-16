af61377b84cf3e98aa7bf0bd283661c192e2803e
Ayaka <user.satoshi69@gmail.com>
2021-12-16 15:07:50 +0530

    fixed [yes] in interactiondefer

--- src/functions/Funcs/callback/interactionDefer.js
+++ src/functions/Funcs/callback/interactionDefer.js
@@ -1,11 +1,17 @@
-module.exports = d => {
-    const data = d.util.openFunc(d);
-    const [ephemeral = 'no'] = data.inside.splits;
-    d.data.interaction?.deferReply({ephemeral: ephemeral === 'yes' }).catch(e => {
-        d.aoiError.fnError(d, 'custom', {}, 'Failed To Reply With Reason: ' + e)
-    });
+module.exports = (d) => {
+	const data = d.util.openFunc(d);
+	const [ephemeral = "no"] = data.inside.splits;
+	d.data.interaction
+		?.deferReply({ ephemeral: ephemeral === "yes" })
+		.catch((e) => {
+			d.aoiError.fnError(d, "custom", {}, "Failed To Reply With Reason: " + e);
+		});
 
-    return {
-        code: d.util.setCode({function: d.func, code: data.code})
-    }
-}
+	return {
+		code: d.util.setCode({
+			function: d.func,
+			code: data.code,
+			inside: data.inside,
+		}),
+	};
+};
