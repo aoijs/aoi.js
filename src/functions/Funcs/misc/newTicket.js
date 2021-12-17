bc9324bc9d89e802c60b82d43f288f89455f5eec
Ayaka <user.satoshi69@gmail.com>
2021-12-17 10:59:00 +0530

    fixed wrong var name in $newTicket

--- src/functions/Funcs/misc/newTicket.js
+++ src/functions/Funcs/misc/newTicket.js
@@ -60,7 +60,7 @@ module.exports = async (d) => {
         }
         d.client.db.set(
             d.client.db.tables[0],
-            "ticketChannel",
+            "ticket",
             channel.id,
             channel.id,
         );
