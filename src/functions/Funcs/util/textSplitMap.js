3d6e31584c4717618d4469fba533db6cacc1143f
Leref <12578483+Leref@users.noreply.github.com>
2021-12-17 21:43:41 -0500

    Updated format

--- src/functions/Funcs/util/textSplitMap.js
+++ src/functions/Funcs/util/textSplitMap.js
@@ -1,44 +1,44 @@
 const Interpreter = require("../../../interpreter.js");
 
 module.exports = async (d) => {
-	const data = d.util.openFunc(d);
+    const data = d.util.openFunc(d);
 
-	let commands = data.inside.splits;
-	const content = [];
+    let commands = data.inside.splits;
+    const content = [];
 
-	if (
-		commands.some(
-			(x) =>
-				!d.client.cmd.awaited.find(
-					(y) => y.name.toLowerCase() === x.trim().toLowerCase(),
-				),
-		)
-	)
-		return d.aoiError.fnError(
-			d,
-			"custom",
-			{ inside: data.inside },
-			"Invalid Awaited Command In",
-		);
+    if (
+        commands.some(
+            (x) =>
+                !d.client.cmd.awaited.find(
+                    (y) => y.name.toLowerCase() === x.trim().toLowerCase(),
+                ),
+        )
+    )
+        return d.aoiError.fnError(
+            d,
+            "custom",
+            {inside: data.inside},
+            "Invalid Awaited Command In",
+        );
 
-	for (const a of d.array) {
-		for (const command of commands) {
-			const cmd = d.client.cmd.awaited.find(
-				(y) => y.name.toLowerCase() === command.trim().toLowerCase(),
-			);
-			if (!cmd) continue;
-			else {
-				const code = (
-					await Interpreter(d.client, d.message, [a], cmd, d.client.db, true,undefined,{...d.data})
-				)?.code;
-				content.push(code);
-			}
-		}
-	}
+    for (const a of d.array) {
+        for (const command of commands) {
+            const cmd = d.client.cmd.awaited.find(
+                (y) => y.name.toLowerCase() === command.trim().toLowerCase(),
+            );
+            if (!cmd) continue;
+            else {
+                const code = (
+                    await Interpreter(d.client, d.message, [a], cmd, d.client.db, true, undefined, {...d.data})
+                )?.code;
+                content.push(code);
+            }
+        }
+    }
 
-	data.result = content.join("\n");
+    data.result = content.join("\n");
 
-	return {
-		code: d.util.setCode(data),
-	};
+    return {
+        code: d.util.setCode(data),
+    };
 };
