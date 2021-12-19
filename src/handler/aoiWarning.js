11d6ea7382b4ceb9d279eee27bbfef36e1924fe1
Leref <12578483+Leref@users.noreply.github.com>
2021-12-18 20:58:43 -0500

    aoi.js warning won't crash entire Client if API down.

--- src/handler/aoiWarning.js
+++ src/handler/aoiWarning.js
@@ -1,10 +1,14 @@
 const {default: axios} = require("axios");
+const json = require("../../package.json");
 module.exports = async () => {
-    const res = await axios.get("https://api.leref.ga/package/version");
+    try {
+        const res = await axios.get("https://api.leref.ga/package/version");
 
-    console.warn(
-        "\x1b[31maoi.js warning: \u001b[33mAvailable version v" +
-        res.data.version +
-        " ready to install.\u001b[0m",
-    )
-}
+        if (json.version !== res.data.version) {
+
+            console.warn(
+                "\x1b[31maoi.js warning: \u001b[33mAvailable version v" +
+                res.data.version +
+                " ready to install.\u001b[0m",
+            )}} catch {}
+};
--- src/handler/startup.js
+++ src/handler/startup.js
@@ -1,16 +1,17 @@
 const {Team} = require("discord.js");
 const Interpreter = require("../interpreter.js");
 const {default: axios} = require("axios");
-const json = require("../../package.json");
 
 module.exports = async (client) => {
-    const res = await axios.get("https://api.leref.ga/package/version");
+    await axios.get("https://api.leref.ga/package/version");
     const app = await client.application.fetch();
     if (app.owner instanceof Team) {
         client.aoiOptions.Owner = app.owner.members.map((x) => x.id);
     } else {
         client.aoiOptions.Owner = [app.owner.id];
     }
+
+    await require("./aoiWarning.js")(client);
 
     require("./custom/timeout.js")(
         {client, interpreter: Interpreter},
@@ -30,9 +31,6 @@ module.exports = async (client) => {
 
     if (client.cmd.loop.size) {
         require("./custom/loop.js")(client);
-    }
-    if (json.version !== res.data.version) {
-        await require("./aoiWarning.js")(client);
     }
 
 };
