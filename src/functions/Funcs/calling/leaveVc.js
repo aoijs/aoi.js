36fa5f14e7b9b7777d65b54925220a2fba77f7b6
Ayaka <user.satoshi69@gmail.com>
2021-12-21 18:31:11 +0530

    fixed $leaveVc

--- src/functions/Funcs/calling/leaveVc.js
+++ src/functions/Funcs/calling/leaveVc.js
@@ -1,12 +1,18 @@
 module.exports = async (d) => {
-    const {code} = d.util.openFunc(d);
+	const { code } = d.util.openFunc(d);
 
-    const state = d.client.voiceManager.servers.get(d.guild.id);
-    if (!(state)) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, `Bot is not in any voice channel`);
+	const state = d.client.voiceManager.servers.get(d.guild.id);
+	if (!state)
+		return d.aoiError.fnError(
+			d,
+			"custom",
+			{},
+			`Bot is not in any voice channel`,
+		);
 
-    state.leaveVc();
+	state.leaveVc();
 
-    return {
-        code: d.util.setCode({function: d.func, code})
-    }
-}
+	return {
+		code: d.util.setCode({ function: d.func, code }),
+	};
+};
