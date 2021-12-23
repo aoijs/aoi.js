cf23e6e34ca4adbff3a832f77bcfef4c8d62ad01
Ayaka <user.satoshi69@gmail.com>
2021-12-23 23:49:18 +0530

    dynamized the type in emoji Count

--- src/functions/Funcs/info/emojiCount.js
+++ src/functions/Funcs/info/emojiCount.js
@@ -1,13 +1,15 @@
-module.exports = async d => {
-    const {code} = d.command
-    const inside = d.unpack()
-    let [guildId, type] = inside.splits;
+module.exports = async (d) => {
+	const { code } = d.command;
+	const inside = d.unpack();
+	let [guildId, type] = inside.splits;
 
-    const guild = await d.util.getGuild(d, guildId);
-    if (!guild) return d.aoiError.fnError(d, "guild", {inside});
+	const guild = await d.util.getGuild(d, guildId);
+	if (!guild) return d.aoiError.fnError(d, "guild", { inside });
 
-    const result = type ? guild.emojis.cache.filter(x => x.type === type).size : guild.emojis.cache.size
-    return {
-        code: d.util.setCode({function: d.func, code, inside, result})
-    }
-}
+	const result = type
+		? guild.emojis.cache.filter((x) => x[type]).size
+		: guild.emojis.cache.size;
+	return {
+		code: d.util.setCode({ function: d.func, code, inside, result }),
+	};
+};
