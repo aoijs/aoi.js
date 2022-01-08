db0722367bcce2c2e80fe6014d5f30a830927570
Ayaka <user.satoshi69@gmail.com>
2022-01-08 20:33:31 +0530

    bumped djs version to latest

--- src/functions/Funcs/calling/footer.js
+++ src/functions/Funcs/calling/footer.js
@@ -1,16 +1,20 @@
-module.exports = async d => {
-    const {code} = d.command
-    const inside = d.unpack()
-    const err = d.inside(inside)
-    if (err) return d.error(err)
-    let [index, name, iconUrl] = inside.splits;
-    index = Number(index) - 1
-    if (isNaN(index) || index < 0) d.aoiError.fnError(d, "custom", {inside}, "Invalid Index Provided In")
-    if (!d.embeds[index]) d.embeds[index] = new d.embed()
-    d.embeds[index].setFooter(name.addBrackets(), iconUrl?.addBrackets());
+module.exports = async (d) => {
+  const { code } = d.command;
+  const inside = d.unpack();
+  const err = d.inside(inside);
+  if (err) return d.error(err);
+  let [index, name, iconUrl] = inside.splits;
+  index = Number(index) - 1;
+  if (isNaN(index) || index < 0)
+    d.aoiError.fnError(d, "custom", { inside }, "Invalid Index Provided In");
+  if (!d.embeds[index]) d.embeds[index] = new d.embed();
+  d.embeds[index].setFooter({
+    text: name.addBrackets(),
+    iconURL: iconUrl?.addBrackets(),
+  });
 
-    return {
-        code: d.util.setCode({function: d.func, code, inside}),
-        embeds: d.embeds
-    }
-}
+  return {
+    code: d.util.setCode({ function: d.func, code, inside }),
+    embeds: d.embeds,
+  };
+};
--- src/functions/Funcs/calling/timeoutMember.js
+++ src/functions/Funcs/calling/timeoutMember.js
@@ -1,53 +1,51 @@
 module.exports = async (d) => {
-    const data = d.util.openFunc(d);
+  const data = d.util.openFunc(d);
 
-    let [
-        guildId = d.guild?.id,
-        memberId = d.author?.id,
-        timeout = "60",
-        timeoutEndsAt = "no",
-        reason,
-    ] = data.inside.splits;
+  let [
+    guildId = d.guild?.id,
+    memberId = d.author?.id,
+    timeout = "60",
+    timeoutEndsAt = "no",
+    reason,
+  ] = data.inside.splits;
 
-    const guild = await d.util.getGuild(d, guildId);
-    if (!guild) return d.aoiError.fnError(d, "guild", {inside: data.inside});
+  const guild = await d.util.getGuild(d, guildId);
+  if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
 
-    const member = await d.util.getMember(guild, memberId);
-    if (!member) return d.aoiError.fnError(d, "member", {inside: data.inside});
+  const member = await d.util.getMember(guild, memberId);
+  if (!member) return d.aoiError.fnError(d, "member", { inside: data.inside });
 
-    timeout = Number(timeout);
+  timeout = Number(timeout);
 
-    if (![0, 60, 300, 600, 3600, 86400, 7 * 86400].includes(timeout))
-        return d.aoiError.fnError(
-            d,
-            "custom",
-            {inside: data.inside},
-            "Invalid Timeout Time Provided In",
-        );
+  if (![0, 60, 300, 600, 3600, 86400, 7 * 86400].includes(timeout))
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      { inside: data.inside },
+      "Invalid Timeout Time Provided In",
+    );
 
-    const mem = await member
-        .edit(
-            {
-                communication_disabled_until:
-                    timeout === 0
-                        ? null
-                        : new Date(Date.now() + timeout * 1000).toISOString(),
-            },
-            reason,
-        )
-        .catch((err) => {
-            return d.aoiError.fnError(
-                d,
-                "custom",
-                {},
-                `Failed To Timeout The Member With Reason:${err}`,
-            );
-        });
+  const mem = await member
+    .edit(
+      {
+        communicationDisabledUntil:
+          timeout === 0 ? null : Date.now() + timeout * 1000,
+      },
+      reason,
+    )
+    .catch((err) => {
+      return d.aoiError.fnError(
+        d,
+        "custom",
+        {},
+        `Failed To Timeout The Member With Reason:${err}`,
+      );
+    });
 
-    data.result =
-        timeoutEndsAt === "yes" ? Date.now() + timeout * 1000 : undefined;
+  data.result =
+    timeoutEndsAt === "yes" ? Date.now() + timeout * 1000 : undefined;
 
-    return {
-        code: d.util.setCode(data),
-    };
+  return {
+    code: d.util.setCode(data),
+  };
 };
