4d2b95f6e468012938e22eadcc13b2da7a1af3cd
Azusfin <Azusfin@users.noreply.github.com>
2022-01-23 12:17:42 +0700

    fix $dbPing format

--- src/functions/Funcs/info/dbPing.js
+++ src/functions/Funcs/info/dbPing.js
@@ -1,6 +1,13 @@
 module.exports = async d => {
     const {code} = d.util.openFunc(d);
-    const ping = d.client.db.roundTrip ? await d.client.db.roundTrip() : d.client.db.ping
+
+    let ping
+
+    if (d.client.db.roundTrip) {
+        ping = await d.client.db.roundTrip()
+    } else {
+        ping = d.client.db.ping
+    }
 
     return {
         code: d.util.setCode({function: d.func, code, result: ping})
