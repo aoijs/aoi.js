2cb3abe0a2ad65633c6579ce6016a5744185a0fd
Ayaka <user.satoshi69@gmail.com>
2022-01-29 15:25:16 +0530

    fixed code is not defined

--- src/functions/Funcs/calling/setRoleColor.js
+++ src/functions/Funcs/calling/setRoleColor.js
@@ -1,17 +1,19 @@
 module.exports = async (d) => {
-    const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
+  const data = d.util.openFunc(d);
+  if (data.err) return d.error(data.err);
 
-    const [roleID, color] = data.inside.splits;
+  const [roleID, color] = data.inside.splits;
 
-    const role = d.guild.roles.cache.get(roleID);
-    if (!role) return d.error(`${d.func}: Invalid role ID in ${data.inside}`);
+  const role = d.guild.roles.cache.get(roleID);
+  if (!role) return d.error(`${d.func}: Invalid role ID in ${data.inside}`);
 
-    const re = await role.setColor(color).catch((err) => {
-    });
-    if (!re) return d.error(`${d.func}: Failed to change ${role.name} color to ${color}!`);
+  const re = await role.setColor(color).catch((err) => {});
+  if (!re)
+    return d.error(
+      `${d.func}: Failed to change ${role.name} color to ${color}!`,
+    );
 
-    return {
-        code: code.replaceLast(`${d.func}${data.inside}`, ""),
-    };
+  return {
+    code: d.util.setCode(data),
+  };
 };
