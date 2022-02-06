a49f1d7cab7a8c1b3e0b282bf505dcaa012f2c86
Ayaka <user.satoshi69@gmail.com>
2022-02-06 15:46:45 +0530

    adding some customFilters

--- src/functions/Funcs/music/addFilter.js
+++ src/functions/Funcs/music/addFilter.js
@@ -1,3 +1,26 @@
+const customFilters = {
+  pitch(number) {
+    return {
+      atempo: 1.0,
+      asetrate: 48000 * number,
+      aresample: 48000 * number,
+    };
+  },
+  volume(number = 100) {
+    return {
+      volume: number / 100,
+    };
+  },
+  nightcore(number) {
+    return {
+      asubboost: `dry=0.7:wet=0.5`,
+      atempo: 1 * number,
+      asetrate: 48000 * number,
+      aresample: 48000,
+    };
+  },
+};
+
 module.exports = async (d) => {
   const data = d.util.openFunc(d);
   if (data.err) return d.error(data.err);
@@ -15,6 +38,13 @@ module.exports = async (d) => {
       },
       "Invalid Filter Provided In",
     );
+  }
+
+  for (const f of Object.keys(filter)) {
+    if (customFilters[f]) {
+      filter = { ...filter, ...customFilters[f](filter[f]) };
+      delete filter[f];
+    }
   }
 
   const player = d.client.voiceManager.players.get(d.guild?.id);
