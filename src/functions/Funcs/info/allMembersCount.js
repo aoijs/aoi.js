252a731eeebc12fe738a0038aee199c735748492
Aniket Bhattacharjee <appsoftlimited@gmail.com>
2022-02-09 15:26:07 +0530

    Update allMembersCount.js

--- src/functions/Funcs/info/allMembersCount.js
+++ src/functions/Funcs/info/allMembersCount.js
@@ -1,5 +1,5 @@
 module.exports = async d => {
-    const {code} = d.command
+    const {code} = d.command;
     const result = d.client.guilds.cache.map(x => x.memberCount ?? 0).reduce((a, b) => a + b)
     return {
         code: d.util.setCode({function: d.func, code, result})
