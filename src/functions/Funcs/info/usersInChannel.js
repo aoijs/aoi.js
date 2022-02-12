88bb65cd93f1e55806a07a8f3fafa7e0277fdc99
Ayaka <user.satoshi69@gmail.com>
2022-02-12 21:26:06 +0530

    fixed $usersINChannel

--- src/functions/Funcs/info/usersInChannel.js
+++ src/functions/Funcs/info/usersInChannel.js
@@ -1,15 +1,28 @@
-module.exports = async d => {
-    const data = d.util.openFunc(d);
+module.exports = async (d) => {
+  const data = d.util.openFunc(d);
 
-    const [channelId, option = 'id', sep = ' , '] = data.inside.splits;
+  const [channelId, option = "id", sep = " , "] = data.inside.splits;
 
-    const channel = await d.util.getChannel(d, channelId, true);
-    if (!channel) return d.aoiError.fnError(d, 'channel', {inside: data.inside});
-    if (!['voice', 'stage'].includes(d.util.channelTypes[channel.type])) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Channel Type Is Not Voice/Stage');
+  const channel = await d.util.getChannel(d, channelId, true);
+  if (!channel)
+    return d.aoiError.fnError(d, "channel", { inside: data.inside });
+  if (
+    ![d.util.channelTypes.Voice, d.util.channelTypes.Stage].includes(
+      channel.type,
+    )
+  )
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      { inside: data.inside },
+      "Channel Type Is Not Voice/Stage",
+    );
 
-    data.result = channel.members.map(x => option === "mention" ? x.toString() : x[option]).join(sep)
+  data.result = channel.members
+    .map((x) => (option === "mention" ? x.toString() : x[option]))
+    .join(sep);
 
-    return {
-        code: d.util.setCode(data)
-    }
-}
+  return {
+    code: d.util.setCode(data),
+  };
+};
