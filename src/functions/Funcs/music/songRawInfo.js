15b5d7d87beed08865e235caa3730b331b99b45b
Ayaka <user.satoshi69@gmail.com>
2022-02-14 02:34:33 +0530

    added songRawInfo

---
+++ src/functions/Funcs/music/songRawInfo.js
@@ -0,0 +1,59 @@
+module.exports = async (d) => {
+  const data = d.util.openFunc(d);
+
+  let [response = "title", position = "current"] = data.inside.splits;
+
+  const player = d.client.voiceManager.players.get(d.guild?.id);
+
+  if (!player)
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      {},
+      "Client Is Not Connected To Voice/Stage.",
+    );
+
+  if (position === "current") {
+    const trackData = player.queue.current;
+
+    response = response.replace(
+      response,
+      response.startsWith("user.")
+        ? trackData.requestUser.user[response.split("user.")[1]]
+        : response.startsWith("member.")
+        ? trackData.requestUser[response.split("member.")[1]]
+        : eval(`trackData.rawInfo.${response}`),
+    );
+    data.result = response;
+  } else if (position === "previous") {
+    const trackData = player.queue.previous;
+    if (!trackData) return;
+    else {
+      response = response.replace(
+        response,
+        response.startsWith("user.")
+          ? trackData.requestUser.user[response.split("user.")[1]]
+          : response.startsWith("member.")
+          ? trackData.requestUser[response.split("member.")[1]]
+          : eval(`trackData.rawInfo.${response}`),
+      );
+      data.result = response;
+    }
+  } else {
+    const trackData = player.queue.list[position];
+    if (!trackData) return;
+    response = response.replace(
+      response,
+      response.startsWith("user.")
+        ? trackData.requestUser.user[response.split("user.")[1]]
+        : response.startsWith("member.")
+        ? trackData.requestUser[response.split("member.")[1]]
+        : eval(`trackData.rawInfo.${response}`),
+    );
+    data.result = response;
+  }
+
+  return {
+    code: d.util.setCode(data),
+  };
+};
