45add86cb01732850126a57fa177cc3d465190d6
Ayaka <user.satoshi69@gmail.com>
2022-02-14 01:26:09 +0530

    updated trackInfo to support url array

--- src/functions/Funcs/callback/trackInfo.js
+++ src/functions/Funcs/callback/trackInfo.js
@@ -4,7 +4,11 @@ module.exports = async (d) => {
 
   const [func] = data.inside.splits;
 
-  data.result = eval(`d.data.track.${func}`);
+  data.result = eval(
+    Array.isArray(d.data.track)
+      ? `d.data.track[${func}]`
+      : `d.data.track.${func}`,
+  );
 
   return {
     code: d.util.setCode(data),
