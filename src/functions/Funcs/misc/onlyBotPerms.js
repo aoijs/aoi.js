d3d7449eeec8cadf726ff885888d112dc10d26f1
Ayaka <user.satoshi69@gmail.com>
2022-02-14 15:37:42 +0530

    added $getCurrentDuration

--- src/functions/Funcs/misc/onlyBotPerms.js
+++ src/functions/Funcs/misc/onlyBotPerms.js
@@ -1,35 +1,40 @@
-const {Perms} = require('../../../utils/Constants.js');
+const { Perms } = require("../../../utils/Constants.js");
 
-module.exports = async d => {
-    const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
-    let error = false;
+module.exports = async (d) => {
+  const data = d.util.openFunc(d);
+  if (data.err) return d.error(data.err);
+  let error = false;
 
-    const [...stuffs] = data.inside.splits;
-    const err = stuffs.pop();
+  const [...stuffs] = data.inside.splits;
+  const err = stuffs.pop();
 
-    if (stuffs.some(x => !Perms[x])) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Permission(s) Provided In');
+  if (stuffs.some((x) => !Perms[x]))
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      { inside: data.inside },
+      "Invalid Permission(s) Provided In",
+    );
 
-    const BotPerms = d.guild.me.permissions.toArray();
-    if (BotPerms.includes(Perms.admin)) {
-
-    } else if (!stuffs.every(x => BotPerms.includes(Perms[x.trim()]))) {
-        error = true;
-        if (err?.trim() === '') {
-        } else {
-            const errorMsg = await d.util.errorParser(err, d);
-            d.aoiError.makeMessageError(
-                d.client,
-                d.channel,
-                errorMsg,
-                errorMsg.options,
-                d,
-            );
-        }
+  const BotPerms = d.guild.me.permissions.toArray();
+  if (BotPerms.includes(Perms.admin)) {
+  } else if (!stuffs.every((x) => BotPerms.includes(Perms[x.trim()]))) {
+    error = true;
+    if (err?.trim() === "") {
+    } else {
+      const errorMsg = await d.util.errorParser(err, d);
+      d.aoiError.makeMessageError(
+        d.client,
+        d.channel,
+        errorMsg,
+        errorMsg.options,
+        d,
+      );
     }
+  }
 
-    return {
-        code: d.util.setCode(data),
-        error
-    }
-}
+  return {
+    code: d.util.setCode(data),
+    error,
+  };
+};
---
+++ src/functions/Funcs/music/getCurrentDuration.js
@@ -0,0 +1,27 @@
+module.exports = async (d) => {
+  const data = d.util.openFunc(d);
+
+  if (!d.client.voiceManager) {
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      {},
+      "Voice Class Isn't Initialized.",
+    );
+  }
+  const player = d.client.voiceManager.players.get(d.guild?.id);
+  if (!player) {
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      {},
+      "Client Is Not Connected To Voice/Stage.",
+    );
+  }
+
+  data.result = player.requestManager._currentDuration;
+
+  return {
+    code: d.util.setCode(data),
+  };
+};
