8ffb442cecbe1612c9b34077aaa09bf15907c59a
Ayaka <user.satoshi69@gmail.com>
2022-02-16 16:48:16 +0530

    channelPermissionFor now accepts both role and member

--- src/functions/Funcs/calling/channelPermissionsFor.js
+++ src/functions/Funcs/calling/channelPermissionsFor.js
@@ -1,19 +1,16 @@
 module.exports = async (d) => {
-    const {code} = d.command;
-    const inside = d.unpack();
+  const { code } = d.command;
+  const inside = d.unpack();
 
-    const [userId = d.author.id, channelId = d.channel.id, sep = " , "] =
-        inside.splits;
+  const [uorrId = d.author.id, channelId = d.channel.id, sep = " , "] =
+    inside.splits;
 
-    const member = await d.util.getMember(d.guild, userId);
-    if (!member) return d.aoiError.fnError(d, "member", {inside});
+  const channel = await d.util.getChannel(d, channelId);
+  if (!channel) return d.aoiError.fnError(d, "channel", { inside });
 
-    const channel = await d.util.getChannel(d, channelId);
-    if (!channel) return d.aoiError.fnError(d, "channel", {inside});
+  const result = channel.permissionFor(uorrId)?.toArray().join(sep);
 
-    const result = member.permissionsIn(channelId)?.toArray().join(sep);
-
-    return {
-        code: d.util.setCode({function: d.func, code, inside, result}),
-    };
+  return {
+    code: d.util.setCode({ function: d.func, code, inside, result }),
+  };
 };
