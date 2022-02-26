3c3a3a599921189fb3d13c2708e2f293ed863c17
Ayaka <user.satoshi69@gmail.com>
2022-02-26 22:56:05 +0530

    fixed isCustomEmoji now it also accepts full string

--- src/functions/Funcs/util/isCustomEmoji.js
+++ src/functions/Funcs/util/isCustomEmoji.js
@@ -1,17 +1,24 @@
-module.exports = async d => {
-    const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
+module.exports = async (d) => {
+  const data = d.util.openFunc(d);
+  if (data.err) return d.error(data.err);
 
-    const [emoji, guildId = d.guild?.id] = data.inside.splits;
+  let [emoji, guildId = d.guild?.id] = data.inside.splits;
+  emoji = emoji.split(":");
+  if (emoji.length > 1) {
+    emoji = emoji.pop().replace(">");
+  } else {
+    emoji = emoji[0];
+  }
 
-    const guild = guildId === 'global' ? d.client : await d.util.getGuild(d, guildId);
-    if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
+  const guild =
+    guildId === "global" ? d.client : await d.util.getGuild(d, guildId);
+  if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
 
-    let isemoji = await guild.emojis.fetch(emoji).catch(e => undefined);
+  let isemoji = await guild.emojis.fetch().catch((e) => undefined);
 
-    data.result = isemoji ? true : false;
+  data.result = isemoji ? true : false;
 
-    return {
-        code: d.util.setCode(data)
-    }
-}
+  return {
+    code: d.util.setCode(data),
+  };
+};
