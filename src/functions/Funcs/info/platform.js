ca85db93bc0540c2f7f0ed3852a95263e505fac8
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2022-03-02 17:03:04 +0530

    Update platform.js

--- src/functions/Funcs/info/platform.js
+++ src/functions/Funcs/info/platform.js
@@ -7,7 +7,7 @@ module.exports = async d => {
     if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
 
     const member = await d.util.getMember(guild, userId);
-    if (!member) return d.aoiError.fnError(guild, 'member', {inside: data.inside});
+    if (!member) return d.aoiError.fnError(d, 'member', {inside: data.inside});
 
     data.result = member.presence?.status === 'offline' ? 'none' : Object.keys(member.presence?.clientStatus || {none: "none"});
 
