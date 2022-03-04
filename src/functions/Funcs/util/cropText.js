f976de19f0526030cbdaac32053245476583a830
Ayaka <user.satoshi69@gmail.com>
2022-03-04 17:18:59 +0530

    added start field and char field in $cropText
    new usage $cropText[text;limit?;start?;char?]

--- src/functions/Funcs/util/cropText.js
+++ src/functions/Funcs/util/cropText.js
@@ -2,9 +2,9 @@ module.exports = async d => {
     const {code, inside, err} = d.util.openFunc(d);
     if (err) return d.error(err);
 
-    const [text, limit = 2000] = inside.splits;
+    const [text, limit = 2000,start = 0,char = ""] = inside.splits;
 
-    const result = text.addBrackets().trim().slice(0, limit);
+    const result = text.addBrackets().trim().split(char).slice(Number(start), Number(limit)).join(char);
 
     return {
         code: d.util.setCode({function: d.func, code, inside, result})
