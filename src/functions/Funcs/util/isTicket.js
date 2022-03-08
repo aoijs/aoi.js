6834d7446e3c26f09d6f2fde76687b794101ae20
Ayaka <user.satoshi69@gmail.com>
2022-03-08 19:15:03 +0530

    removed duplicated $isTicket FIle

--- src/functions/Funcs/util/isTicket.js
+++
@@ -1,9 +0,0 @@
-module.exports = async d => {
-    const data = d.util.openFunc(d);
-
-    const [ticketId] = data.inside.splits;
-    data.result = (await d.client.db.get(d.client.db.tables[0], 'ticketChannel', ticketId)) ? true : false
-    return {
-        code: d.util.setCode(data)
-    }
-}
