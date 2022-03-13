3c738b0d36f3f234665f5fb935ec1973c6d05a98
Ayaka <user.satoshi69@gmail.com>
2022-03-14 01:30:12 +0530

    updated $deleteChannels channel finder loop

--- src/functions/Funcs/calling/deleteChannels.js
+++ src/functions/Funcs/calling/deleteChannels.js
@@ -5,12 +5,12 @@ module.exports = async d => {
     const [...channels] = inside.splits;
     const wrongChannels = []
     const chans = []
-    channels.forEach(x => {
-        if (!d.client.channels.cache.find(y => y.name.toLowerCase() === x.toLowerCase().addBrackets() || y.id === x.id)) wrongChannels.push(x)
-        else chans.push(d.client.channels.cache.find(y => y.name.toLowerCase() === x.toLowerCase().addBrackets() || y.id === x.id))
-    })
+    await channels.forEach(async x => {
+        const c = await d.util.getChannel(d,x);
+        if (!c) wrongChannels.push(x);
+        else chans.push(c);
+    });
     if (wrongChannels.length) return d.aoiError.fnError(d, "custom", {inside}, `Invalid Channels : ${wrongChannels.join(" , ")} Provided In`);
-
     for (let i = chans.length - 1; i >= 0; i--) {
         chans[i].delete().catch(e => {
             d.aoiError.fnError(d, "custom", {}, "Failed To Delete Channel: " + chans[i].name + " With Reason: " + e);
