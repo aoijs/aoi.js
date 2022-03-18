3fad276ce4ca912144b610d81425b751d693fd9c
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2022-03-19 00:55:10 +0530

    optional chaining for partial message data

--- src/functions/Funcs/calling/clear.js
+++ src/functions/Funcs/calling/clear.js
@@ -1,32 +1,59 @@
-module.exports = async d => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    const err = d.inside(inside);
-    if (err) return d.error(err);
+module.exports = async (d) => {
+  const { code } = d.command;
+  const inside = d.unpack();
+  const err = d.inside(inside);
+  if (err) return d.error(err);
 
-    let [amt, filter = "everyone", returnCount = "no", channelId = d.channel.id] = inside.splits;
+  let [amt, filter = "everyone", returnCount = "no", channelId = d.channel.id] =
+    inside.splits;
 
-    amt = Number(amt);
-    if (isNaN(amt)) return d.aoiError.fnError(d, "custom", {inside}, "Invalid Amout Provided In");
+  amt = Number(amt);
+  if (isNaN(amt))
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      { inside },
+      "Invalid Amout Provided In",
+    );
 
-    const channel = await d.util.getChannel(d, channelId);
-    if (!channel) return d.aoiError.fnError(d, "channel", {inside});
+  const channel = await d.util.getChannel(d, channelId);
+  if (!channel) return d.aoiError.fnError(d, "channel", { inside });
 
-    let messages = filter === "everyone" ?  await channel.messages.fetch({limit: amt, cache: false}).catch(err => {
-        d.aoiError.fnError(d, "custom", {}, "Failed To Fetch Messages With Reason: " + err);
-    }) : channel.messages.cache;
+  let messages =
+    filter === "everyone"
+      ? await channel.messages
+          .fetch({ limit: amt, cache: false })
+          .catch((err) => {
+            d.aoiError.fnError(
+              d,
+              "custom",
+              {},
+              "Failed To Fetch Messages With Reason: " + err,
+            );
+          })
+      : channel.messages.cache;
 
-    messages = messages.filter(x => (filter === "everyone" ? true : (filter === "unPins" ? !x.pinned : filter === 'bot' ? x.author.bot : x.author.id === filter)));
+  messages = messages.filter((x) =>
+    filter === "everyone"
+      ? true
+      : filter === "unPins"
+      ? !x.pinned
+      : filter === "bot"
+      ? x.author?.bot
+      : x.author?.id === filter,
+  );
 
-    let result = await channel.bulkDelete(
-        messages,
-        true
-    ).catch(err => {
-        d.aoiError.fnError(d, "custom", {}, "Failed To Delete Message With Reason: " + err);
-    });
-    result = returnCount === "yes" ? result.size : undefined;
+  let result = await channel.bulkDelete(messages, true).catch((err) => {
+    d.aoiError.fnError(
+      d,
+      "custom",
+      {},
+      "Failed To Delete Message With Reason: " + err,
+    );
+  });
+  result = returnCount === "yes" ? result.size : undefined;
 
-    return {
-        code: d.util.setCode({function: d.func, code, inside, result})
-    }
-}
+  return {
+    code: d.util.setCode({ function: d.func, code, inside, result }),
+  };
+};
