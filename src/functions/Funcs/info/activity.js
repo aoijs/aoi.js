043e2c75ff979658b68d143b606b4db39ee8b41c
Leref <12578483+Leref@users.noreply.github.com>
2022-04-06 12:24:23 +0000

    bump($activity as example for new method)

--- src/functions/Funcs/info/activity.js
+++ src/functions/Funcs/info/activity.js
@@ -1,19 +1,16 @@
 module.exports = async (d) => {
-    const code = d.command.code;
-    const inside = d.unpack()
-    const [id = d.author?.id, guildId = d.guild?.id] = inside.splits;
+    const data = d.util.openFunc(d);
+    const [id = d.author?.id, guildId = d.guild?.id] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildId);
     if (!guild) return d.aoiError.fnError(d, 'guild', {inside: DataTransfer.inside});
 
     let u = await d.util.getMember(guild, id)
-    if (!u) return d.aoiError.fnError(d, "member", {inside})
+    if (!u) return d.aoiError.fnError(d, "member", {inside: data.inside})
+
+    data.result =  u.presence?.activities?.join(", ")?.deleteBrackets() || "none"
+
     return {
-        code: d.util.setCode({
-            function: d.func,
-            code,
-            inside,
-            result: u.presence?.activities?.join(", ")?.deleteBrackets() || "none"
-        })
+        code: d.util.setCode(data)
     }
 };
