4f0ad467252e248315c7c6bc1b950cc995dd27bb
Ayaka <user.satoshi69@gmail.com>
2022-04-17 20:31:30 +0530

    added none in $autoPlay

--- src/functions/Funcs/music/autoPlay.js
+++ src/functions/Funcs/music/autoPlay.js
@@ -5,7 +5,7 @@ module.exports = async (d) => {
 
   const [type] = data.inside.splits;
 
-  if (!["soundcloud", "relative", "youtube"].includes(type))
+  if (!["soundcloud", "relative", "youtube", "none"].includes(type))
     return d.aoiError.fnError(
       d,
       "custom",
@@ -15,13 +15,13 @@ module.exports = async (d) => {
       "Invalid Type Provided In",
     );
 
-    if (!d.client.voiceManager)
-      return d.aoiError.fnError(
-        d,
-        "custom",
-        {},
-        "Voice Class Is Not Initialised.",
-      );
+  if (!d.client.voiceManager)
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      {},
+      "Voice Class Is Not Initialised.",
+    );
 
   const player = d.client.voiceManager.manager.players.get(d.guild?.id);
   if (!player)
@@ -32,7 +32,7 @@ module.exports = async (d) => {
       "Client Is Not Connected To Voice/Stage.",
     );
 
-  player.options.autoPlay = type;
+  player.options.autoPlay = type === "none" ? null : type;
 
   return {
     code: d.util.setCode(data),
