ae83754ad9badefd01cd630ede45fba8557647dd
Ayaka <user.satoshi69@gmail.com>
2022-05-17 19:23:55 +0530

    added more options for emojis

--- src/functions/Funcs/info/allEmojisCount.js
+++ src/functions/Funcs/info/allEmojisCount.js
@@ -1,9 +1,19 @@
-module.exports = async d => {
-    const {code} = d.command
-    const inside = d.unpack()
-    let [type] = inside.splits
-    const result = type ? d.client.emojis.cache.filter(x => x.type === type).size : d.client.emojis.cache.size
-    return {
-        code: d.util.setCode({function: d.func, code, inside, result})
-    }
-}
+module.exports = async (d) => {
+  const { code } = d.command;
+  const inside = d.unpack();
+  let [type] = inside.splits;
+  const result = type
+    ? d.client.emojis.cache.filter((x) =>
+        type === "animated"
+          ? x.animated
+          : type === "roles"
+          ? x.roles.size
+          : type === "normal"
+          ? !x.animated
+          : x[type],
+      ).size
+    : d.client.emojis.cache.size;
+  return {
+    code: d.util.setCode({ function: d.func, code, inside, result }),
+  };
+};
--- src/functions/Funcs/info/emojiCount.js
+++ src/functions/Funcs/info/emojiCount.js
@@ -1,19 +1,29 @@
 module.exports = async (d) => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    let [guildId, type,force = "no"] = inside.splits;
+  const { code } = d.command;
+  const inside = d.unpack();
+  let [guildId, type, force = "no"] = inside.splits;
 
-    const guild = await d.util.getGuild(d, guildId);
-    if (!guild) return d.aoiError.fnError(d, "guild", {inside});
+  const guild = await d.util.getGuild(d, guildId);
+  if (!guild) return d.aoiError.fnError(d, "guild", { inside });
 
-    if(force === "yes")  {
-        guild.emojis.fetch();
-    }
+  if (force === "yes") {
+    guild.emojis.fetch();
+  }
 
-    const result = type
-        ? guild.emojis.cache.filter((x) => type === "animated" ? x.animated : type === "normal" ? !x.animated : type === "all" ? true : x[type]).size
-        : guild.emojis.cache.size;
-    return {
-        code: d.util.setCode({function: d.func, code, inside, result}),
-    };
+  const result = type
+    ? guild.emojis.cache.filter((x) =>
+        type === "animated"
+          ? x.animated
+          : type === "roles"
+          ? x.roles.size
+          : type === "normal"
+          ? !x.animated
+          : type === "all"
+          ? true
+          : x[type],
+      ).size
+    : guild.emojis.cache.size;
+  return {
+    code: d.util.setCode({ function: d.func, code, inside, result }),
+  };
 };
