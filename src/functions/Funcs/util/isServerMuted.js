4933f26af0f4ccf26c5d4bbd336160844ad4a9f3
Leref <12578483+Leref@users.noreply.github.com>
2022-06-10 17:25:56 +0000

    $isServerMuted -> guildID is first field.

--- src/functions/Funcs/util/isServerMuted.js
+++ src/functions/Funcs/util/isServerMuted.js
@@ -1,14 +1,14 @@
 module.exports = async d => {
-    const data = d.util.openFunc(d);
-    if (data.err) return d.error(data.err);
-    const [userId = d.member?.id, guildId = d.guild?.id] = data.inside.splits;
-    const guild = await d.util.getGuild(d, guildId);
+    const data = d.util.aoiFunc(d);
+    const [guildID = d.guild?.id, userID = d.member?.id] = data.inside.splits;
+    
+    const guild = await d.util.getGuild(d, guildID);
     if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
-    const user = await d.util.getMember(guild, userId)
+    const user = await d.util.getMember(guild, userID)
     if (!user) return d.aoiError.fnError(d, 'member', {inside: data.inside});
+
     data.result = user.voice.serverMute
     return {
         code: d.util.setCode(data)
     }
 }
-//Usage `$isServerMuted[user's id(optional); server's id(optional)]`
