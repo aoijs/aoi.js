99986c92d648a10315e510b0c1daaa77b94092f5
Leref <12578483+Leref@users.noreply.github.com>
2022-06-12 01:40:48 +0000

    updated method.

--- src/functions/Funcs/misc/getApplicationCommandId.js
+++ src/functions/Funcs/misc/getApplicationCommandId.js
@@ -7,14 +7,16 @@ module.exports = async d => {
     if (type === 'global') {
         const all = await d.client.application.commands.fetch();
 
-        data.result = all.find(x => x.name === name.addBrackets().toLowerCase())?.id;
+        data.result = all.find(
+          (x) => x.name.toLowerCase() === name.addBrackets().toLowerCase(),
+        )?.id;
     } else {
         const guild = await d.util.getGuild(d, type);
         if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
 
         const all = await guild.commands.fetch();
 
-        data.result = all.find(x => x.name === name.addBrackets().toLowerCase())?.id;
+        data.result = all.find(x => x.name.toLowerCase() === name.addBrackets().toLowerCase())?.id;
     }
 
     return {
