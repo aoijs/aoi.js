addaf4449b6c4783ac54c47d765a12ea413b3c37
Leref <12578483+Leref@users.noreply.github.com>
2022-06-12 00:18:45 +0000

    removed(cachehandler for structures)

--- src/cachehandler/index.js
+++
@@ -1,8 +0,0 @@
-const Group = require("./group.js");
-const LimitGroup = require("./limitGroup.js");
-const SuperSet = require("./superSet.js");
-module.exports = {
-    cache: Group,
-    limitedCache: LimitGroup,
-    setCache: SuperSet,
-};
