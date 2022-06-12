d48b2fe8b1d61ed72f4cc4267dd2d6936c6c5f50
Leref <12578483+Leref@users.noreply.github.com>
2022-06-12 01:38:55 +0000

    updated more functions.

--- src/functions/Funcs/info/shardPing.js
+++ src/functions/Funcs/info/shardPing.js
@@ -1,13 +1,21 @@
-module.exports = d => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-
+  
     const [shardId = 0] = data.inside.splits;
-
-    if (isNaN(shardId)) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid ShardId Provided In');
-
-    data.result = d.client.ws.shards.get(Number(shardId))?.ping || 0;
-
+  
+    if (isNaN(shardId))
+      return d.aoiError.fnError(
+        d,
+        "custom",
+        { inside: data.inside },
+        "Invalid ShardId Provided In",
+      );
+  
+    data.result = await d.client.shard.broadcastEval((c) => c.ws.ping, {
+      shard: Number(shardId),
+    });
+  
     return {
-        code: d.util.setCode(data)
-    }
-}
+      code: d.util.setCode(data),
+    };
+  };
