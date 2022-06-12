908c6fa1c779b7da253ce4b9fa45e485448e62aa
Leref <12578483+Leref@users.noreply.github.com>
2022-06-12 02:01:05 +0000

    updated $randomUserID

--- src/functions/Funcs/util/randomUserId.js
+++ src/functions/Funcs/util/randomUserId.js
@@ -1,14 +1,17 @@
-module.exports = d => {
-    let {code, result} = d.util.aoiFunc(d);
-
-    result = d.client.users.cache.random()?.id;
-    if (!d.randoms.randomUserId) {
-        d.randoms.randomUserId = result
-    } else {
-        result = d.randoms.randomUserId;
+module.exports = (d) => {
+    const data = d.util.aoiFunc(d);
+  
+    const [guildId = "global"] = data.inside.splits;
+    if (d.randoms.randomUserId) data.result = d.randoms.randomUserId;
+    else {
+      if (guildId === "global") data.result = d.client.users.cache.random()?.id;
+      else
+        data.result = d.client.guilds.cache
+          .get(guildId)
+          ?.members.cache.random()?.id;
+      d.randoms.randomUserId = data.result;
     }
-
     return {
-        code: d.util.setCode({function: d.func, code, result})
-    }
-}
+      code: d.util.setCode(data),
+    };
+  };
