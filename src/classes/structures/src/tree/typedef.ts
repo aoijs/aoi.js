990218e4a3ff900edaa9e30bb965f112e28bc006
Leref <12578483+Leref@users.noreply.github.com>
2022-06-25 16:52:08 -0400

    cleanup structures

--- src/classes/structures/src/tree/typedef.ts
+++
@@ -1,6 +0,0 @@
-import { Branch } from "./branch";
-import { Leaf } from "./leaf";
-
-export type func<U> = (arg:any,index:number,array:any[])=> U
-export type TreeK = string;
-export type TreeV = Leaf | Branch;
