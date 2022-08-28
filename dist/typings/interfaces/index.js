3979fabf0763171abe6b8c04edbe4ec8a338d1a7
Leref <12578483+Leref@users.noreply.github.com>
2022-08-27 22:52:55 -0400

    fixed index's

---
+++ dist/typings/interfaces/index.js
@@ -0,0 +1,30 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./AoiOptions"), exports);
+__exportStar(require("./CommandData"), exports);
+__exportStar(require("./ConditionFieldData"), exports);
+__exportStar(require("./ContainerData"), exports);
+__exportStar(require("./FieldData"), exports);
+__exportStar(require("./FunctionData"), exports);
+__exportStar(require("./FunctionFieldData"), exports);
+__exportStar(require("./InteractionType"), exports);
+__exportStar(require("./InterpreterData"), exports);
+__exportStar(require("./Module"), exports);
+__exportStar(require("./PrefixOptions"), exports);
+__exportStar(require("./StatusData"), exports);
+__exportStar(require("./ThisArgData"), exports);
+//# sourceMappingURL=index.js.map
---
+++ dist/typings/types/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./Async"), exports);
+__exportStar(require("./CommandDataUnion"), exports);
+__exportStar(require("./CommandTypes"), exports);
+__exportStar(require("./ConditionFieldUnion"), exports);
+__exportStar(require("./DiscordEvent"), exports);
+__exportStar(require("./Environment"), exports);
+__exportStar(require("./FunctionExecutor"), exports);
+__exportStar(require("./FunctionFieldTypes"), exports);
+__exportStar(require("./If"), exports);
+__exportStar(require("./InterpreterDataReturn"), exports);
+__exportStar(require("./JSNativeTypes"), exports);
+__exportStar(require("./Option"), exports);
+__exportStar(require("./ReplyType"), exports);
+__exportStar(require("./Sendable"), exports);
+//# sourceMappingURL=index.js.map
