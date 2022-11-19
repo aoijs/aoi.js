ab510593270009c8e2fcb2827a02853290703ef2
Leref <12578483+Leref@users.noreply.github.com>
2022-11-19 10:20:33 -0500

    didn't commit oops.

--- src/functions/Funcs/info/userBannerColor.js
+++ src/functions/Funcs/info/userBannerColor.js
@@ -1,14 +1,13 @@
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
 
-    const [userID] = data.inside.splits;
+    const [userID = d.author?.id] = data.inside.splits;
 
     const user = await d.util.getUser(d, userID);
     if (!user) return d.aoiError.fnError(d, 'user', {inside: data.inside});
 
     if (!user.accentColor) {
-        user.fetch({force: true})
+        await user.fetch({force: true})
     }
 
     data.result = user.hexAccentColor;
