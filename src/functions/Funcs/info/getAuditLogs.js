b873d83e3ea5c300a26cfcf271a3bebf1422936f
Ayaka <user.satoshi69@gmail.com>
2022-12-30 00:56:33 +0530

    fixed targetid

--- src/functions/Funcs/info/getAuditLogs.js
+++ src/functions/Funcs/info/getAuditLogs.js
@@ -1,55 +1,56 @@
 module.exports = async (d) => {
-    const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
 
-    let [
-        limit = 5,
-        userID = d.author?.id,
-        action = "All",
-        guildID = d.guild?.id,
-        format = "{executor.username}: {target.id} - {action}",
-    ] = data.inside.splits;
+  let [
+    limit = 5,
+    userID = d.author?.id,
+    action = "All",
+    guildID = d.guild?.id,
+    format = "{executor.username}: {target.id} - {action}"
+  ] = data.inside.splits;
 
-    const guild = await d.util.getGuild(d, guildID);
-    if (!guild) return d.aoiError.fnError(d, "guild", {inside: data.inside});
+  const guild = await d.util.getGuild(d, guildID);
+  if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
 
-    limit = Number(limit);
-    if (isNaN(limit))
-        return d.aoiError.fnError(
-            d,
-            "custom",
-            {inside: data.inside},
-            "Invalid Index Provided In",
-        );
+  limit = Number(limit);
+  if (isNaN(limit))
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      { inside: data.inside },
+      "Invalid Index Provided In"
+    );
 
-    const audit = await guild
-        .fetchAuditLogs({
-            limit,
-            user: userID === "" ? undefined : userID,
-            type: action,
-        })
-        .catch((e) => {
-            d.aoiError.fnError(
-                d,
-                "custom",
-                {},
-                "Failed To Get Audit Logs With Reason: " + e,
-            );
-        });
+  const audit = await guild
+    .fetchAuditLogs({
+      limit,
+      user: userID === "" ? undefined : userID,
+      type: action
+    })
+    .catch((e) => {
+      d.aoiError.fnError(
+        d,
+        "custom",
+        {},
+        "Failed To Get Audit Logs With Reason: " + e
+      );
+    });
 
-    data.result = audit.entries
-        .map((logs) =>
-            format
-                .replaceAll(`{executor.username}`, logs.executor.username)
-                .replaceAll(`{executor.mention}`, logs.executor)
-                .replaceAll(`{executor.id}`, logs.executor.id)
-                .replaceAll(`{executor.tag}`, logs.executor.tag)
-                .replaceAll("{target.id}", logs.id)
-                .replaceAll("{action}", logs.action),
-        )
-        .join("\n");
+  data.result = audit.entries
+    .map((logs) => {
+        return format
+          .replaceAll(`{executor.username}`, logs.executor.username)
+          .replaceAll(`{executor.mention}`, logs.executor)
+          .replaceAll(`{executor.id}`, logs.executor.id)
+          .replaceAll(`{executor.tag}`, logs.executor.tag)
+          .replaceAll("{target.id}", logs.target.id)
+          .replaceAll("{action}", logs.action)
+          .replaceAll("{id}", log.id);
+      }
+    ).join("\n");
 
-    return {
-        code: d.util.setCode(data),
-    };
+  return {
+    code: d.util.setCode(data)
+  };
 };
