94706de5fe783f62b7390c9b861a53c2776aa6f6
Leref <12578483+Leref@users.noreply.github.com>
2022-12-31 00:19:34 -0500

    AoiWarning is now using undici.
     Better message and cleaned up.

--- src/handler/AoiWarning.js
+++ src/handler/AoiWarning.js
@@ -1,15 +1,16 @@
-const {default: axios} = require("axios");
+const { request } = require('undici');
+
 const json = require("../../package.json");
 module.exports = async () => {
     try {
-        const res = await axios.get("https://api.leref.repl.co/package/version"); //This will change in v6 to be native.
+        const { body } = await request("https://api.leref.repl.co/package/version");
+        const data = (await body.json()).version;
 
-        if (json.version !== res.data.version) {
+        if (json.version !== data) {
 
             console.warn(
-                "\x1b[31mAoiWarning: \u001b[33mv" +
-                res.data.version +
-                " is available to install.\u001b[0m" + " (\x1b[31mnpm i aoi.js@" + res.data.version + "\x1b[0m)")
+                "\x1b[31mAoiWarning: \u001b[33mYou are currently using an outdated version, install the latest version by" +
+                "\u001b[0m" + " (\x1b[31mnpm i aoi.js@latest\x1b[0m)")
         }
     } catch {
     }
