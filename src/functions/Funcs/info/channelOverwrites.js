62a8111caf58dc10dfccb4e603e27be7c6448637
Leref <12578483+Leref@users.noreply.github.com>
2023-01-03 13:46:10 -0500

    fixed $channelOverwrites not mentioning.

--- src/functions/Funcs/info/channelOverwrites.js
+++ src/functions/Funcs/info/channelOverwrites.js
@@ -6,7 +6,7 @@ module.exports = async d => {
     if (!channel) return d.aoiError.fnError(d, "channel", {inside: data.inside});
 
     let overwrites = [...channel.permissionOverwrites.cache.values()];
-    data.result = overwrites.map(x => response.replaceAll("{mention}", x.type === "role" ? `<@&${x.id}>` : `<@${x.id}>`).replaceAll("{type}", x.type).replaceAll("{allow}", x.allow.toArray().join(" , ")).replaceAll("{deny}", x.deny.toArray().join(" , "))).join(sep);
+    data.result = overwrites.map(x => response.replaceAll("{mention}", x.type === "role" ? `<@&${x.id}>` : `<@&${x.id}>`).replaceAll("{type}", x.type).replaceAll("{allow}", x.allow.toArray().join(" , ")).replaceAll("{deny}", x.deny.toArray().join(" , "))).join(sep);
 
     return {
         code: d.util.setCode(data)
