ab878cdfb619dcaf9471f5c789e464c7a9699981
Ayaka <user.satoshi69@gmail.com>
2023-01-04 17:06:29 +0530

    more changes

--- src/functions/Funcs/info/boostingSince.js
+++ src/functions/Funcs/info/boostingSince.js
@@ -4,7 +4,7 @@ module.exports = async d => {
     let [guildID = d.guild?.id, userID = d.author.id, format = "ms"] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildID)
-    if (d.channel.type !== d.util.channelTypes.Dm && !guild) return d.aoiError.fnError(d, "guild", {inside: data.inside});
+    if (d.channel.type !== d.util.channelTypes.DM && !guild) return d.aoiError.fnError(d, "guild", {inside: data.inside});
 
     const user = !guild ? true : await d.util.getMember(guild, userID);
     if (!user) return d.aoiError.fnError(d, "user", {inside: data.inside});
