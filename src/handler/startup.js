7e7b8ec228abf91cbae7369696ec0215c4939d4c
Leref <12578483+Leref@users.noreply.github.com>
2023-02-10 22:14:02 -0500

    Cleaner Startup.

--- src/handler/startup.js
+++ src/handler/startup.js
@@ -3,28 +3,22 @@ const Interpreter = require("../interpreter.js");
 
 module.exports = async (client) => {
     if (client.aoiOptions.aoiLogs !== false) {
-        console.log(
-            "Initialized on \x1b[36maoi.js \x1b[32mv" +
-            require("../../package.json").version +
-            `\x1b[0m || \x1b[36m${client.user.tag}` +
-            "\x1b[0m",
-        );
-        console.log(
-            "Official Support Server: https://discord.gg/HMUfMXDQsV",
-        );
+        console.warn('\x1b[36maoi.js:\x1B[0m Initializing on \x1B[32mv' + require('../../package.json').version + '\x1B[0m')
+        console.warn(
+            '\x1b[33mSupport:\x1B[0m Discord Server: https://discord.gg/HMUfMXDQsV')
     }
 
     await require("./AoiWarning.js")(client);
 
     await require("./custom/timeout.js")(
-        { client, interpreter: Interpreter },
+        {client, interpreter: Interpreter},
         undefined,
         undefined,
         undefined,
         true,
     );
     await require("./custom/timeoutPulse.js")(
-        { client, interpreter: Interpreter },
+        {client, interpreter: Interpreter},
         undefined,
         undefined,
         undefined,
