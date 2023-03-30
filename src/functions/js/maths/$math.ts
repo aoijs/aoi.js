f7b4d35f0b4bf470bf32a21a1d4969483922167e
Ayaka <user.satoshi69@gmail.com>
2023-03-30 22:56:54 +0530

    shorten $math.js

--- src/functions/js/maths/$math.ts
+++ src/functions/js/maths/$math.ts
@@ -106,50 +106,8 @@ export const $math: FunctionData = {
                 `${data.name} requires a valid math expression`,
             );
         }
-        let math = numbers
-            ?.replaceAll("sin", "Math.sin")
-            .replaceAll("cos", "Math.cos")
-            .replaceAll("tan", "Math.tan")
-            .replaceAll("asin", "Math.asin")
-            .replaceAll("acos", "Math.acos")
-            .replaceAll("atan", "Math.atan")
-            .replaceAll("atan2", "Math.atan2")
-            .replaceAll("sinh", "Math.sinh")
-            .replaceAll("cosh", "Math.cosh")
-            .replaceAll("tanh", "Math.tanh")
-            .replaceAll("asinh", "Math.asinh")
-            .replaceAll("acosh", "Math.acosh")
-            .replaceAll("atanh", "Math.atanh")
-            .replaceAll("exp", "Math.exp")
-            .replaceAll("log", "Math.log")
-            .replaceAll("log10", "Math.log10")
-            .replaceAll("log2", "Math.log2")
-            .replaceAll("log1p", "Math.log1p")
-            .replaceAll("expm1", "Math.expm1")
-            .replaceAll("cbrt", "Math.cbrt")
-            .replaceAll("hypot", "Math.hypot")
-            .replaceAll("pow", "Math.pow")
-            .replaceAll("sqrt", "Math.sqrt")
-            .replaceAll("ceil", "Math.ceil")
-            .replaceAll("floor", "Math.floor")
-            .replaceAll("round", "Math.round")
-            .replaceAll("sign", "Math.sign")
-            .replaceAll("trunc", "Math.trunc")
-            .replaceAll("fround", "Math.fround")
-            .replaceAll("abs", "Math.abs")
-            .replaceAll("max", "Math.max")
-            .replaceAll("min", "Math.min")
-            .replaceAll("random", "Math.random")
-            .replaceAll("EULERNUM", "Math.E")
-            .replaceAll("LN10", "Math.LN10")
-            .replaceAll("LN2", "Math.LN2")
-            .replaceAll("LOG10E", "Math.LOG10E")
-            .replaceAll("LOG2E", "Math.LOG2E")
-            .replaceAll("PI", "Math.PI")
-            .replaceAll("SQRT1_2", "Math.SQRT1_2")
-            .replaceAll("SQRT2", "Math.SQRT2")
-            .replaceAll("clz32", "Math.clz32")
-            .replaceAll("imul", "Math.imul");
+        const regex = /(abs|acos|acosh|asin|asinh|atan|atan2|atanh|cbrt|ceil|clz32|cos|cosh|exp|expm1|floor|fround|hypot|imul|log|log10|log1p|log2|max|min|pow|random|round|sign|sin|sinh|sqrt|tan|tanh|trunc|LN10|LN2|LOG10E|LOG2E|PI|SQRT1_2|SQRT2)/g;
+        let math = numbers?.replaceAll(regex, "Math.$1").replaceAll("EULERNUM", "Math.E");
 
         const res = escapeMathResult(`(${math})`);
         currentScope.update(res, data);
