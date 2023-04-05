e6c71c366413e7d95e0ba446021a06f9d577144b
Leref <Leref@users.noreply.github.com>
2023-04-05 11:17:35 -0400

    shorten index.js

--- src/index.js
+++ src/index.js
@@ -1,37 +1,19 @@
-const Client = require("./classes/AoiClient");
+const AoiClient = require("./classes/AoiClient");
 const CustomEvent = require("./classes/NewEvent.js");
 const LoadCommands = require("./classes/LoadCommands.js");
 const ClientShard = require("./classes/ClientShard.js");
 const AoiError = require("./classes/AoiError.js");
 const Util = require("./classes/Util.js");
 
+/**
+ * aoiclient, custom events, command loader, client shard, error handling and utility functions
+ * @namespace
+ */
 module.exports = {
-  /**
-   * * The Discord Bot Client
-   * @param {!Object} options The options to use Discord as a Bot
-   * @example new Bot({
-   * 				token:"Discord Bot Token",
-   * 				prefix:"!",
-   * 				intents : ['GUILDS','GUILD_MESSAGES']
-   * 			})
-   */
-  AoiClient: Client,
-  /**
-   * *  Custom Events Class
-   * @example new CustomEvent(client (eg: bot))
-   **/
+  AoiClient,
   CustomEvent,
-  /**
-     * * loadCommands
-     * loads the commands of the provided file
-     @example new LoadCommands(path,true/false)
-     **/
   LoadCommands,
   ClientShard,
-  // * AoiError Class
   AoiError,
-  // * Util Class
   Util,
 };
-
-/*Copyright © 2021 @Akarui Development*/
