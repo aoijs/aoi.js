08898f68da23f419a1681df5a01dd5c6b392d9a5
Leref <Leref@users.noreply.github.com>
2023-04-05 11:11:29 -0400

    update(AoiReader)

--- src/functions/AoiReader.js
+++ src/functions/AoiReader.js
@@ -1,18 +1,17 @@
 const fs = require("fs");
-const {Group} = require("../classes/structures/dist");
+const path = require("path");
+const { Group } = require("../classes/structures/dist");
+
 const maps = {};
 const grp = new Group();
+
 const functions = fs
-    .readdirSync(__dirname + "/Funcs")
-    .map((x) => fs.readdirSync(__dirname + `/Funcs/${x}`))
-    .map((x) => x.join(","))
-    .join(",")
-    .split(",")
-    .map((x) => "$" + x.split(".js")[0]);
+  .readdirSync(path.join(__dirname, "Funcs"))
+  .map((dir) => {
+    const files = fs.readdirSync(path.join(__dirname, "Funcs", dir));
+    maps[dir] = files.map((file) => file.split(".js")[0]);
+    return files.map((file) => "$" + file.split(".js")[0]);
+  })
+  .reduce((acc, val) => acc.concat(val), []);
 
-fs.readdirSync(__dirname + "/Funcs").map((x) => {
-    const filelist = fs.readdirSync(__dirname + "/Funcs/" + x);
-    maps[x] = filelist.map((y) => y.split(".js")[0]);
-});
-
-module.exports = {functions, maps, grp};
+module.exports = { functions, maps: Object.values(maps), grp };
