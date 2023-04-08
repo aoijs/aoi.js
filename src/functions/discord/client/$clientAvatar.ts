a4e3a392d800399763e5c8227a06419871b7f85a
Neo <neoaichan@gmail.com>
2023-04-08 22:31:18 +0000

    Created `$clientAvatar[]` function.

---
+++ src/functions/discord/client/$clientAvatar.ts
@@ -0,0 +1,53 @@
+import { Scope } from "../../../index.js";
+import { FunctionData, funcData } from "../../../typings/interfaces.js";
+import { escapeResult } from "../../../util/transpilerHelpers.js";
+
+export const $clientAvatar: FunctionData = {
+    name: "$clientAvatar",
+    type: "getter",
+    brackets: true,
+    optional: false,
+    fields: [
+        {
+            name: "size",
+            type: "number",
+            required: false,
+        },
+        {
+            name: "format",
+            type: "string",
+            required: false,
+        },
+        {
+            name: "dynamic",
+            type: "boolean",
+            required: false,
+        },
+    ],
+    version: "7.0.0",
+    default: ["void", "void", "void"],
+    returns: "string",
+    description: "Returns the Avatar of client",
+    code: (data: funcData, scope: Scope[]) => {
+        // Adding default values
+        const [size = 4096, dynamic = "true", format = "png"] = data.splits;
+
+        // Getting the current scope
+        const currentScope = scope[scope.length - 1];
+
+        // Getting the client avatar
+        const clientAvatar = `__$DISCORD_DATA$__.client.AvatarURL({format: ${format}, dynamic: ${dynamic}, size: ${size}})`;
+
+        // Returning the result
+        const res = escapeResult(`(${clientAvatar})`);
+
+        // Updating the scope
+        currentScope.update(res, data);
+
+        // Returning the result
+        return {
+            code: res,
+            scope,
+        };
+    },
+};
