0c460c07cd7e517ad10ce920b282b94c31ab2a87
Ayaka <user.satoshi69@gmail.com>
2023-04-14 23:44:02 +0530

    fixed path for esm builds

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { AoiClient } from "./structures/AoiClient.js";
-export * from "./core";
+export * from "./core/index.js";
 export * from "./typings/interfaces.js";
 import functions from "./functions/index.js";
 export { AoiClient, functions };
