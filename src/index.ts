65f52c65d0af24bb296ce57dfc09706b625cd4a2
USERSATOSHI <user.satoshi69@gmail.com>
2023-06-22 21:52:13 +0530

    exported more managers

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ export * from "./core/index.js";
 export * from "./typings/interfaces.js";
 import functions from "./functions/index.js";
 import { defaultCacheConfig } from "./util/DapiHelper.js";
+export * from "./manager/Command.js";
+export * from "./manager/Function.js";
+export * from "./manager/Plugin.js";
 export { AoiClient, functions,defaultCacheConfig };
