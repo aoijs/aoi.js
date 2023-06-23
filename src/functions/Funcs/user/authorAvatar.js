47ef37d8fb7d587d1716e9f3f73bb78c508f6a27
ethann2004 <117716577+ethann2004@users.noreply.github.com>
2023-06-23 16:07:09 -0700

    Format was changed to extension in discord.js v14

--- src/functions/Funcs/user/authorAvatar.js
+++ src/functions/Funcs/user/authorAvatar.js
@@ -1,8 +1,8 @@
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
-    const [size = 4096, dynamic = "true", format = "webp"] = data.inside.splits;
+    const [size = 4096, dynamic = "true", extension = "webp"] = data.inside.splits;
 
-    data.result = d.author?.displayAvatarURL({size: Number(size), dynamic: dynamic === 'true', format})
+    data.result = d.author?.displayAvatarURL({size: Number(size), dynamic: dynamic === 'true', extension})
     return {
         code: d.util.setCode(data)
     }
