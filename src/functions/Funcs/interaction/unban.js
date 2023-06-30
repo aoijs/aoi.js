46e952320fa298ae7f6f77287cd3b0d684cebb65
Leref <12578483+Leref@users.noreply.github.com>
2023-06-30 19:10:58 -0400

    $unban updated / added reason

--- src/functions/Funcs/interaction/unban.js
+++ src/functions/Funcs/interaction/unban.js
@@ -1,23 +1,17 @@
-module.exports = async (d) => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    const err = d.inside(inside);
-    if (err) return d.error(err);
-    let [guildID = d.guild?.id, userID] = inside.splits;
+module.exports = async d => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    let [guildID = d.guild?.id, userID, reason] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildID);
-    if (!guild) return d.aoiError.fnError(d, "guild", {inside: inside});
+    if (!guild) return d.aoiError.fnError(d, "guild", {inside: data.inside});
 
-    guild.members.unban(userID).catch((err) => {
-        d.aoiError.fnError(
-            d,
-            "custom",
-            {},
-            `Failed To Unban ${userID} With Reason: ` + err,
-        );
+    guild.members.unban(userID, reason).catch((err) => {
+        d.aoiError.fnError(d, 'custom', {}, 'Failed To Unban With Reason: ' + err)
     });
 
     return {
-        code: d.util.setCode({function: d.func, code, inside}),
+        code: d.util.setCode(data)
     };
 };
