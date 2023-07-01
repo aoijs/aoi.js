023aa497fe26b5ab0ed5d219415e950d85955c93
Leref <12578483+Leref@users.noreply.github.com>
2023-06-30 23:27:34 -0400

    new aoi startup

--- src/handler/AoiStart.js
+++ src/handler/AoiStart.js
@@ -2,9 +2,15 @@ const Interpreter = require("../interpreter.js");
 
 module.exports = async (client) => {
     if (client.aoiOptions.aoiLogs !== false) {
-        console.warn('\x1b[36maoi.js:\x1B[0m Initializing on \x1B[32mv' + require('../../package.json').version + '\x1B[0m')
-        console.warn(
-            '\x1b[33mSupport:\x1B[0m Discord Server: https://discord.gg/HMUfMXDQsV')
+        const version = require('../../package.json').version;
+        const line = '═\x1b[36m═\x1b[0m'.repeat(18);
+        const topBorder = `╔${line}╗`;
+
+        console.log(`\n\x1b[36m${topBorder}\x1b[0m`);
+        console.log(`\n\x1b[1mInitializing on \x1b[36maoi.js\x1b[0m`);
+        console.log(`\x1b[1mInstalled on \x1b[32mv${version}\x1b[0m`);
+        console.log(`\x1b[1mDiscord Server: \x1b[32mhttps://discord.gg/HMUfMXDQsV\x1b[0m`);
+        console.log(`\n\x1b[36m╚${line}╝\x1b[0m`);
     }
 
     if (client.aoiOptions.aoiWarning !== false) {
