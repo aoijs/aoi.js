e72a269ae61f2d93814e8988e82f20ba235993c2
Leref <12578483+Leref@users.noreply.github.com>
2023-07-13 15:44:02 -0400

    updated $addThreadMember

--- src/functions/guild/addThreadMember.js
+++ src/functions/guild/addThreadMember.js
@@ -1,18 +1,20 @@
 module.exports = async (d) => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    const err = d.inside(inside);
-    if (err) return d.error(err);
-    let [channelID, threadId, userID, reason] = inside.splits;
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    let [channelID, threadID, userID, reason] = data.inside.splits;
     const channel = await d.util.getChannel(d, channelID);
-    if (!channel) d.aoiError.fnError(d, "channel", {inside});
-    const thread = channel.threads.cache.get(threadId);
+
+    if (!channel) d.aoiError.fnError(d, "channel", {inside: data.inside});
+    const thread = channel.threads.cache.get(threadID);
+
     if (!thread)
-        d.aoiError.fnError(d, "custom", {inside}, "Invalid ThreadId Provided In");
+        d.aoiError.fnError(d, "custom", {inside: data.inside}, "Invalid ThreadID Provided In");
     await thread.members
         .add(userID, reason)
         .catch((err) => d.aoiError.fnError(d, "custom", {}, err.message));
+
     return {
-        code: d.util.setCode({function: d.func, inside, code}),
+        code: d.util.setCode(data),
     };
 };
