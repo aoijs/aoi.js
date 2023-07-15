08eef769bdc9045c1f6536f40cf118058fcfa198
Leref <12578483+Leref@users.noreply.github.com>
2023-07-15 19:32:12 -0400

    Added $clientPrefix

---
+++ src/functions/client/clientPrefix.js
@@ -0,0 +1,13 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    if (Array.isArray(d.client.prefix)) {
+        data.result = d.client.prefix.join(" , ");
+    } else {
+        data.result = d.client.prefix;
+    }
+
+    return {
+        code: d.util.setCode(data),
+    };
+};
