65f64caa5685e7e8c8eca05648a854e250161e37
Leref <12578483+Leref@users.noreply.github.com>
2023-07-18 12:09:46 -0400

    Fixed $isUnicodeEmoji to regex

--- src/functions/util/isUnicodeEmoji.js
+++ src/functions/util/isUnicodeEmoji.js
@@ -1,12 +1,13 @@
-module.exports = async d => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
     const [emoji] = data.inside.splits;
 
-    data.result = /\p{Extended_Pictographic}/u.test(emoji.addBrackets());
+    const extendedPictographicRegex = /[\u{1F300}-\u{1F5FF}\u{1F900}-\u{1F9FF}\u{1FA70}-\u{1FAFF}\u{2600}-\u{27BF}\u{1F680}-\u{1F6FF}\u{24C2}-\u{1F251}]/u;
+    data.result = extendedPictographicRegex.test(emoji);
 
     return {
         code: d.util.setCode(data)
-    }
-}
+    };
+};
