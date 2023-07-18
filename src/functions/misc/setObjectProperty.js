683547abcd3b510ba2b2062aee7ffe1ff0ab9235
Fafa <87046111+Faf4a@users.noreply.github.com>
2023-07-18 20:46:13 +0200

    updated setObjectProperty

--- src/functions/misc/setObjectProperty.js
+++ src/functions/misc/setObjectProperty.js
@@ -1,20 +1,24 @@
-module.exports = async d => {
-    const {code} = d.command
-    const inside = d.unpack()
-    const err = d.inside(inside)
-    if (err) return d.error(err)
-    let [name, value] = inside.splits;
-    if (!d.object) return d.aoiError.fnError(d, "custom", {}, "object")
-    try {
-        value = JSON.parse(value)
-    } catch (e) {
-        value = value
-    }
-    const object = d.object
-    eval(`object.${name} = value`)
+module.exports = async (d) => {
+  const data = d.util.aoiFunc(d);
+  const { code } = d.command;
 
-    return {
-        code: d.util.setCode({function: d.func, inside, code}),
-        data: { ...d.data, object: object },
-    }
-}
+  const [objectName, propertyName, propertyValue] = data.inside.splits;
+
+  if (!d.data.objects) return d.aoiError.fnError(d, "custom", {}, "Object");
+
+  const object = d.data.objects[objectName] || {};
+
+  try {
+    object[propertyName] = JSON.parse(propertyValue);
+  } catch (e) {
+    object[propertyName] = propertyValue;
+  }
+
+  d.data.objects[objectName] = object;
+  d.object = object;
+
+  return {
+    code: d.util.setCode({ function: d.func, code: code, inside: data.inside }),
+    data: { ...d.data, objects: { ...d.data.objects } },
+  };
+};
