e8d8bd0e2536634efc934b306c62c880afc819e5
Fafa <87046111+Faf4a@users.noreply.github.com>
2023-07-19 03:14:42 +0200

    fixed `$getLeaderboardInfo` (?)

--- src/functions/variables/getLeaderboardInfo.js
+++ src/functions/variables/getLeaderboardInfo.js
@@ -2,14 +2,35 @@ module.exports = async (d) => {
   const data = d.util.aoiFunc(d);
   if (data.err) return d.error(data.err);
 
-  const [variable, id, type, table = d.client.db.tables[0]] =
-    data.inside.splits;
+  const [variable, id, type = "guild", format, table = d.client.db.tables[0]] = data.inside.splits;
+
+  let key = null;
+  let cache = null;
+  let user = null;
 
   const all = await d.client.db.all(table, variable);
 
-  const key = `${variable.addBrackets()}_${`${id}_${
-    d.guild?.id === undefined ? "dm" : d.guild?.id
-  }`}`;
+  if (type === "guild") {
+    key = `${variable.addBrackets()}_${`${id}_${
+      d.guild?.id === undefined ? "dm" : d.guild?.id
+    }`}`;
+    cache = await d.client.guilds.cache.get(id);
+    user =
+      typeof cache === "undefined" || Object.keys(cache).length === 0
+        ? await d.client.users.fetch(id)
+        : undefined;
+  } else if (type === "global") {
+    key = `${variable.addBrackets()}_${id}`;
+    cache = await d.client.guilds.cache.get(id);
+    user =
+      typeof cache === "undefined" || Object.keys(cache).length === 0
+        ? await d.client.users.fetch(id)
+        : undefined;
+  } else if (type === "message" || type === "channel") {
+    key = `${variable.addBrackets()}_${id}`;
+  } else {
+    d.aoiError.fnError(d, "custom", { inside: data.inside }, `type`);
+  }
 
   const foundData = all.find((x) => x.key === key);
   const foundDataMap = all
@@ -17,24 +38,18 @@ module.exports = async (d) => {
     .sort((a, b) => a.value - b.value);
   let value = foundData?.value ?? 0;
 
-  const cache = await d.client.guilds.cache.get(id);
-  const user =
-    typeof cache === "undefined" || Object.keys(cache).length === 0
-      ? await d.client.users.fetch(id)
-      : undefined;
-
-  switch (type) {
+  switch (format) {
     case "top":
-      data.result = foundDataMap.find((x) => x.key === key).pos;
+      data.result = foundDataMap.find((x) => x.key === key)?.pos || null;
       break;
     case "value":
       data.result = value;
       break;
     case "tag":
-      data.result = user.tag;
+      data.result = user.tag || null;
       break;
     case "username":
-      data.result = user.username;
+      data.result = user.username || null;
       break;
     default:
       data.result = null;
