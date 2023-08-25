a6a7e47d9e32691ad0f572414da7b15c497571c4
ethann2004 <117716577+ethann2004@users.noreply.github.com>
2023-08-25 01:02:51 -0700

    Changed dynamic, format is now extension in authorBanner

--- src/functions/user/authorBanner.js
+++ src/functions/user/authorBanner.js
@@ -1,18 +1,18 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
 
-    const [size = "4096", dynamic = "true", format = "webp"] = data.inside.splits;
+    const [size = 4096, dynamic = 'true', extension] = data.inside.splits;
 
     if (!d.author?.banner) {
-        await d.author?.fetch();
+        await d.author?.fetch({force: true});
     }
 
     if (!d.author.banner) data.result = "null";
     else {
         data.result = d.author?.bannerURL({
             size: Number(size),
-            dynamic: dynamic === "true",
-            format,
+            forceStatic: dynamic === 'false',
+            extension,
         });
     }
 
