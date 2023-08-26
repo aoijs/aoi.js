c29f46907cc1bc0211d6b2163420625016d5dfdd
Leref <12578483+Leref@users.noreply.github.com>
2023-08-26 18:42:29 -0400

    Client status changes
    Now support Custom Status / No longer case-sensitive, as well updated with $setStatus function.
    
    `text` is now `name` for better reference.

--- src/handler/status.js
+++ src/handler/status.js
@@ -1,8 +1,12 @@
-module.exports = (statuses, client) => {
+const createCustomBoxedMessage = require('../utils/CustomBox.js'); // Adjust the path accordingly
+
+let recommendationLogged = false;
+
+module.exports = async (statuses, client) => {
   if (statuses.size !== 0) {
     let y = 0;
-
     let status = statuses.allValues();
+
     const f = async () => {
       if (!status[y]) {
         y = 0;
@@ -14,18 +18,33 @@ module.exports = (statuses, client) => {
         stats.activity.type = status[y].activity.type;
         stats.activity.url = status[y].activity.url;
 
-        stats.activity.name = status[y].activity.name.includes("$")
-          ? (
+        if (status[y].activity.name && status[y].activity.name.includes("$")) {
+          stats.activity.name = (
               await client.functionManager.interpreter(
-                client,
-                {},
-                [],
-                { code: status[y].activity.name },
-                client.db,
-                true,
+                  client,
+                  {},
+                  [],
+                  { code: status[y].activity.name },
+                  client.db,
+                  true,
               )
-            )?.code
-          : status[y].activity.name;
+          )?.code;
+        } else {
+          if (!recommendationLogged) {
+            const recommendationMessage = `Provide a name for status[${y}]`;
+
+            // Include 'AoiWarning' as the title for this message
+            createCustomBoxedMessage([{ text: recommendationMessage, textColor: 'red' }], 'white', {
+              text: 'AoiWarning',
+              textColor: 'yellow',
+            });
+            recommendationLogged = true;
+          }
+
+          // Use the provided name or a default value
+          stats.activity.name = status[y].activity.name || 'Using aoi.js';
+        }
+
         client.user.setPresence({
           status: status[y].status,
           activities: [stats.activity],
