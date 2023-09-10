722d7509f960978b0b974a0d6bc4f8cee9535925
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-09-10 11:20:45 +0200

    Update getAutomodRuleId.js

--- src/functions/guild/getAutomodRuleId.js
+++ src/functions/guild/getAutomodRuleId.js
@@ -1,8 +1,8 @@
 module.exports = async(d) => {
     const data = d.util.aoiFunc(d);
-    const [guildID, automodRuleName, force = true] = data.inside.splits;
+    const [guildID = d.guild?.id, automodRuleName, force = true] = data.inside.splits;
 
-    const guild = await d.client.guilds.fetch(guildID);
+    const guild = await d.util.getGuild(d, guildID);
 
     const autoModerationRules = await guild.autoModerationRules.fetch({ force: force, cache: force === false ? true : false });
     const result = autoModerationRules.find(rule => rule.name === automodRuleName || rule.id === automodRuleName);
