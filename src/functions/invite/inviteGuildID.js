46a5e1c2e1fa5c5502b83bd38a3de044d90f4c38
Leref <12578483+Leref@users.noreply.github.com>
2023-09-11 22:07:24 -0400

    wrong invite functions deletion

---
+++ src/functions/invite/inviteGuildID.js
@@ -0,0 +1,9 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    data.result = d.data.inviteData.guildId;
+
+    return {
+        code: d.util.setCode(data),
+    };
+};
---
+++ src/functions/invite/inviteMaxUses.js
@@ -0,0 +1,9 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    data.result = d.data.inviteData.maxUses;
+
+    return {
+        code: d.util.setCode(data),
+    };
+};
