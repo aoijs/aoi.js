50375be86dd26624d186b7fdb1b460f063abd941
Leref <12578483+Leref@users.noreply.github.com>
2023-09-16 21:11:47 -0400

    updated timeout functions

--- src/handler/Custom/timeout.js
+++ src/handler/Custom/timeout.js
@@ -1,53 +1,35 @@
-const AoiError = require("../../classes/AoiError.js");
-const {
-  AoijsAPI,
-  CustomDb,
-  Promisify
-} = require("../../classes/Database.js");
+const { AoijsAPI } = require("../../classes/Database.js");
 
 module.exports = async (d, name, duration, timeoutData, onReady) => {
   let cmds = d.client.cmd.timeout.allValues();
   if (onReady) {
     const datas = await d.client.db.all(d.client.db.tables[0], "setTimeout");
-    try {
-      for (const data of datas.filter((x) => {
-        if (d.client.db instanceof AoijsAPI) {
-          if (d.client.db.type === "aoi.db") return !x.value.__pulseEvery__;
-          else return !x.data.value.__pulseEvery__;
-        } else if (
-          d.client.db instanceof CustomDb ||
-          d.client.db instanceof Promisify
-        ) {
-          return (
+    for (const data of datas.filter((x) => {
+      if (d.client.db instanceof AoijsAPI) {
+        return !x.value.__pulseEvery__;
+      } else {
+        return (
             !x?.value?.__pulseEvery__ &&
-            !x["setTimeout"]?.__pulseEvery__ &&
             (typeof x.data === "object"
-              ? !x.data.value?.__pulseEvery__
-              : !x.data?.__pulseEvery__)
-          );
-        }
-      })) {
-        let t;
+                ? !x.data.value?.__pulseEvery__
+                : !x.data?.__pulseEvery__)
+        );
+      }
+    })) {
+      let t;
 
-        if (d.client.db instanceof AoijsAPI) {
-          if(d.client.db.type === "aoi.db") t = data.value;
-          else t = data.data.value;
-        } else if (
-          d.client.db instanceof CustomDb ||
-          d.client.db instanceof Promisify
-        ) {
-          t =
-            data?.value ||
-            data["setTimeout"] ||
-            (typeof data.data === "object" ? data.data.value : data.data);
-        }
-        t.__timeoutIds__ = [];
-        const dura = t.__duration__ - Date.now();
-        cmds = cmds.filter((x) => x.name === t.__timeoutName__);
-        if (dura > 0) {
-          for (const cmd of cmds) {
-            const timeout = setTimeout(async () => {
-              await d.interpreter(
+      if (d.client.db instanceof AoijsAPI) {
+        t = data.value;
+      } else {
+        t = data?.value || (typeof data.data === "object" ? data.data.value : data.data);
+      }
+      t.__timeoutIds__ = [];
+      const dura = t.__duration__ - Date.now();
+      cmds = cmds.filter((x) => x.name === t.__timeoutName__);
+      if (dura > 0) {
+        for (const cmd of cmds) {
+          const timeout = setTimeout(async () => {
+            await d.interpreter(
                 d.client,
                 {},
                 [],
@@ -56,36 +38,29 @@ module.exports = async (d, name, duration, timeoutData, onReady) => {
                 false,
                 undefined,
                 { timeoutData: t },
-              );
-              t.__timeoutIds__.push(timeout[Symbol.toPrimitive]());
+            );
+            t.__timeoutIds__.push(timeout[Symbol.toPrimitive]());
 
-              await d.client.db.delete(d.client.db.tables[0], data.key);
-            }, dura);
-          }
-          d.client.db.set(d.client.db.tables[0], "setTimeout", t.__id__, t);
-        } else {
-          for(const cmd of cmds) {
-          await d.interpreter(
-            d.client,
-            {},
-            [],
-            cmd,
-            d.client.db,
-            false,
-            undefined,
-            { timeoutData: t },
-          );
-          }
-          await d.client.db.delete(d.client.db.tables[0], data.key);
-          continue;
+            await d.client.db.delete(d.client.db.tables[0], data.key);
+          }, dura);
         }
+        d.client.db.set(d.client.db.tables[0], "setTimeout", t.__id__, t);
+      } else {
+        for (const cmd of cmds) {
+          await d.interpreter(
+              d.client,
+              {},
+              [],
+              cmd,
+              d.client.db,
+              false,
+              undefined,
+              { timeoutData: t },
+          );
+        }
+        await d.client.db.delete(d.client.db.tables[0], data.key);
+        continue;
       }
-    } catch (e) {
-      AoiError.consoleError(
-        "DatabaseSupportError",
-        "Database Not Supported , You Can Create An Issue At aoi.js Github. \n Error : " +e,
-      );
-      console.log("Link : https://github.com/aoijs/aoi.js");
     }
   } else {
     timeoutData.__timeoutIds__ = [];
@@ -96,30 +71,30 @@ module.exports = async (d, name, duration, timeoutData, onReady) => {
     for (const cmd of cmds) {
       const timeout = setTimeout(async () => {
         await d.interpreter(
-          d.client,
-          {},
-          [],
-          cmd,
-          d.client.db,
-          false,
-          undefined,
-          { timeoutData },
+            d.client,
+            {},
+            [],
+            cmd,
+            d.client.db,
+            false,
+            undefined,
+            { timeoutData },
         );
 
         d.client.db.delete(
-          d.client.db.tables[0],
-          "setTimeout",
-          timeoutData.__id__,
+            d.client.db.tables[0],
+            "setTimeout",
+            timeoutData.__id__,
         );
       }, duration);
       ids.push(timeout[Symbol.toPrimitive]());
     }
     timeoutData.__timeoutIds__ = ids;
     await d.client.db.set(
-      d.client.db.tables[0],
-      "setTimeout",
-      timeoutData.__id__,
-      timeoutData,
+        d.client.db.tables[0],
+        "setTimeout",
+        timeoutData.__id__,
+        timeoutData,
     );
   }
 };
--- src/handler/Custom/timeoutPulse.js
+++ src/handler/Custom/timeoutPulse.js
@@ -1,54 +1,27 @@
-const AoiError = require("../../classes/AoiError.js");
-const {
-  AoijsAPI,
-  CustomDb,
-  Promisify
-} = require("../../classes/Database.js");
+const { AoijsAPI } = require("../../classes/Database.js");
 
 module.exports = async (d, name, duration, pulse, timeoutData, onReady) => {
   let cmds = d.client.cmd.pulse.allValues();
 
   if (onReady) {
     const datas = await d.client.db.all(d.client.db.tables[0], "setTimeout");
-    try {
-      for (const data of datas.filter((x) => {
-        if (d.client.db instanceof AoijsAPI) {
-          if (d.client.db.type === "aoi.db") return x.value.__pulseEvery__;
-          else return x.data.value.__pulseEvery__;
-        } else if (
-          d.client.db instanceof CustomDb ||
-          d.client.db instanceof Promisify
-        ) {
-          return (
-            x?.value?.__pulseEvery__ &&
-            x["setTimeout"]?.__pulseEvery__ &&
-            (typeof x.data === "object"
-              ? x.data.value?.__pulseEvery__
-              : x.data?.__pulseEvery__)
-          );
-        }
-      })) {
-        let t;
+    for (const data of datas.filter((x) => x.value?.__pulseEvery__)) {
+      let t;
 
-        if (d.client.db instanceof AoijsAPI) {
-          if (d.client.db.type === "aoi.db") t = data.value;
-          else t = data.data.value;
-        } else if (
-          d.client.db instanceof CustomDb ||
-          d.client.db instanceof Promisify
-        ) {
-          t =
-            data?.value ||
-            data["setTimeout"] ||
-            (typeof data.data === "object" ? data.data.value : data.data);
-        }
-        t.__timeoutIds__ = [];
-        t.__pulseIds__ = [];
-        cmds = cmds.filter((x) => x.name === t.__timeoutName__);
-        if ((t.__duration__ - Date.now()) > 0) {
-          for (const cmd of cmds) {
-            const interval = setInterval(async () => {
-              await d.interpreter(
+      if (d.client.db instanceof AoijsAPI) {
+        t = data.value;
+      } else {
+        t = data.data.value;
+      }
+
+      t.__timeoutIds__ = [];
+      t.__pulseIds__ = [];
+      cmds = cmds.filter((x) => x.name === t.__timeoutName__);
+
+      if (t.__duration__ - Date.now() > 0) {
+        for (const cmd of cmds) {
+          const interval = setInterval(async () => {
+            await d.interpreter(
                 d.client,
                 {},
                 [],
@@ -56,53 +29,53 @@ module.exports = async (d, name, duration, pulse, timeoutData, onReady) => {
                 d.client.db,
                 false,
                 undefined,
-                { timeoutData: t },
-              );
-            }, t.__pulseEvery__);
-            t.__pulseIds__.push(interval[Symbol.toPrimitive]());
+                { timeoutData: t }
+            );
+          }, t.__pulseEvery__);
 
-            const timeout = setTimeout(async () => {
-              clearInterval(interval);
-              await d.client.db.delete(d.client.db.tables[0], data.key);
-            }, t.__duration__ - Date.now());
-            t.__timeoutIds__.push(timeout[Symbol.toPrimitive]());
-          }
-          d.client.db.set(d.client.db.tables[0], "setTimeout", t.__id__, t);
-        } else {
-          await d.client.db.delete(d.client.db.tables[0], data.key).catch(console.error);
+          t.__pulseIds__.push(interval[Symbol.toPrimitive()]());
+
+          const timeout = setTimeout(async () => {
+            clearInterval(interval);
+            await d.client.db.delete(d.client.db.tables[0], data.key);
+          }, t.__duration__ - Date.now());
+
+          t.__timeoutIds__.push(timeout[Symbol.toPrimitive()]());
         }
+        d.client.db.set(d.client.db.tables[0], "setTimeout", t.__id__, t);
+      } else {
+        await d.client.db.delete(d.client.db.tables[0], data.key).catch(console.error);
       }
-    } catch (e) {
-      console.error(e)
-      AoiError.consoleError(
-        "DatabaseSupportError",
-        "Database Not Supported , You Can Create An Issue In Aoi.js Github.\n Error ->" + e,
-      );
-      console.log("Link : https://github.com/aoijs/aoi.js");
     }
   } else {
     if (name) {
       cmds = cmds.filter((x) => x.name === name);
     }
+
     timeoutData.__timeoutIds__ = [];
     timeoutData.__pulseIds__ = [];
+
     for (const cmd of cmds) {
       const interval = setInterval(() => {
         d.interpreter(d.client, {}, [], cmd, d.client.db, false, undefined, {
           timeoutData,
         });
       }, pulse || duration);
-        timeoutData.__pulseIds__.push(interval[Symbol.toPrimitive]());
-      const timeout = setTimeout(async() => {
+
+      timeoutData.__pulseIds__.push(interval[Symbol.toPrimitive()]());
+
+      const timeout = setTimeout(async () => {
         clearInterval(interval);
         await d.client.db.delete(
-          d.client.db.tables[0],
-          "setTimeout",
-          timeoutData.__id__,
+            d.client.db.tables[0],
+            "setTimeout",
+            timeoutData.__id__
         );
       }, duration);
-        timeoutData.__timeoutIds__.push(timeout[Symbol.toPrimitive]());
+
+      timeoutData.__timeoutIds__.push(timeout[Symbol.toPrimitive()]());
     }
+
     d.client.db.set(d.client.db.tables[0], "setTimeout", timeoutData.__id__, timeoutData);
   }
 };
