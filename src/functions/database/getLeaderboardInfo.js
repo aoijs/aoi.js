4f53f284d3e2467fe50b55827c8da3342e03e963
Faf4a <fesserfabrice@gmail.com>
2023-09-17 05:18:49 +0200

    fix: getleaderboardinfo

--- src/functions/database/getLeaderboardInfo.js
+++ src/functions/database/getLeaderboardInfo.js
@@ -1,61 +1,67 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
-
-    const [variable, id, type = "guild", format, table = d.client.db.tables[0]] = data.inside.splits;
-
+  
+    const [variable, id, type = "guild", format, table = d.client.db.tables[0]] =
+      data.inside.splits;
+  
+    if (!d.client.variableManager.has(variable, table))
+      return d.aoiError.fnError(
+        d,
+        "custom",
+        {},
+        `Variable "${variable}" Not Found`
+      );
+  
     let key = null;
     let cache = null;
     let user = null;
-
+  
     const all = await d.client.db.all(table, variable);
-
+  
     if (type === "guild") {
-        key = `${variable.addBrackets()}_${`${id}_${
-            d.guild?.id === undefined ? "dm" : d.guild?.id
-        }`}`;
-        cache = await d.client.guilds.cache.get(id);
-        user =
-            typeof cache === "undefined" || Object.keys(cache).length === 0
-                ? await d.client.users.fetch(id)
-                : undefined;
+      key = `${variable.addBrackets()}_${`${id}_${
+        d.guild?.id === "undefined" ? "dm" : d.guild?.id
+      }`}`;
+      cache = await d.util.getGuild(d, id);
+      user =
+        typeof cache === "undefined" || Object.keys(cache).length === 0
+          ? await d.util.getUser(d, id)
+          : undefined;
     } else if (type === "global") {
-        key = `${variable.addBrackets()}_${id}`;
-        cache = await d.client.guilds.cache.get(id);
-        user =
-            typeof cache === "undefined" || Object.keys(cache).length === 0
-                ? await d.client.users.fetch(id)
-                : undefined;
+      key = `${variable.addBrackets()}_${id}`;
+      cache = await d.util.getGuild(d, id);
+      user =
+        typeof cache === "undefined" || Object.keys(cache).length === 0
+          ? await d.util.getUser(d, id)
+          : undefined;
     } else if (type === "message" || type === "channel") {
-        key = `${variable.addBrackets()}_${id}`;
+      key = `${variable.addBrackets()}_${id}`;
     } else {
-        d.aoiError.fnError(d, "custom", {inside: data.inside}, `type`);
+      d.aoiError.fnError(d, "custom", { inside: data.inside }, `type`);
     }
-
-    const foundData = all.find((x) => x.key === key);
-    const foundDataMap = all
-        .map((x, pos) => ({value: x.value, key: x.key, pos: pos + 1}))
-        .sort((a, b) => a.value - b.value);
-    let value = foundData?.value ?? 0;
-
+  
     switch (format) {
-        case "top":
-            data.result = foundDataMap.find((x) => x.key === key)?.pos || null;
-            break;
-        case "value":
-            data.result = value;
-            break;
-        case "tag":
-            data.result = user.tag || null;
-            break;
-        case "username":
-            data.result = user.username || null;
-            break;
-        default:
-            data.result = null;
+      case "top":
+        data.result = all.slice().sort((a, b) => b.value - a.value).findIndex((x) => x.key === key) + 1 || 0;
+        break;
+      case "value":
+        data.result = all.find(x => x.key === key).value || 0;
+        break;
+      case "id":
+        data.result = user.id || null;
+        break;
+      case "username":
+        data.result = user.username || null;
+        break;
+      case "tag":
+        data.result = user.tag || '0000';
+        break;
+      default:
+        data.result = null;
     }
-
+  
     return {
-        code: d.util.setCode(data),
+      code: d.util.setCode(data),
     };
-};
+  };
