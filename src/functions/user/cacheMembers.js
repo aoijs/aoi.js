d412d4dad69a9e44e401044b957ae6f2a7ae8805
Faf4a <fesserfabrice@gmail.com>
2023-09-17 05:19:02 +0200

    fix: cachemembers

--- src/functions/user/cacheMembers.js
+++ src/functions/user/cacheMembers.js
@@ -1,24 +1,26 @@
 module.exports = async (d) => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    const [guildID = d.guild.Id, returnCount = "false"] = inside.splits;
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    const [guildID = d.guild.id, returnCount = "false"] = data.inside.splits;
     let result;
-
+  
     const guild = await d.util.getGuild(d, guildID);
-    if (!guild) return d.aoiError.fnError(d, "guild", {inside});
-
+    if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
+  
     if (guild.memberCount > guild.members.cache.size) {
-        result = await guild.members.fetch().catch((err) => {
-            d.aoiError.fnError(
-                d,
-                "custom",
-                {},
-                "Failed To Fetch Members With Reason :" + err,
-            );
-        });
+      result = await guild.members.fetch().catch((err) => {
+        d.aoiError.fnError(
+          d,
+          "custom",
+          {},
+          "Failed To Fetch Members With Reason :" + err
+        );
+      });
     }
-
+  
+    data.result = returnCount === "true" ? guild.members.cache.size.toString() : undefined;
+  
     return {
-        code: d.util.setCode({function: d.func, code, inside, result}),
+      code: d.util.setCode(data),
     };
-};
+  };
