e2706d4a45c9236b0fb472fd1983b34c7a37bf95
Leref <12578483+Leref@users.noreply.github.com>
2023-10-07 18:02:32 -0400

    updated $eval

--- src/functions/util/eval.js
+++ src/functions/util/eval.js
@@ -1,17 +1,19 @@
-module.exports = async d => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
-    const [Code, returnCode = "false", sendMessage = "true", returnExecution = "false", returnID = "false"] = data.inside.splits;
+    const [code, returnCode = "false", sendMessage = "true", returnExecution = "false", returnID = "false"] = data.inside.splits;
 
-    let result = await d.interpreter(d.client, d.message, d.args, {
+    const options = {
         name: "eval",
-        code: Code.addBrackets()
-    }, d.client.db, returnCode === "true", undefined, {}, undefined, undefined, returnExecution === "true", returnID === "true", sendMessage === "true")
+        code: code.addBrackets(),
+    };
 
-    data.result = returnCode === "true" ? ([returnID, returnExecution].join(",") === "false,false" ? result.code : require('util').inspect(result)) : undefined
+    let result = await d.interpreter(d.client, d.message, d.args, options, d.client.db, returnCode === "true", undefined, {}, undefined, undefined, returnExecution === "true", returnID === "true", sendMessage === "true");
+
+    data.result = returnCode === "true" ? ([returnID, returnExecution].join(",") === "false,false" ? result.code : require('util').inspect(result)) : undefined;
 
     return {
-        code: d.util.setCode(data)
-    }
-} 
+        code: d.util.setCode(data),
+    };
+};
