7ae52567c4d5ec31f3b905a1ebe668031facf15a
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-04 08:52:24 +0000

    fix spacing

--- src/functions/interaction/clear.js
+++ src/functions/interaction/clear.js
@@ -3,20 +3,10 @@ module.exports = async (d) => {
   const { code } = d.command;
   if (data.err) return d.error(data.err);
 
-  let [
-    channelID = d.channel.id,
-    amount,
-    filters = "everyone",
-    returnCount = false,
-  ] = data.inside.splits;
+  let [channelID = d.channel.id, amount, filters = "everyone", returnCount = false ] = data.inside.splits;
 
   if (isNaN(amount))
-    return d.aoiError.fnError(
-      d,
-      "custom",
-      { inside: data.inside },
-      "Amount Provided In"
-    );
+    return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Amount Provided In" );
   amount = +amount + 1;
 
   const channel = await d.util.getChannel(d, channelID);
@@ -26,12 +16,7 @@ module.exports = async (d) => {
   let messages = await channel.messages
     .fetch({ limit: 100, cache: false })
     .catch((err) => {
-      d.aoiError.fnError(
-        d,
-        "custom",
-        {},
-        "Failed To Fetch Messages With Reason: " + err
-      );
+      d.aoiError.fnError(d, "custom", {}, "Failed To Fetch Messages With Reason: " + err);
     });
 
   filters = filters.toLowerCase().split(",");
@@ -57,22 +42,12 @@ module.exports = async (d) => {
   }
 
   let result = await channel.bulkDelete(messages, true).catch((err) => {
-    d.aoiError.fnError(
-      d,
-      "custom",
-      {},
-      "Failed To Delete Message With Reason: " + err
-    );
+    d.aoiError.fnError(d, "custom" ,{}, "Failed To Delete Message With Reason: " + err);
   });
 
   result = returnCount === true ? result.size : undefined;
 
   return {
-    code: d.util.setCode({
-      function: d.func,
-      code,
-      inside: data.inside,
-      result,
-    }),
+    code: d.util.setCode({ function: d.func, code, inside: data.inside, result }),
   };
 };
