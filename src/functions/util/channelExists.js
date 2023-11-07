e45459d16327ac3aa8348ae4bd8dd789e00610eb
Scriptyyy <81494246+scriptyyy13@users.noreply.github.com>
2023-11-07 19:06:18 +0300

    Update channelExists.js
    NOW IT WILL WORK)))

--- src/functions/util/channelExists.js
+++ src/functions/util/channelExists.js
@@ -3,11 +3,12 @@ module.exports = async d => {
     if (data.err) return d.error(data.err);
 
     let [channel] = data.inside.splits;
-
-    channel = d.client.channels.cache.find(x => x.name.toLowerCase() === channel.toLowerCase().addBrackets() || x.id === channel)
-
+    try {
+  channel = d.client.channels.cache.find(x => x.name.toLowerCase() === channel.toLowerCase().addBrackets() || x.id === channel)
     data.result = !!channel
-
+} catch (error) {
+  data.result = false;
+}
     return {
         code: d.util.setCode(data)
     }
