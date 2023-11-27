95eea4124c0a90dcc1a7278c9232ae071fe70f25
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-27 20:56:59 +0100

    feat: $awaitExecute[awaited]

---
+++ src/functions/interaction/awaitExecute.js
@@ -0,0 +1,37 @@
+const Interpreter = require("../../core/interpreter.js");
+
+module.exports = async (d) => {
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
+
+  const [awaitfunc] = data.inside.splits;
+
+  const cmd = d.client.cmd.awaited.find(
+    (x) => x.name.toLowerCase() === awaitfunc.addBrackets().toLowerCase()
+  );
+
+  if (!cmd)
+    return d.aoiError.fnError(
+      d,
+      "custom",
+      {},
+      `Invalid Awaited Command: '${awaitfunc.addBrackets()}' Provided`
+    );
+
+  await Interpreter(
+    d.client,
+    d.message,
+    d.args,
+    cmd,
+    d.client.db,
+    false,
+    undefined,
+    d.data
+  );
+
+  data.result = null;
+
+  return {
+    code: d.util.setCode(data),
+  };
+};
