f9eeda381cc959cedf0315c1f007fd90f8419d83
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-27 21:01:42 +0100

    fix: $fetchClientValues

--- src/functions/client/fetchClientValues.js
+++ src/functions/client/fetchClientValues.js
@@ -3,9 +3,9 @@ module.exports = async d => {
     if (data.err) return d.error(data.err);
 
     const [func] = data.inside.splits;
-    if (!d.client.clientShard) return d.aoiError.fnError(d, 'custom', {}, 'ClientShard Class is Not Initialised');
+    if (!d.client.shard) return d.aoiError.fnError(d, 'custom', {}, 'ClientShard Class is Not Initialised');
 
-    data.result = await d.client.clientShard.fetchClientValues(func);
+    data.result = await d.client.shard.fetchClientValues(func);
 
     return {
         code: d.util.setCode(data),
