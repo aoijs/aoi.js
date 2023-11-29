95d95b72bae0243827842d81e9e57df162efe6aa
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-29 03:14:41 +0100

    fix($resolveEmojiId): returning emoji string not id

--- src/functions/util/resolveEmojiID.js
+++ src/functions/util/resolveEmojiID.js
@@ -1,14 +1,10 @@
-module.exports = (d) => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
     const emoji = data.inside.inside;
 
-    data.result = d.client.emojis.cache.find(
-        (x) =>
-            x.name.toLowerCase() === emoji.addBrackets().toLowerCase() ||
-            x.toString() === emoji.addBrackets(),
-    );
+    data.result = (await d.util.getEmoji(d, emoji))?.id ?? "";
 
     return {
         code: d.util.setCode(data),
