5d2d8876e1cc03fa84cf39d9582a8cf4b4f7ef49
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-29 01:45:15 +0100

    fix($createThread): invalid parameters

--- src/functions/guild/createThread.js
+++ src/functions/guild/createThread.js
@@ -1,28 +1,29 @@
 module.exports = async d => {
-    const {code} = d.command;
-    const inside = d.unpack();
-    const err = d.inside(inside);
-    if (err) return d.error(err);
+    const data = d.util.aoiFunc(d);
+    const { code } = d.command;
+    if (data.err) return d.error(data.err);
 
-    let [channelID, name, archive = "MAX", type = "public", startMessage, returnID = "false"] = inside.splits;
+    let [channelID, name, archive = "MAX", type = "public", startMessage, returnID = "false"] = data.inside.splits;
 
     const channel = await d.util.getChannel(d, channelID);
-    if (!channel) return d.aoiError.fnError(d, "channel", {inside});
+    if (!channel) return d.aoiError.fnError(d, "channel", { inside: data.inside });
 
     type = d.util.threadTypes[type];
-    if (!type) return d.aoiError.fnError(d, "custom", {inside}, "Invalid Type Provided In");
-    if (!["60", "1440", "4320", "10080", "MAX"].includes(archive.toUpperCase())) d.aoiError.fnError(d, "custom", {inside}, "Invalid Archive Duration Provided In");
+    if (!type) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Type Provided In");
+    if (!["60", "1440", "4320", "10080", "MAX"].includes(archive.toUpperCase())) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Archive Duration Provided In");
 
     const result = await channel.threads.create({
         name,
-        autoArchiveDuration: archive,
+        autoArchiveDuration: archive.toUpperCase().replace("MAX", "10080"),
         type,
         startMessage: startMessage?.trim() === "" ? undefined : startMessage
     }).catch(e => {
-        d.aoiError.fnError(d, "custom", {}, "Failed To Create Thread With Reason: " + e);
+        return d.aoiError.fnError(d, "custom", {}, "Failed To Create Thread With Reason: " + e);
     });
 
+    data.result = returnID === "true" ? result?.id : undefined;
+
     return {
-        code: d.util.setCode({function: d.func, code, inside, result: returnID === "true" ? result?.id : ""})
+        code: d.util.setCode(data)
     }
 }
