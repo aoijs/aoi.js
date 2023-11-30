8b680ae872b61bc03c4f2814ead1c90754b44ae1
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-11-30 21:41:09 +0100

    fix: aoi.db logging

--- src/handler/AoiLogs.js
+++ src/handler/AoiLogs.js
@@ -3,21 +3,24 @@ const AoiError = require("../classes/AoiError.js");
 
 module.exports = async () => {
   try {
-    const version = json.version;
-    const installedVersion = {
-      text: `Installed on v${version}`,
-      textColor: "green",
-    };
-    const discordServer = {
-      text: "Discord Server: https://discord.gg/HMUfMXDQsV",
-      textColor: "blue",
-    };
-
-    AoiError.createCustomBoxedMessage([installedVersion, discordServer], "white", {
-      text: "aoi.js ",
-      textColor: "cyan",
-    });
-  } catch (error) {
-    console.error(`AoiLogs: Error during initialization: ${error.message}`);
+    AoiError.createCustomBoxedMessage(
+      [
+        {
+          text: `Installed on v${json.version}`,
+          textColor: "green",
+        },
+        {
+          text: "Discord Server: https://discord.gg/HMUfMXDQsV",
+          textColor: "blue",
+        },
+      ],
+      "white",
+      {
+        text: "aoi.js ",
+        textColor: "cyan",
+      }
+    );
+  } catch (err) {
+    console.error(`AoiLogs: Error during initialization: ${err}`);
   }
 };
