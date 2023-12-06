69b7d12df1496c511111d1ff9237a2dbfb32f7d2
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-12-06 07:44:57 +0100

    fix: getCooldownTime

--- src/functions/misc/getCooldownTime.js
+++ src/functions/misc/getCooldownTime.js
@@ -1,30 +1,28 @@
-const {Time} = require('../../utils/helpers/customParser.js');
+const { Time } = require("../../utils/helpers/customParser.js");
 
-module.exports = async d => {
-    const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
+module.exports = async (d) => {
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
 
-    let [time, type, cmdName, id] = data.inside.splits;
+  let [time, type, cmdName, id] = data.inside.splits;
 
-    time = Time.parse(time)?.ms
-    if (!time) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Time Provided In');
+  time = Time.parse(time)?.ms;
+  if (!time) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Type");
 
-    const types = {
-        globalUser: id,
-        user: `${id}_${d.message.guild.id || 'dm'}`,
-        server: id,
-        channel: id,
-        static: undefined,
-    }
+  const types = {
+    globalUser: id,
+    user: `${id}_${d.message.guild.id || "dm"}`,
+    server: id,
+    channel: id,
+    static: undefined,
+  };
 
-    if (!types[type]) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Type Provided In');
+  if (!types[type]) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Type");
 
-    const timeEnd = await d.client.db.get(d.client.db.tables[0], 'cooldown', cmdName + "_" + types[type]);
+  const timeEnd = await d.client.db.get("__aoijs_vars__", "cooldown", cmdName + "_" + types[type]);
+  data.result = ((timeEnd?.value || d) - Date.now()) < 0 ? 0 : (timeEnd?.value || d) - Date.now();
 
-    data.result = (timeEnd?.value || 0) - Date.now();
-
-    return {
-        code: d.util.setCode(data)
-    }
-}
-
+  return {
+    code: d.util.setCode(data),
+  };
+};
