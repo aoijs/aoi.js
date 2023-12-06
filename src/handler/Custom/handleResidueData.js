a76346cb57be5794b92a566a415561d0cf68be11
Ayaka <73595362+USERSATOSHI@users.noreply.github.com>
2023-12-06 11:47:54 +0530

    Update handleResidueData.js

--- src/handler/Custom/handleResidueData.js
+++ src/handler/Custom/handleResidueData.js
@@ -1,7 +1,7 @@
 async function handleResidueData(client) {
     const db = client.db;
     await client.db.deleteMany("__aoijs_vars__", (data) => {
-        const key = data.id.split("_")[0];
+        const key = data.key.split("_")[0];
         if (key === "cooldown" && data.value < Date.now()) return true;
     });
 }
