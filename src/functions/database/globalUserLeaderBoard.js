8f12bce9b435a8b0954a12bea520cb61aafa8a71
Emi <136156874+aho-emi@users.noreply.github.com>
2023-12-11 08:36:26 +0545

    added {value:,}, hide zero & negative value support
    also moved the slicing before the loop

--- src/functions/database/globalUserLeaderBoard.js
+++ src/functions/database/globalUserLeaderBoard.js
@@ -1,88 +1,93 @@
 module.exports = async (d) => {
-    const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
+  let c = Date.now();
+  const [
+    variable,
+    type = "asc",
+    custom = `{top}. {username}: {value:,}`,
+    list = 10,
+    page = 1,
+    table = d.client.db.tables[0],
+    hideNegativeValue = false,
+    hideZeroValue = false
+  ] = data.inside.splits;
 
-    const [
-        variable,
-        type = "asc",
-        custom = `{top}. {username}: {value}`,
-        list = 10,
-        page = 1,
-        table = d.client.db.tables[0],
-    ] = data.inside.splits;
+  if (!d.client.variableManager.has(variable, table))
+    return d.aoiError.fnError(d, 'custom', {}, `Variable "${variable}" Not Found`);
 
-    const all = await d.client.db.all(table, (data) =>
-        data.key.startsWith(variable.deleteBrackets()) && data.key.split("_").length === 2,
-    );
+  let v = d.client.variableManager.get(variable, table);
 
+  let db = await d.client.db.all(table, variable.addBrackets(), 1);
+  if (d.client.db.type === "aoi.db")
+    db.sort((a, b) => Number(a.value) - Number(b.value));
+  else db.sort((a, b) => Number(y.data.value) - Number(x.data.value));
 
-    let y = 0;
-    let value;
-    let content = [];
+  if (type === "desc") db = db.reverse();
 
-    for (const Data of all.sort((x, y) => {
-        if (d.client.db.type === "aoi.db")
-            return Number(y.value) - Number(x.value);
-        else return Number(y.data.value) - Number(x.data.value);
-    })) {
-        let user;
-        if (d.client.db.type === "aoi.db") value = Number(Data.value);
-        else value = Number(Data.data.value);
-        user = await d.util.getUser(d, Data.key.split("_")[1]);
+  if (hideNegativeValue === "true") db = db.filter(x => x.value >= 0);
+  if (hideZeroValue === "true") db = db.filter(x => x.value != 0);
 
-        if (user) {
-            y++;
+  let y = 0;
+  let value;
+  let content = [];
 
-            let text = custom
-                .replace(`{top}`, y)
-                .replace("{id}", user.id)
-                .replace("{tag}", user.tag)
-                .replace(`{username}`, user.username.removeBrackets())
-                .replace(`{value}`, value);
+  db = db.slice(page * list - list, page * list);
 
-            if (text.includes("{execute:")) {
-                let ins = text.split("{execute:")[1].split("}")[0];
+  for (const Data of db) {
+    let user;
+    if (d.client.db.type === "aoi.db") value = Number(Data.value);
+    else value = Number(Data.data.value);
+    user = await d.util.getUser(d, Data.key.split("_")[1]);
 
-                const awaited = d.client.cmd.awaited.find(
-                    (c) => c.name === ins,
-                );
+    if (user) {
+      y++;
+      let text = custom
+        .replaceAll(`{top}`, y)
+        .replaceAll("{id}", user.id)
+        .replaceAll("{tag}", user.tag)
+        .replaceAll(`{username}`, user.username.removeBrackets())
+        .replaceAll(`{value}`, value);
 
-                if (!awaited)
-                    return d.aoiError.fnError(
-                        d,
-                        "custom",
-                        { inside: data.inside },
-                        ` Invalid awaited command '${ins}' in`,
-                    );
+      if (text.includes("{value:")) {
+        let sep = text.split("{value:")[1].split("}")[0];
+        text = text.replaceAll(`{value:${sep}}`, value.toLocaleString().replaceAll(",", sep));
+      }
 
-                const CODE = await d.interpreter(
-                    d.client,
-                    {
-                        guild: d.message.guild,
-                        channel: d.message.channel,
-                        author: user,
-                    },
-                    d.args,
-                    awaited,
-                    undefined,
-                    true,
-                );
+      if (text.includes("{execute:")) {
+        let ins = text.split("{execute:")[1].split("}")[0];
+        const awaited = d.client.cmd.awaited.find((c) => c.name === ins);
 
-                text = text.replace(`{execute:${ins}}`, CODE);
-            }
+        if (!awaited)
+          return d.aoiError.fnError(
+            d,
+            "custom",
+            { inside: data.inside },
+            ` Invalid awaited command '${ins}' in`,
+          );
 
-            content.push(text);
-        }
+        const CODE = await d.interpreter(
+          d.client,
+          {
+            guild: d.message.guild,
+            channel: d.message.channel,
+            author: user,
+          },
+          d.args,
+          awaited,
+          undefined,
+          true,
+        );
+        text = text.replaceAll(`{execute:${ins}}`, CODE);
+      }
+
+      content.push(text);
     }
+  }
 
-    if (type === "desc") content = content.reverse();
+  data.result = content.join("\n");
 
-    const px = page * list - list,
-        py = page * list;
-
-    data.result = content.slice(px, py).join("\n");
-
-    return {
-        code: d.util.setCode(data),
-    };
+  return {
+    code: d.util.setCode(data),
+  };
 };
