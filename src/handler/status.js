b497bd25c0f505ad8ed1c51c8c68e7927ceab792
5UP3R_D1N <superdinmc@gmail.com>
2023-12-17 11:44:37 +0000

    Simplify handler/status

--- src/handler/status.js
+++ src/handler/status.js
@@ -1,57 +1,45 @@
 module.exports = async (statuses, client) => {
-  if (statuses.size === 0) {
-    return;
-  }
+   if (statuses.size <= 0) {
+      return;
+   }
 
-  const statusArray = statuses.allValues();
-  let y = 0;
+   const statusArray = statuses.allValues();
+   let y = 0;
 
-  const processStatus = async () => {
-    if (!statusArray[y]) {
-      y = 0;
-    }
-
-    const stats = {
-      activity: {
-        type: statusArray[y].activity.type,
-        url: statusArray[y].activity.url,
-      },
-    };
-
-    if (statusArray[y].activity.name) {
-      if (statusArray[y].activity.name.includes("$")) {
-        stats.activity.name = (
-            await client.functionManager.interpreter(
-                client,
-                {},
-                [],
-                { code: statusArray[y].activity.name },
-                client.db,
-                true
-            )
-        )?.code;
-      } else {
-        stats.activity.name = statusArray[y].activity.name;
+   const processStatus = async () => {
+      if (!statusArray[y]) {
+         y = 0;
       }
-    } else {
-      throw new TypeError(`Missing or invalid 'name' method in status[${y}]`);
-    }
 
-    client.user.setPresence({
-      status: statusArray[y].status,
-      activities: [stats.activity],
-      afk: statusArray[y].afk,
-    });
+      const stats = {
+         activity: statusArray[y].activity
+      };
 
-    y++;
+      if (name = stats?.activity?.name) {
+         if (name.includes("$")) {
+            stats.activity.name = (
+               await client.functionManager.interpreter(
+                  client,
+                  {},
+                  [],
+                  { code: statusArray[y].activity.name },
+                  client.db,
+                  true
+               )
+            )?.code;
+         }
+      } else {
+         throw new TypeError(`Missing or invalid 'name' method in status[${y}]`);
+      }
 
-    if (y < statusArray.length) {
+      client.user.setPresence({
+         status: statusArray[y].status,
+         activities: [stats.activity],
+         afk: statusArray[y].afk,
+      });
       setTimeout(processStatus, statusArray[y]?.time * 1000 || 0);
-    } else {
-      y = 0;
-      setTimeout(processStatus, statusArray[y]?.time * 1000 || 0);
-    }
-  };
+      y++;
+   };
 
-  await processStatus();
+   await processStatus();
 };
