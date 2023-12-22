fb45467b968b9f3466c73f79dc41484fea2a1594
Emi <136156874+aho-emi@users.noreply.github.com>
2023-12-23 01:01:10 +0545

    modified $root

--- src/functions/math/root.js
+++ src/functions/math/root.js
@@ -1,15 +1,29 @@
+function nthRoot(x, n) {
+                try {
+    var negate = n % 2 == 1 && x < 0;
+    if(negate)
+      x = -x;
+    var possible = Math.pow(x, 1 / n);
+    n = Math.pow(possible, n);
+    if(Math.abs(x - n) < 1 && (x > 0 == n > 0))
+      return negate ? -possible : possible;
+  } catch(e){}
+}
+
 module.exports = d => {
-      const data = d.util.aoiFunc(d);
-      if (data.err) return d.error(data.err);
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
 
-      const [number, root] = data.inside.splits;
+  const numbers = data.inside.splits;
 
-      if (isNaN(number) || number.trim() === '') return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Number Provided In');
-      if (isNaN(root) || root.trim() === '') return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Root Number Provided In');
-
-      data.result = number < 0 && root % 2 === 0 ? NaN : Math.pow(Number(number), 1 / Number(root));
-
-      return {
-          code: d.util.setCode(data)
-      }
+  if (numbers.some((x) => isNaN(x) || !x.trim())) {
+    return d.aoiError.fnError(d, 'custom', { inside: data.inside }, 'Power Numbers Provided In');
   }
+
+  data.result = numbers.reduce((current,root) => nthRoot(current, root));
+
+
+  return {
+    code: d.util.setCode(data)
+  }
+}
