9660b2afc88805da67856181191a7bc351cdddbb
Emi <136156874+aho-emi@users.noreply.github.com>
2023-12-22 22:45:02 +0545

    $pow - power math func

---
+++ src/functions/math/pow.js
@@ -0,0 +1,15 @@
+module.exports = d => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [number, power] = data.inside.splits;
+
+    if (isNaN(number) || number.trim() === '') return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Number Provided In');
+    if (isNaN(power) || power.trim() === '') return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Invalid Power Provided In');
+
+    data.result = Math.pow(Number(number), Number(power);
+
+    return {
+        code: d.util.setCode(data)
+    }
+}
