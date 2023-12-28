b961a7616ca1171e1bd9ad63539be01a2ebecdee
Faf4a <87046111+Faf4a@users.noreply.github.com>
2023-12-28 04:02:44 +0100

    $index function

---
+++ src/functions/misc/index.js
@@ -0,0 +1,14 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const i = d?.data.index;
+
+    if (!i && i !== 0) return d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'No loop command.');
+
+    data.result = Number(i);
+    
+    return {
+        code: d.util.setCode(data),
+    };
+};
