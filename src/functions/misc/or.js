176ed33de95ca51030370b94523188ca13b3e0d8
Emi <136156874+aho-emi@users.noreply.github.com>
2023-12-30 09:03:08 +0545

    fixed <= and >= in $or

--- src/functions/misc/or.js
+++ src/functions/misc/or.js
@@ -2,25 +2,34 @@ module.exports = (d) => {
 		const data = d.util.aoiFunc(d);
 		if (data.err) return d.error(data.err);
 
-		let conditions = data.inside.splits;
+		const conditions = data.inside.splits.map(c => c.trim());
 
 		function evaluateOR(conditions) {
+
 			return conditions.reduce((prev, current) => {
+
 				let result;
-				const [left, comparator, right] = current.split(/(==|!=|>|<)/);
-        
+
+				const [left, comparator, right] = current.split(/(==|!=|>=|<=|>|<)/);
+
 				if (comparator === '==') {
 					result = left === right;
 				} else if (comparator === '!=') {
 					result = left !== right;
+				} else if (comparator === '<=') {
+					result = parseFloat(left) <= parseFloat(right);
+				} else if (comparator === '>=') {
+					result = parseFloat(left) >= parseFloat(right);
 				} else if (comparator === '>') {
-					result = parseInt(left) > parseInt(right);
+					result = parseFloat(left) > parseFloat(right);
 				} else if (comparator === '<') {
-					result = parseInt(left) < parseInt(right);
+					result = parseFloat(left) < parseFloat(right);
 				}
 
 				return prev || result;
+
 			}, false);
+
 		}
 
 		data.result = evaluateOR(conditions);
