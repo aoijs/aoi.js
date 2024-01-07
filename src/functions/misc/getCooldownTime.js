fd836de8f6368e5b1db976ca3afc43dff44b11cf
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-01-07 21:54:33 +0100

    fix: getCooldownTIme

--- src/functions/misc/getCooldownTime.js
+++ src/functions/misc/getCooldownTime.js
@@ -12,15 +12,18 @@ module.exports = async (d) => {
   const types = {
     globalUser: id,
     user: `${id}_${d.message.guild.id || "dm"}`,
-    server: id,
+    guild: id,
     channel: id,
-    static: undefined,
   };
 
   if (!types[type]) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Type");
 
   const timeEnd = await d.client.db.get("__aoijs_vars__", "cooldown", cmdName + "_" + types[type]);
-  data.result = ((timeEnd?.value || d) - Date.now()) < 0 ? 0 : (timeEnd?.value || d) - Date.now();
+  if (!timeEnd) {
+    data.result = 0;
+  } else {
+    data.result = (timeEnd?.value - Date.now()) < 0 ? 0 : timeEnd?.value - Date.now();
+  }
 
   return {
     code: d.util.setCode(data),
