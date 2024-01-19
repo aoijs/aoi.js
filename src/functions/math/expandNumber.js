ba9ea93f1c7f66bff43e5472242aaf4cb8e87210
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-01-19 18:20:21 +0100

    fix: expandNumber

--- src/functions/math/expandNumber.js
+++ src/functions/math/expandNumber.js
@@ -1,37 +1,31 @@
-const {SI_SYMBOL} = require("../../utils/Constants")
+const { SI_SYMBOL } = require("../../utils/Constants");
 
-module.exports = async d => {
-    const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
+module.exports = async (d) => {
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
 
-    const [abbrNumber] = data.inside.inside.toUpperCase()
+  const abbrNumber = data.inside.inside.toUpperCase();
 
-    let abbrIndex
+  let abbrIndex;
 
-    for (let i = SI_SYMBOL.length - 1; i >= 0; i--) {
-        const sym = SI_SYMBOL[i]
-        if (abbrNumber.endsWith(sym.toUpperCase())) {
-            abbrIndex = i
-            break
-        }
+  for (let i = SI_SYMBOL.length - 1; i >= 0; i--) {
+    const sym = SI_SYMBOL[i];
+    if (abbrNumber.endsWith(sym.toUpperCase())) {
+      abbrIndex = i;
+      break;
     }
+  }
+  const abbr = SI_SYMBOL[abbrIndex];
+  const number = abbrIndex ? abbrNumber.slice(0, -abbr.length) : abbrNumber;
+  if (isNaN(Number(number)))
+    return d.aoiError.fnError(d, "custom", { inside: data.inside }, "number in");
 
-    const abbr = SI_SYMBOL[abbrIndex]
-    const number = abbrIndex ? abbrNumber.slice( 0, -abbr.length ) : abbrNumber
+  const num = parseFloat(number);
+  const multiplier = abbrIndex ? 1e3 ** abbrIndex : 1;
 
-    if (isNaN(Number(number))) return d.aoiError.fnError(
-        d,
-        "custom",
-        {inside: data.inside},
-        "Invalid number in"
-    )
+  data.result = num * multiplier;
 
-    const num = parseFloat(number)
-    const multiplier = abbrIndex ? 1e3 ** abbrIndex : 1
-
-    data.result = num * multiplier
-
-    return {
-        code: d.util.setCode(data)
-    }
-}
+  return {
+    code: d.util.setCode(data),
+  };
+};
