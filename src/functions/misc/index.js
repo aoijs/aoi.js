d0286063b6c21621dbd059b80c7ef97b3241a4a3
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-01-30 22:04:15 +0100

    fix: index returning error

--- src/functions/misc/index.js
+++ src/functions/misc/index.js
@@ -1,6 +1,6 @@
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
+    //if (data.err) return d.error(data.err);
 
     const i = d?.data.index;
 
