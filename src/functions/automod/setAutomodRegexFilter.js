f6cf3dd902b85e41654f0ca53afbbb2cd23e98d7
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-11 11:13:09 +0100

    fix(setAutomodRegexFilter): remove logging

--- src/functions/automod/setAutomodRegexFilter.js
+++ src/functions/automod/setAutomodRegexFilter.js
@@ -2,14 +2,6 @@ module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     const [...regex] = data.inside.splits;
 
-    console.log({ 
-        triggerMetadata: { 
-            regexPatterns: regex, 
-            ...d.data.automodRule?.triggerMetadata 
-        },
-        ...d.data.automodRule 
-    })
-
     return {
         code: d.util.setCode(data),
         data: { 
