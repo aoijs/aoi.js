5793e80de6ecdb1d39cb86dee3a8d9a51c91221c
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-17 14:25:23 +0100

    feat(referenceMessageId): channel option

--- src/functions/guild/referenceMessageID.js
+++ src/functions/guild/referenceMessageID.js
@@ -1,7 +1,10 @@
 module.exports = d => {
     const data = d.util.aoiFunc(d);
+    let [ channel = d.channel.id, message = d.message.id ] = data.inside.splits
 
-    data.result = d.message?.reference?.messageId;
+    channel = await d.util.getChannel(d, channel)
+    
+    data.result = (await d.util.fetchMessage(channel, message)).reference?.messageId
 
     return {
         code: d.util.setCode(data)
