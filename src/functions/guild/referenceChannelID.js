20fe0d75fc69418e9941ae020d729c1b9d859385
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-17 14:35:34 +0100

    feat: channel option for reference functions

--- src/functions/guild/referenceChannelID.js
+++ src/functions/guild/referenceChannelID.js
@@ -1,7 +1,10 @@
-module.exports = d => {
+module.exports = async d => {
     const data = d.util.aoiFunc(d);
+    let [ channel = d.channel.id, message = d.message.id ] = data.inside.splits
 
-    data.result = d.message?.reference.channelId
+    channel = await d.util.getChannel(d, channel)
+    
+    data.result = (await d.util.fetchMessage(channel, message)).reference?.channelId
 
     return {
         code: d.util.setCode(data)
