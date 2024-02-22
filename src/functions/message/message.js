ecadc0e15aaa0fc3c9a86adc848f7cf5fa3a363d
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-22 15:56:49 +0100

    fix: error returning when somehow args is undefined

--- src/functions/message/message.js
+++ src/functions/message/message.js
@@ -3,9 +3,9 @@ module.exports = async d => {
 
     const [args] = data.inside.splits;
 
-    if (isNaN(args) && args < 1) return d.aoiError.fnError(d, "custom", {inside: data.inside}, "Invalid Index Provided In");
+    if (isNaN(args) && args < 1) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Index Provided In");
 
-     data.result = args ? d.args[args - 1]?.deleteBrackets() : d.args.join(" ")?.deleteBrackets();
+    data.result = args ? d.args[args - 1]?.deleteBrackets() : d.args?.join(" ")?.deleteBrackets();
 
     return {
         code: d.util.setCode(data)
