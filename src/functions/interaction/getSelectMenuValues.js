b80bd6a72a5ee8af0e49c8464ed5781c0198d40d
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-24 07:08:43 +0100

    feat(getSelectMenuValues[]): returns select menu values

---
+++ src/functions/interaction/getSelectMenuValues.js
@@ -0,0 +1,21 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    const [ value = "1", sep = "," ] = data.inside.splits; 
+
+    let interaction = d.data?.interaction
+
+    if (!interaction) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "interaction");
+
+    if (value.toLowerCase() === "all") {
+        data.result = d.data.interaction.values.join(sep);
+    } else {
+        if (isNaN(Number(value)) || Number(value - 1) < 0) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "number");
+
+        data.result = interaction.values[Number(value - 1)]
+    }
+
+    return {
+        code: d.util.setCode(data)
+    }
+}
