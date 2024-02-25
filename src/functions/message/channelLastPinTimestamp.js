3fc5e83af532978f5533d137ff4c071c0a7c4cd7
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-25 17:56:01 +0100

    feat: channelLastPinTimestamp

---
+++ src/functions/message/channelLastPinTimestamp.js
@@ -0,0 +1,13 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    const [ channelID = d.channel?.id ] = data.inside.splits;
+
+    const channel = await d.util.getChannel(d, channelID);
+
+    data.result = channel?.lastPinTimestamp;
+
+    return {
+        code: d.util.setCode(data),
+    }
+}
