8b3e3684860da5a9520a47cfaa20adc5002b178b
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-25 17:56:20 +0100

    feat: memberPremiumSince

---
+++ src/functions/user/memberPremiumSince.js
@@ -0,0 +1,15 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    const [ memberID = d.member?.id, guildID = d.guild?.id ] = data.inside.splits;
+
+    const guild = await d.util.getGuild(d, guildID)
+
+    const member = await d.util.getMember(guild, memberID);
+
+    data.result = member?.premiumSinceTimestamp || 0;
+
+    return {
+        code: d.util.setCode(data),
+    }
+}
