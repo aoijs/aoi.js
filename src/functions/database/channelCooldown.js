46ddfdf6ccfa61336b8f110c11a0dd0f17883d48
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-02-28 15:14:46 +0100

    fix: error parsing in cooldown functions

--- src/functions/database/channelCooldown.js
+++ src/functions/database/channelCooldown.js
@@ -36,7 +36,7 @@ module.exports = async (d) => {
                 .replaceAll("%ms%", object.ms)
                 .replaceAll("%fullTime%", toString());
 
-            errorObject = await d.util.errorParser(errorObject);
+            errorObject = await d.util.errorParser(errorObject, d);
             await d.aoiError.makeMessageError(
                 d.client,
                 d.channel,
