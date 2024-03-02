5728f55b74a93acdab6fb3f3ddff257c1406e203
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-02 15:49:46 +0100

    fix(awaitComponents): fixed function not working

--- src/functions/awaited/awaitComponents.js
+++ src/functions/awaited/awaitComponents.js
@@ -1,49 +1,31 @@
+const { Time } = require("../../utils/helpers/customParser.js");
+
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
-    let [messageID, userFilter, customIDs, cmds, errorMsg = "", uses = 1, awaitData = ""] = data.inside.splits;
+    let [channelID, messageID, userFilter, customIDs, cmds, errorMsg = "", uses = 1, time] = data.inside.splits;
 
-    if (errorMsg?.trim !== "" && errorMsg) {
-        errorMsg = await d.util.errorParser(errorMsg, d);
-    }
-    if (awaitData !== "") {
-        try {
-            awaitData = JSON.parse(awaitData);
-        } catch (e) {
-            d.aoiError.fnError(d, "custom", {inside: data.inside}, "Invalid awaitData Provided In");
-        }
-    }
     cmds = cmds.split(",");
     cmds.forEach((x) => {
-        if (
-            d.client.cmd.awaited.find((y) => y.name.toLowerCase() === x.toLowerCase())
-        ) {
-        } else {
-            d.aoiError.fnError(d, "custom", {}, "Could not find awaitedCommand " + x);
+        if (!(d.client.cmd.interaction.selectMenu.find((y) => y.name.toLowerCase() === x.toLowerCase()) || d.client.cmd.interaction.button.find((y) => y.name.toLowerCase() === x.toLowerCase()))) {
+            return d.aoiError.fnError(d, "custom", {}, "awaitedCommand " + x);
         }
     });
+
     customIDs = customIDs.split(",");
 
-    const Component = new d.client.interactionManager.awaitComponents(
-        {
-            msgId: messageID,
-            filter: userFilter,
-            customIds: customIDs,
-            cmds: cmds,
-            errorMessage: errorMsg.awaitData,
-            uses: uses,
-        },
-        d.client,
-        awaitData,
-    );
-    d.client.interactionManager.on("messageComponentInteraction", interact);
-    Component.on("AwaitComponent", async (interaction) => {
+    const channel = await d.util.getChannel(d, channelID);
+    const message = await d.util.getMessage(channel, messageID);
+
+    const filter = interaction => customIDs.includes(interaction.customId) && (userFilter.includes("everyone") || userFilter.includes(m.user.id));
+    const collector = message.createMessageComponentCollector({ filter: filter, max: uses, time: Time.parse(time)?.ms });
+
+    collector.on("collect", async (interaction) => {
         const index = cmds[customIDs?.indexOf(interaction.customId)];
-        const cmd = d.client.cmd.awaited.find(
-            (x) => x.name.toLowerCase() === index.toLowerCase(),
-        );
+        const cmd = d.client.cmd.interaction.selectMenu.find((y) => y.name.toLowerCase() === index.toLowerCase()) || d.client.cmd.interaction.button.find((y) => y.name.toLowerCase() === index.toLowerCase())
         if (!cmd) return;
+
         await d.interpreter(
             d.client,
             interaction,
@@ -52,24 +34,24 @@ module.exports = async (d) => {
             d.client.db,
             false,
             undefined,
-            {awaitawaitData: Component.awaitData, interaction: interaction},
+            { interaction: interaction },
         );
     });
 
-    //--------------------------------------//
-    async function interact(interaction) {
-        if (Component.uses <= Component.tries) {
-            d.client.interactionManager.removeListener(
-                "messageComponentInteraction",
-                interact,
-            );
-        } else
-            await Component.await(
-                interaction.message.id,
-                interaction.user.id,
-                interaction.customId,
-                interaction,
-            );
+    if (errorMsg?.trim !== "" && errorMsg) {
+        errorMsg = await d.util.errorParser(errorMsg, d);
+
+        collector.on("end", async (collected) => {
+            if (collected.size < Number(uses)) {
+                await d.aoiError.makeMessageError(
+                    d.client,
+                    channel,
+                    errorMsg.data ?? errorMsg,
+                    errorMsg.options,
+                    d
+                );
+            }
+        });
     }
 
     return {
