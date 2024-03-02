763db499152fc88204abc56402170dc6f31fbb47
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-02 22:23:26 +0100

    feat(getComponentCustomId): returns component custom id

---
+++ src/functions/interaction/getComponentCustomId.js
@@ -0,0 +1,13 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+
+    let interaction = d.data?.interaction
+
+    if (!interaction) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "interaction");
+
+    data.result = d.data.interaction.customId
+
+    return {
+        code: d.util.setCode(data)
+    }
+}
