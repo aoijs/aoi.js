34545fb3a622fca21c84e2295a9a8ed4d4feb071
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-05 15:47:42 +0100

    feat: disconnectUser[]

---
+++ src/functions/user/disconnectUser.js
@@ -0,0 +1,23 @@
+module.exports = async (d) => {
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
+
+  const [guildID, userID, reason] = data.inside.splits;
+
+  const guild = await d.util.getGuild(d, guildID);
+  if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
+
+  const member = await d.util.getMember(guild, userID);
+  if (!member) return d.aoiError.fnError(d, "member", { inside: data.inside });
+
+  const state = guild.voiceStates.cache.get(member.id);
+  if (!state?.channel) return d.aoiError.fnError(d, "custom", {}, "User Is Not In Any Voice/Stage Channel.");
+
+  await state.disconnect(reason?.addBrackets()).catch((e) => {
+    return d.aoiError.fnError(d, "custom", {}, "Failed To Disconnect User With Reason: " + e);
+  });
+
+  return {
+    code: d.util.setCode(data),
+  };
+};
