5d4d00e5068c3a06d5550455f5cbf744f578c454
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-05 15:39:50 +0100

    feat: hasComponents[]

---
+++ src/functions/interaction/hasComponents.js
@@ -0,0 +1,28 @@
+const { ComponentType } = require("discord.js");
+
+module.exports = async d => {
+    const data = d.util.aoiFunc(d);
+
+    const [ channelId = d.channel?.id, messageId = d.message?.id, type = "any" ] = data.inside.splits;
+
+    const channel = await d.util.getChannel(d, channelId);
+    const message = await d.util.getMessage(channel, messageId);
+
+    let hasComponent = false;
+
+    for (const row of message.components) {
+        for (const component of row.components) {
+            if (type === "any" || ComponentType[component.type] === type) {
+                hasComponent = true;
+                break;
+            }
+        }
+        if (hasComponent) break;
+    }
+
+    data.result = hasComponent;
+
+    return {
+        code: d.util.setCode(data)
+    }
+};
