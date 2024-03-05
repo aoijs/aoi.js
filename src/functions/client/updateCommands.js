a6721d88ef821faf909dadceba2be93e5706756e
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-05 15:12:05 +0100

    feat(updateCommads): support sharding

--- src/functions/client/updateCommands.js
+++ src/functions/client/updateCommands.js
@@ -1,8 +1,19 @@
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
 
+    const [ all = "true" ] = data.inside.splits;
+
     if (!d.client.loader) return d.aoiError.fnError(d, 'custom', {}, 'LoadCommands Class Is Not Initiated');
-    await d.client.loader.update();
+
+    if (d.client.shard && all === "true") {
+        await d.client.shard.broadcastEval(async client => {
+            if (client.loader) {
+                await client.loader.update();
+            }
+        });
+    } else {
+        await d.client.loader.update();
+    }
 
     return {
         code: d.util.setCode(data)
