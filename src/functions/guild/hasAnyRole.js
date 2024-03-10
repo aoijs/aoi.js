e3e07490155b2faa66bd730468d6692b113eff95
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-10 09:13:57 +0100

    fix: return false when member doesnt exist

--- src/functions/guild/hasAnyRole.js
+++ src/functions/guild/hasAnyRole.js
@@ -10,11 +10,12 @@ module.exports = async d => {
     if (!guild) return d.aoiError.fnError(d, "guild", {inside: data.inside});
 
     const member = await d.util.getMember(guild, userID);
-    if (!member) return d.aoiError.fnError(d, 'member', {inside: data.inside});
-
-    const memRoles = member.roles.cache
-
-    data.result = roles.some(x => memRoles.has(x) || memRoles.find(y => y.name.toLowerCase() === x));
+    if (!member) {
+        data.result = false;
+    } else {
+        const memRoles = member.roles.cache
+        data.result = roles.some(x => memRoles.has(x) || memRoles.find(y => y.name.toLowerCase() === x));
+    }
 
     return {
         code: d.util.setCode(data)
