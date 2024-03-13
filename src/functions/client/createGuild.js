92820a119f0ab303cf7f5c8700e9c28266bb72f2
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-13 12:27:01 +0100

    feat: $createGuild

---
+++ src/functions/client/createGuild.js
@@ -0,0 +1,20 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [ name, icon, returnId = "false" ] = data.inside.splits;
+
+    if (!name) return d.aoiError.fnError(d, "custom", { inside: data.inside } ,"Missing Guild Name")
+    
+    try {
+        const guild = await d.client.guilds.create({ name: name.addBrackets(), icon: icon.addBrackets() });
+
+        data.result = returnId === "true" ? guild.id : null;
+    } catch (err) {
+        return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Failed to create Guild with Reason: " + err)
+    }
+
+    return {
+        code: d.util.setCode(data)
+    }
+}
