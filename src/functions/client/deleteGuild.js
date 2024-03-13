3cdb11f49d0adcdcb7902ed6817af115ef5fd877
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-13 12:27:09 +0100

    feat: $deleteGuild

---
+++ src/functions/client/deleteGuild.js
@@ -0,0 +1,16 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [ guildId ] = data.inside.splits;
+
+    if (!guildId) return d.aoiError.fnError(d, "guild", { inside: data.inside })
+    
+    const guild = await d.util.getGuild(d, guildId);
+
+    guild.delete().catch((err) => d.aoiError.fnError(d, "custom", {}, "Failed to delete guild with Reason: " + err));
+
+    return {
+        code: d.util.setCode(data)
+    }
+}
