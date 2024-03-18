b382311121921f6aec1e2265bd0c6c40db96b12c
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-18 09:49:09 +0100

    feat: $clientShards[]: returns all shards of the client

---
+++ src/functions/client/clientShards.js
@@ -0,0 +1,37 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [format, separator = ", "] = data.inside.splits;
+
+    if (!d.client.shard) return d.aoiError.fnError(d, "custom", {}, "Your Bot requires sharding to use this function");
+
+    const results = await d.client.shard.broadcastEval((client, args) => {
+        const placeholders = {
+            "{readyTimestamp}": client.readyTimestamp,
+            "{uptime}": client.uptime,
+            "{humanizedUptime}": humanize(client.uptime),
+            "{guildCount}": client.guilds.cache.size,
+            "{userCount}": client.users.cache.size,
+            "{ping}": client.ws.ping,
+            "{shardId}" : client.shard.ids[0],
+        };
+
+        function humanize(ms) {
+            const seconds = Math.floor((ms / 1000) % 60);
+            const minutes = Math.floor((ms / (1000 * 60)) % 60);
+            const hours = Math.floor((ms / (1000 * 60 * 60)) % 24);
+            const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+        
+            return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        }
+
+        return args.format.replace(/{\w+}/g, x => placeholders[x] !== undefined ? placeholders[x] : x);
+    }, { context: { format: format.addBrackets() } });
+
+    data.result = results.join(separator);
+
+    return {
+        code: d.util.setCode(data)
+    };
+};
