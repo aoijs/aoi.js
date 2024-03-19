b0093a4ec40daead561f4a3e5fda56669526b845
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-20 00:00:28 +0100

    refactor(closeTicket): change db folder to __aoijs_vars__

--- src/functions/database/closeTicket.js
+++ src/functions/database/closeTicket.js
@@ -3,18 +3,30 @@ module.exports = async (d) => {
 
     let [error] = data.inside.splits;
 
-    const exists = await d.client.db.get(
-        d.client.db.tables[0],
-        `ticketChannel`,
-        d.channel?.id
-    );
-
     if (error) error = await d.util.errorParser(error, d);
 
-    if (!exists) return d.aoiError.makeMessageError(d.client, d.channel, error.data ?? error, error.options, d);
-    const channel = await d.message.channel.delete().catch((err) => {
-        d.aoiError.makeMessageError(d.client, d.channel, error, error.options, d);
+    const exists = await d.client.db.get("__aoijs_vars__", `ticketChannel`, d.channel?.id);
+
+    if (!exists) {
+        await d.aoiError.makeMessageError(d.client, d.channel, error.data ?? error, error.options);
+
+        return {
+            code: d.util.setCode(data)
+        };
+    }
+    
+    await d.message.channel.delete().catch(async () => {
+        if (error) {
+            await d.aoiError.makeMessageError(
+                d.client,
+                d.channel,
+                error.data ?? error,
+                error.options,
+            );
+        }
     });
+
+    await d.client.db.delete("__aoijs_vars__", `ticketChannel`, d.channel.id);
 
     return {
         code: d.util.setCode(data)
