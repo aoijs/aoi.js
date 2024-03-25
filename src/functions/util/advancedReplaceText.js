f5c4568947ce4beb84817482023fb3ddf8c461a0
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-25 23:59:06 +0100

    feat: advancedReplaceText[]

---
+++ src/functions/util/advancedReplaceText.js
@@ -0,0 +1,19 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [...replace] = data.inside.splits;
+    let [text, ...replacements] = replace;
+
+    for (let i = 0; i < replacements.length; i += 2) {
+        const replacer = replacements[i];
+        const replaceTo = replacements[i + 1];
+        text = text.replaceAll(replacer.addBrackets(), replaceTo.addBrackets());
+    }
+
+    data.result = text.addBrackets();
+
+    return {
+        code: d.util.setCode(data)
+    };
+};
