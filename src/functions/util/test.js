c24e936bf734066126add56624d518eb21c3848a
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-26 06:31:06 +0100

    feat: support proper regex flags

--- src/functions/util/test.js
+++ src/functions/util/test.js
@@ -4,8 +4,8 @@ module.exports = async (d) => {
 
     let [text, pattern, flag = "g"] = data.inside.splits;
     
-    if (!["g", "i", "m", "s", "u", "y"].includes(flag)) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Flag Provided In")
-    
+    if (![...flag].every(char => ["g", "i", "m", "s", "u", "y"].includes(char))) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Flag Provided In")
+        
     const regex = new RegExp(pattern, flag);
 
     data.result = regex.test(text);
