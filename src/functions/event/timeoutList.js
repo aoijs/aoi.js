64521258c2897a1aa9be2a7f4c44f505ddac04f7
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-26 07:05:13 +0100

    feat: timeoutList: returns list of on-going timeouts

---
+++ src/functions/event/timeoutList.js
@@ -0,0 +1,36 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [format = "key", separator = " , "] = data.inside.splits;
+
+    const timeouts = await d.client.db.findMany("__aoijs_vars__", (data) => {
+        return data.key.startsWith("setTimeout_");
+    });
+
+    if (timeouts) {
+        const timeoutData = timeouts.map((x) => {
+            if (format === "duration") {
+                return x.value.__duration__;
+            } else if (format === "key") {
+                return x.key;
+            } else if (format === "id") {
+                return x.value.__id__;
+            } else {
+                let timeout = format
+                    .replaceAll("{duration}", x.value.__duration__)
+                    .replaceAll("{key}", x.key)
+                    .replaceAll("{id}", x.value.__id__);
+                return timeout;
+            }
+        });
+
+        data.result = timeoutData.join(separator);
+    } else {
+        data.result = null;
+    }
+
+    return {
+        code: d.util.setCode(data)
+    };
+};
