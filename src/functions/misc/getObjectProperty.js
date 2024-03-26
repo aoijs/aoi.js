598a24f4dc651f683e14aad89ef6299d0aff6f51
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-26 06:41:13 +0100

    fix(bug): unable to return properties when outside of int limit

--- src/functions/misc/getObjectProperty.js
+++ src/functions/misc/getObjectProperty.js
@@ -1,23 +1,20 @@
 module.exports = (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
-  
+
     const [objectName, option] = data.inside.splits;
-  
+
     const object = d.data.objects?.[objectName];
     if (!object) return d.aoiError.fnError("Object not found");
-  
+
     try {
-      const evaled = eval(`object?.${option}`);
-      data.result = typeof evaled === "object"
-        ? JSON.stringify(evaled, null, 2)
-        : evaled ?? "undefined";
+        const evaled = object[option];
+        data.result = typeof evaled === "object" ? JSON.stringify(evaled) : evaled ?? "undefined";
     } catch (e) {
-      data.result = "undefined";
+        data.result = "undefined";
     }
-  
+
     return {
-      code: d.util.setCode(data),
+        code: d.util.setCode(data)
     };
-  };
-  
+};
