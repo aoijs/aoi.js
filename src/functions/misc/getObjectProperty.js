5c49edad23d2846b178bc7c70217753417e079cf
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-26 22:01:59 +0100

    fix: getObjectProperty

--- src/functions/misc/getObjectProperty.js
+++ src/functions/misc/getObjectProperty.js
@@ -5,32 +5,16 @@ module.exports = (d) => {
     const [objectName, option] = data.inside.splits;
 
     const object = d.data.objects?.[objectName];
-    if (!object) return d.aoiError.fnError(d, "custom", {}, "Invalid Object Provided In");
+    if (!object) return d.aoiError.fnError("Object not found");
 
     try {
-        const properties = option.split(".");
-        let evaled = object;
-
-        for (let prop of properties) {
-            let match;
-            if ((match = prop.match(/(\w+)((\[\d+\])*)/))) {
-                let arrayIndices = match[2].match(/\d+/g);
-                evaled = evaled[match[1]];
-                if (arrayIndices) {
-                    for (let index of arrayIndices) {
-                        evaled = evaled[parseInt(index)];
-                    }
-                }
-            } else if (prop in evaled) {
-                evaled = evaled[prop];
-            } else {
-                evaled = "undefined";
-                break;
-            }
+        if (option.startsWith("[")) {
+            data.result = eval(`object${option}`);
+        } else {
+            data.result = eval(`object.${option}`);
         }
-
-        data.result = typeof evaled === "object" ? JSON.stringify(evaled) : evaled ?? "undefined";
     } catch (e) {
+        console.log(e);
         data.result = "undefined";
     }
 
