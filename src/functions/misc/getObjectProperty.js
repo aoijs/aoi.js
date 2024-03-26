bb29ce9f6a58d133627f5664b918a8adca1b18e1
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-26 16:40:14 +0100

    fix: nested arrays

--- src/functions/misc/getObjectProperty.js
+++ src/functions/misc/getObjectProperty.js
@@ -5,10 +5,30 @@ module.exports = (d) => {
     const [objectName, option] = data.inside.splits;
 
     const object = d.data.objects?.[objectName];
-    if (!object) return d.aoiError.fnError("Object not found");
+    if (!object) return d.aoiError.fnError(d, "custom", {}, "Invalid Object Provided In");
 
     try {
-        const evaled = object[option];
+        const properties = option.split(".");
+        let evaled = object;
+
+        for (let prop of properties) {
+            let match;
+            if ((match = prop.match(/(\w+)((\[\d+\])*)/))) {
+                let arrayIndices = match[2].match(/\d+/g);
+                evaled = evaled[match[1]];
+                if (arrayIndices) {
+                    for (let index of arrayIndices) {
+                        evaled = evaled[parseInt(index)];
+                    }
+                }
+            } else if (prop in evaled) {
+                evaled = evaled[prop];
+            } else {
+                evaled = "undefined";
+                break;
+            }
+        }
+
         data.result = typeof evaled === "object" ? JSON.stringify(evaled) : evaled ?? "undefined";
     } catch (e) {
         data.result = "undefined";
