22b476f1afc6afd79a52f71948818a9f1647ea2f
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-26 06:40:52 +0100

    fix: add check for missing args

--- src/functions/util/test.js
+++ src/functions/util/test.js
@@ -3,6 +3,8 @@ module.exports = async (d) => {
     if (data.err) return d.error(data.err);
 
     let [text, pattern, flag = "g"] = data.inside.splits;
+
+    if (!text) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Text Provided In");
     
     if (![...flag].every(char => ["g", "i", "m", "s", "u", "y"].includes(char))) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Flag Provided In")
         
