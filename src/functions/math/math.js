dacf6401555997a661ab405fa78c125d3e0a198d
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-27 20:51:46 +0100

    fix: addBrackets

--- src/functions/math/math.js
+++ src/functions/math/math.js
@@ -6,7 +6,7 @@ module.exports = async (d) => {
 
     const regex =
         /(abs|acos|acosh|asin|asinh|atan|atan2|atanh|cbrt|ceil|clz32|cos|cosh|exp|expm1|floor|fround|hypot|imul|log|log10|log1p|log2|max|min|pow|random|round|sign|sin|sinh|sqrt|tan|tanh|trunc|LN10|LN2|LOG10E|LOG2E|PI|SQRT1_2|SQRT2)/g;
-    const math = numbers?.replaceAll(regex, "Math.$1").replaceAll("EULERNUM", "Math.E");
+    const math = numbers.addBrackets().replaceAll(regex, "Math.$1").replaceAll("EULERNUM", "Math.E");
 
     if (!math) {
         return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid math expression");
