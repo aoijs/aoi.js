abea5e801ddc29b21e3529f4d683995ef02eac0e
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-27 20:46:01 +0100

    fix: remove equation check

--- src/functions/math/math.js
+++ src/functions/math/math.js
@@ -1,59 +1,9 @@
-function isMathExpression(expression) {
-    expression = expression.trim();
-    const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-    const mathOperators = ["+", "-", "*", "/", "%", "**", "(", ")", "^", "|", "&", ">>", "<<"];
-    const MathClassFunctions = [
-        "abs",
-        "acos",
-        "acosh",
-        "asin",
-        "asinh",
-        "atan",
-        "atan2",
-        "atanh",
-        "cbrt",
-        "ceil",
-        "clz32",
-        "cos",
-        "cosh",
-        "exp",
-        "expm1",
-        "floor",
-        "fround",
-        "hypot",
-        "imul",
-        "log",
-        "log10",
-        "log1p",
-        "log2",
-        "max",
-        "min",
-        "pow",
-        "random",
-        "round",
-        "sign",
-        "sin",
-        "sinh",
-        "sqrt",
-        "tan",
-        "tanh",
-        "trunc"
-    ];
-    const MathClassProperties = ["EULERNUM", "LN10", "LN2", "LOG10E", "LOG2E", "PI", "SQRT1_2", "SQRT2"];
-    const ops = [...numbers, ...mathOperators, ...MathClassFunctions, ...MathClassProperties];
-
-    for (const op of ops) {
-        expression = expression.replaceAll(op, "");
-    }
-    return expression.trim() === "";
-}
-
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     const [numbers] = data.inside.splits;
 
-    if (numbers && !isMathExpression(numbers)) {
-        return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid math expression");
+    if (numbers) {
+        return d.aoiError.fnError(d, "custom", {}, "Invalid math expression");
     }
 
     const regex =
