aa51b2771cf499fd490ad55b53abe317b20ec43f
USERSATOSHI <user.satoshi69@gmail.com>
2024-03-29 20:58:56 +0530

    fixed timeout

--- src/handler/Custom/timeout.js
+++ src/handler/Custom/timeout.js
@@ -2,10 +2,7 @@ module.exports = async (d, name, duration, timeoutData, onReady) => {
     let cmds = d.client.cmd?.timeout.V();
 
     if (onReady) {
-        const datas = await d.client.db.all(
-            "__aoijs_vars__",
-            (data) => data.key.startsWith("setTimeout_"),
-        );
+        const datas = await d.client.db.all("__aoijs_vars__", (data) => data.key.startsWith("setTimeout_"));
         if (!datas) return;
         for (const data of datas.filter((x) => {
             return !x.value.__pulseEvery__;
@@ -16,67 +13,32 @@ module.exports = async (d, name, duration, timeoutData, onReady) => {
             cmds = cmds.filter((x) => x.name === t.__timeoutName__);
             if (dura > 0) {
                 if (dura > 2147483647) {
-                    const interval = setInterval(async () => {
-                        if (Date.now() >= t.__duration__) {
-                            clearInterval(interval);
-                            for (const cmd of cmds) {
-                                await d.interpreter(
-                                    d.client,
-                                    {},
-                                    [],
-                                    cmd,
-                                    d.client.db,
-                                    false,
-                                    undefined,
-                                    { timeoutData: t },
-                                );
+                    const interval = setInterval(
+                        async () => {
+                            if (Date.now() >= t.__duration__) {
+                                clearInterval(interval);
+                                for (const cmd of cmds) {
+                                    await d.interpreter(d.client, {}, [], cmd, d.client.db, false, undefined, { timeoutData: t });
+                                }
+                                await d.client.db.delete("__aoijs_vars__", data.key);
                             }
-                            await d.client.db.delete(
-                                "__aoijs_vars__",
-                                data.key,
-                            );
-                        }
-                    }, 1000 * 60 * 60);
+                        },
+                        1000 * 60 * 60
+                    );
                 } else {
                     for (const cmd of cmds) {
                         const timeout = setTimeout(async () => {
-                            await d.interpreter(
-                                d.client,
-                                {},
-                                [],
-                                cmd,
-                                d.client.db,
-                                false,
-                                undefined,
-                                { timeoutData: t },
-                            );
+                            await d.interpreter(d.client, {}, [], cmd, d.client.db, false, undefined, { timeoutData: t });
                             t.__timeoutIds__.push(timeout[Symbol.toPrimitive]());
 
-                            await d.client.db.delete(
-                                "__aoijs_vars__",
-                                data.key,
-                            );
+                            await d.client.db.delete("__aoijs_vars__", data.key);
                         }, dura);
                     }
                 }
-                d.client.db.set(
-                    "__aoijs_vars__",
-                    "setTimeout",
-                    t.__id__,
-                    t,
-                );
+                d.client.db.set("__aoijs_vars__", "setTimeout", t.__id__, t);
             } else {
                 for (const cmd of cmds) {
-                    await d.interpreter(
-                        d.client,
-                        {},
-                        [],
-                        cmd,
-                        d.client.db,
-                        false,
-                        undefined,
-                        { timeoutData: t },
-                    );
+                    await d.interpreter(d.client, {}, [], cmd, d.client.db, false, undefined, { timeoutData: t });
                 }
                 await d.client.db.delete("__aoijs_vars__", data.key);
                 continue;
@@ -90,63 +52,32 @@ module.exports = async (d, name, duration, timeoutData, onReady) => {
         const ids = [];
         for (const cmd of cmds) {
             if (duration > 2147483647) {
-                const interval = setInterval(async () => {
-                    if (Date.now() >= timeoutData.__duration__) {
-                        clearInterval(interval);
-                        await d.interpreter(
-                            d.client,
-                            {},
-                            [],
-                            cmd,
-                            d.client.db,
-                            false,
-                            undefined,
-                            { timeoutData },
-                        );
-                        await d.client.db.delete(
-                            "__aoijs_vars__",
-                            timeoutData.__id__,
-                        );
-                    }
-                }, 1000 * 60 * 60);
+                const interval = setInterval(
+                    async () => {
+                        if (Date.now() >= timeoutData.__duration__) {
+                            clearInterval(interval);
+                            await d.interpreter(d.client, {}, [], cmd, d.client.db, false, undefined, { timeoutData });
+                            await d.client.db.delete("__aoijs_vars__", "setTimeout", timeoutData.__id__);
+                        }
+                    },
+                    1000 * 60 * 60
+                );
                 ids.push(interval[Symbol.toPrimitive]());
             } else {
                 const timeout = setTimeout(async () => {
-                    await d.interpreter(
-                        d.client,
-                        {},
-                        [],
-                        cmd,
-                        d.client.db,
-                        false,
-                        undefined,
-                        { timeoutData },
-                    );
+                    await d.interpreter(d.client, {}, [], cmd, d.client.db, false, undefined, { timeoutData });
 
                     ids.splice(ids.indexOf(timeout[Symbol.toPrimitive]()), 1);
                     if (ids.length === 0) {
-                        await d.client.db.delete(
-                            "__aoijs_vars__",
-                            timeoutData.__id__,
-                        );
+                        await d.client.db.delete("__aoijs_vars__", "setTimeout", timeoutData.__id__);
                     } else {
-                        await d.client.db.set(
-                            "__aoijs_vars__",
-                            "setTimeout",
-                            timeoutData.__id__,
-                            timeoutData,
-                        );
+                        await d.client.db.set("__aoijs_vars__", "setTimeout", timeoutData.__id__, timeoutData);
                     }
                 }, duration);
                 ids.push(timeout[Symbol.toPrimitive]());
             }
         }
         timeoutData.__timeoutIds__ = ids;
-        await d.client.db.set(
-            "__aoijs_vars__",
-            "setTimeout",
-            timeoutData.__id__,
-            timeoutData,
-        );
+        await d.client.db.set("__aoijs_vars__", "setTimeout", timeoutData.__id__, timeoutData);
     }
 };
