0a9d2ad87cd42f34d438e38075dfc088c5b2f5e4
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-03-31 21:33:36 +0200

    fix objects
    I dont like json

--- src/functions/misc/setObjectProperty.js
+++ src/functions/misc/setObjectProperty.js
@@ -1,24 +1,28 @@
-module.exports = async d => {
-  const data = d.util.aoiFunc(d);
-  const { code } = d.command;
-  if (data.err) return d.error(data.err);
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    const { code } = d.command;
+    if (data.err) return d.error(data.err);
 
-  let [name, property, value] = data.inside.splits;
-  if (!d.data.objects) return d.aoiError.fnError(d, "custom", {}, "object")
+    let [name, property, value] = data.inside.splits;
+    if (!d.data.objects) return d.aoiError.fnError(d, "custom", {}, "object");
 
-  try {
-      value = JSON.parse(value)
-  } catch (e) {
-      value = value
-  }
+    try {
+        value = JSON.parse(value);
+    } catch (e) {
+        value = value;
+    }
 
-  const object = d.data.objects[name] || {};
-  eval(`object.${property} = value`)
-  
-  d.data.objects[name] = object;
+    const object = d.data.objects[name] || {};
+    if (property.startsWith("[")) {
+        eval(`object${property} = value`);
+    } else {
+        eval(`object.${property} = value`);
+    }
+    
+    d.data.objects[name] = object;
 
-  return {
-      code: d.util.setCode({ function: d.func, code: code, inside: data.inside }),
-      data: { ...d.data, objects: { ...d.data.objects }},
-  }
-}
+    return {
+        code: d.util.setCode({ function: d.func, code: code, inside: data.inside }),
+        data: { ...d.data, objects: { ...d.data.objects } }
+    };
+};
