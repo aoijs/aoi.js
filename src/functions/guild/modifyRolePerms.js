2ceb8bf235ad7ed5610871a245723d0df86671d2
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2024-04-01 09:38:54 -0700

    Fixed actually

--- src/functions/guild/modifyRolePerms.js
+++ src/functions/guild/modifyRolePerms.js
@@ -1,4 +1,5 @@
-const { Permissions } = require('../../utils/Constants.js');
+const {Permissions} = require('../../utils/Constants.js');
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
@@ -7,35 +8,40 @@ module.exports = async d => {
     const [guildID, roleID, ...perms] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildID);
-    if (!guild) return d.aoiError.fnError(d, 'guild', { inside: data.inside });
+    if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
 
     const role = await guild.roles.fetch(roleID).catch(e => undefined);
-    if (!role) return d.aoiError.fnError(d, 'role', { inside: data.inside });
+    if (!role) return d.aoiError.fnError(d, 'role', {inside: data.inside});
 
-    let newPerms = BigInt(0);
-
+    let arrayPerms = role.permissions;
+    arrayPerms = Array.from(arrayPerms);
     if (perms.includes('+all')) {
-        newPerms = Permissions.ALL;
+        arrayPerms = ['Administrator'];
     } else if (perms.includes('-all')) {
-        newPerms = BigInt(0);
+        arrayPerms = [];
     } else {
         for (const perm of perms) {
             const sign = perm.slice(0, 1);
-            const permission = Permissions[perm.slice(1)];
 
             if (sign === '+') {
-                newPerms |= BigInt(permission); 
+                arrayPerms.push(Permissions[perm.slice(1)]);
             } else if (sign === '-') {
-                newPerms &= ~BigInt(permission); 
+                const permissions = new PermissionsBitField(Permissions[perm.slice(1)]);
+                const permissionNames = permissions.toArray();
+                const index = arrayPerms.indexOf(permissionNames.toString());
+                if (index !== -1) {
+                    arrayPerms.splice(index, 1);
+                }
             }
+            arrayPerms = Array.from(arrayPerms);
         }
     }
 
-    role.setPermissions(newPerms).catch(e => {
-        d.aoiError.fnError(d, 'custom', { inside: data.inside }, 'Failed To Modify Channel Permissions With Reason: ' + e);
+   let abc = role.setPermissions(arrayPerms).catch(e => {
+        d.aoiError.fnError(d, 'custom', {inside: data.inside}, 'Failed To Modify Channel Permissions With Reason: ' + e);
     });
 
     return {
         code: d.util.setCode(data)
-    };
-};
+    }
+}
