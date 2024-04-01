190eb7250550d6149cf156d27dcaafdac2c1e5ab
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2024-04-01 09:46:40 -0700

    Fixed modify role perms ( should ) (#567)

--- src/functions/guild/modifyRolePerms.js
+++ src/functions/guild/modifyRolePerms.js
@@ -1,4 +1,5 @@
 const {Permissions} = require('../../utils/Constants.js');
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
@@ -13,6 +14,7 @@ module.exports = async d => {
     if (!role) return d.aoiError.fnError(d, 'role', {inside: data.inside});
 
     let arrayPerms = role.permissions;
+    arrayPerms = Array.from(arrayPerms);
     if (perms.includes('+all')) {
         arrayPerms = ['Administrator'];
     } else if (perms.includes('-all')) {
@@ -22,9 +24,14 @@ module.exports = async d => {
             const sign = perm.slice(0, 1);
 
             if (sign === '+') {
-                arrayPerms.add(Permissions[perm.slice(1)]);
+                arrayPerms.push(Permissions[perm.slice(1)]);
             } else if (sign === '-') {
-                arrayPerms.remove(Permissions[perm.slice(1)]);
+                const permissions = new PermissionsBitField(Permissions[perm.slice(1)]);
+                const permissionNames = permissions.toArray();
+                const index = arrayPerms.indexOf(permissionNames.toString());
+                if (index !== -1) {
+                    arrayPerms.splice(index, 1);
+                }
             }
             arrayPerms = Array.from(arrayPerms);
         }
