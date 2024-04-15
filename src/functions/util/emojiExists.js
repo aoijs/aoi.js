cf06fe95261e06cbd920725b729332575fdcc593
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-04-15 10:10:50 +0200

    use Extended_Pictographic for regex

--- src/functions/util/emojiExists.js
+++ src/functions/util/emojiExists.js
@@ -17,7 +17,7 @@ module.exports = async (d) => {
         if (!emoji) {
             data.result = false;
         } else {
-            const regex = /[\u{1F300}-\u{1F5FF}\u{1F900}-\u{1F9FF}\u{1FA70}-\u{1FAFF}\u{2600}-\u{27BF}\u{1F680}-\u{1F6FF}\u{24C2}-\u{1F251}\u{1F600}-\u{1F64F}]/u;
+            const regex = /\p{Extended_Pictographic}/gu;
             data.result = regex.test(emoji);
         }
     }
