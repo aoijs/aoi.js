8ae2dfb95daa0bc6823e19cd130c702fb7145b1f
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2024-04-17 10:40:51 +0700

    PR for $isValidObject (#580)
    * Update modifyRolePerms.js
    
    fixed TypeError: arrayPerms.add is not a function maybe
    
    * fixed it cuz i broke it even worse should be better now tho
    
    * was forced to remove commits
    
    * Fixed actually
    
    * remove not needed var
    
    * fixed function
    
    ---------
    
    Co-authored-by: Fafa <87046111+Faf4a@users.noreply.github.com>

--- src/functions/util/isValidObject.js
+++ src/functions/util/isValidObject.js
@@ -3,14 +3,17 @@ module.exports = d => {
     if (data.err) return d.error(data.err);
 
     let [json] = data.inside.splits;
-    let validObject = false;
+    let validObject
 
     try {
         json = JSON.parse(json.addBrackets());
         validObject = true;
 
     } catch (e) {
+        validObject = false;
     }
+
+    data.result = validObject
 
     return {
         code: d.util.setCode(data)
