fa7f9b554b4140c2da1e01f54d23f1b26679c256
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-05-07 12:29:32 +0200

    add json support for createPoll

--- src/functions/message/createPoll.js
+++ src/functions/message/createPoll.js
@@ -7,40 +7,48 @@ module.exports = async (d) => {
 
     const [channelId = d.channel?.id, question, duration, allowMultiselect = "false", ...answers] = data.inside.splits;
 
+    let poll = question;
     const channel = await d.util.getChannel(d, channelId);
-    const time = Time.parse(duration)?.ms / 3600000;
 
-    if (!channel) return d.aoiError.fnError(d, "channel", { inside: data.inside});
-    if (!time) return d.aoiError.fnError(d, "custom", { inside: data.inside}, "Invalid Time Provided In")
-    if (Math.floor(time) > 168 ) return d.aoiError.fnError(d, "custom", { inside: data.inside}, "Poll duration must be less than or equal to 7 days")
-    if (Math.floor(time) < 1 ) return d.aoiError.fnError(d, "custom", { inside: data.inside}, "Poll duration must be longer than or equal 1 hour")
+    try {
+        poll = JSON.parse(poll);
+    } catch {
+        const time = Time.parse(duration)?.ms / 3600000;
 
-    answers.forEach((x) => {
-        const answer = x.split(":");
-        let emoji = answer[1];
+        if (!channel) return d.aoiError.fnError(d, "channel", { inside: data.inside });
+        if (!time) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Time Provided In");
+        if (Math.floor(time) > 168) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Poll duration must be less than or equal to 7 days");
+        if (Math.floor(time) < 1) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Poll duration must be longer than or equal 1 hour");
 
-        try {
-            emoji = d.util.getEmoji(d, emoji.addBrackets()).id;
-        } catch {
-            emoji = emoji?.addBrackets() ?? undefined;
-        }
+        answers.forEach((x) => {
+            const answer = x.split(":");
+            let emoji = answer[1];
 
-        answers[answers.indexOf(x)] = {
-            text: answer[0],
-            emoji
-        };
-    });
+            try {
+                emoji = d.util.getEmoji(d, emoji.addBrackets()).id;
+            } catch {
+                emoji = emoji?.addBrackets() ?? undefined;
+            }
 
-    if (answers.length > 10) return d.aoiError.fnError(d, "custom", { inside: data.inside}, "Poll answers must be less than or equal to 10");
+            answers[answers.indexOf(x)] = {
+                text: answer[0],
+                emoji
+            };
+        });
 
-    channel.send({
-        poll: {
+        if (answers.length > 10) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Poll answers must be less than or equal to 10");
+
+        poll = {
             question: { text: question },
             duration: time,
             allowMultiselect: allowMultiselect === "true",
             layout: PollLayoutType.Default,
             answers
-        }
+        };
+    }
+
+    channel.send({
+        poll: poll
     });
 
     return {
