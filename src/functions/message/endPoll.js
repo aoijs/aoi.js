ddb59c06597ec87341680f193c5843eab70ffa8d
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-05-13 01:15:04 +0200

    endPoll

---
+++ src/functions/message/endPoll.js
@@ -0,0 +1,18 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+
+    const [channelId = d.channel?.id, messageId = d.message?.id] = data.inside.splits;
+
+    const channel = await d.util.getChannel(d, channelId);
+    if (!channel) return d.aoiError.fnError(d, "channel", { inside: data.inside});
+
+    const message = await d.util.getMessage(channel, messageId);
+    if (!message) return d.aoiError.fnError(d, "message", { inside: data.inside});
+
+    channel.messages.endPoll(message.id);
+
+    return {
+        code: d.util.setCode(data)
+    };
+};
