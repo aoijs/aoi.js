13a3d090a4596915d7d604fff10a0e83f20ea233
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-05-13 01:06:33 +0200

    resolve conflicts

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -44,7 +44,18 @@ for (let i = 1; i <= 12; i++) {
     );
 }
 
-
+const ApplicationCmdOptions = {
+    id: "id of the slash cmd;.id",
+    name: "name of the slash cmd;.name",
+    description: "description of the slash cmd;.description",
+    version: "version of slash cmd;.version",
+    options: "options of slash cmd;.options",
+    guildID: "guildID of the slash cmd (returns null for global);.guildID",
+    applicationID: "returns Application ID",
+    defaultPermission: "returns default permission of the slash cmd;.defaultPermission",
+    timestamp: "returns timestamp of the creation of slash cmd (in ms);.timestamp",
+    createdAt: "returns the date of creation of slash cmd;.createdAt"
+};
 const ChannelOptions = {
     createdAt: "createdAt",
     createdTimestamp: "createdTimestamp",
@@ -440,13 +451,14 @@ const EventstoFile = {
     onAutoModerationActionExecution: "autoModActionExecution",
     onAutoModerationRuleDelete: "autoModCreate",
     onAutoModerationRuleCreate: "autoModDelete",
-    onAutoModerationRuleUpdate: "autoModUpdate",
+    onAutoModerationRuleUpdate: "autoModUpdate"
 };
 
 const AllEvents = Object.keys(EventstoFile);
 
 module.exports = {
     FormatOptions,
+    ApplicationCmdOptions,
     ActivityTypeAvailables,
     EventAvailables,
     SI_SYMBOL,
