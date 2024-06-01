d9bfa1b6320b2546089a1e7100ff20252a68d70e
Leref <12578483+Leref@users.noreply.github.com>
2024-06-01 11:46:53 -0400

    revert aoiwarning

--- src/handler/AoiWarning.js
+++ src/handler/AoiWarning.js
@@ -1,65 +1,41 @@
-const { Agent, fetch } = require("undici");
+const { Agent, fetch } = require('undici');
 const json = require("../../package.json");
 const AoiError = require("../classes/AoiError.js");
 
-const compareVersions = (a, b) =>
-    a
-        .split(".")
-        .map(Number)
-        .some((v, i) => v > (b.split(".")[i] || 0));
-
 module.exports = async () => {
     try {
-        const res = await fetch("https://registry.npmjs.org/aoi.js", {
+        const res = await fetch('https://registry.npmjs.org/aoi.js', {
             dispatcher: new Agent({
                 keepAliveTimeout: 10000, // 10 seconds
                 keepAliveMaxTimeout: 15000 // 15 seconds
             }),
             headers: {
-                "User-Agent": "aoi.js" // required by npm registry API
+                'User-Agent': 'aoi.js' // required by npm registry API
             }
         });
 
         const data = await res.json();
-        const latestVersion = data["dist-tags"].latest;
+        const latestVersion = data['dist-tags'].latest;
+        const isDevVersion = json.version.includes('dev');
 
-        if (compareVersions(json.version, latestVersion) === false && json.version !== latestVersion) {
+        if (!isDevVersion && json.version !== latestVersion) {
             AoiError.createConsoleMessage(
-                [
-                    {
-                        text: 'aoi.js is outdated! Update with "npm install aoi.js@latest".',
-                        textColor: "red"
-                    }
-                ],
-                "white",
-                { text: "AoiWarning", textColor: "yellow" }
+                [{ text: 'AoiWarning: aoi.js is outdated! Update with "npm install aoi.js@latest".', textColor: 'red' }],
+                'white',
+                { text: 'AoiWarning', textColor: 'yellow' }
             );
-        } else if (json.version.includes("dev") || compareVersions(json.version, latestVersion) === true) {
+        } else if (isDevVersion) {
             AoiError.createConsoleMessage(
-                [
-                    {
-                        text: "You are currently on a development version.",
-                        textColor: "red"
-                    },
-                    {
-                        text: "This version may or may not contain unfinished features.",
-                        textColor: "red"
-                    }
-                ],
-                "white",
-                { text: "AoiWarning  ", textColor: "yellow" }
+                [{ text: 'AoiWarning: You are currently on a development version.', textColor: 'yellow' }],
+                'white',
+                { text: 'AoiWarning', textColor: 'yellow' }
             );
         }
     } catch (error) {
         AoiError.createConsoleMessage(
-            [
-                {
-                    text: `AoiWarning: Failed to check for updates: ${error.message}`,
-                    textColor: "white"
-                }
-            ],
-            "red",
-            { text: "AoiWarning", textColor: "yellow" }
+            [{ text: `AoiWarning: Failed to check for updates: ${error.message}`, textColor: 'white' }],
+            'red',
+            { text: 'AoiWarning', textColor: 'yellow' }
         );
     }
 };
