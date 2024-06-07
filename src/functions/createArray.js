8ed95e9eb5170680fabca3dbc7278bc976486df2
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2024-06-07 06:17:41 +0530

    Edit: Added JSON support

--- src/functions/createArray.js
+++ src/functions/createArray.js
@@ -1,14 +1,23 @@
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
-		if(data.err) return d.error(data.err);
+                  if(data.err) return d.error(data.err);
+                  let [name, ...elements] = data.inside.splits;    
 
-		const [name,...elements] = data.inside.splits;
-		d.arrays[name] = elements;
-		d.data.arrays = d.arrays;
-		
-		return {
-			code: d.util.setCode(data),
-			arrays: d.arrays,
-			data:d.data,
-		}
+    if (elements[0]?.startsWith("[") && elements[0]?.endsWith("]")) {
+        const element = elements[0];
+        try {
+            elements = JSON.parse(element);
+        } catch (err) {
+            d.aoiError.fnError(d, "custom", {}, `${err}`); 
+        };
+    };
+
+                  d.arrays[name] = elements;
+                  d.data.arrays = d.arrays;
+
+                  return {
+                            code: d.util.setCode(data),
+                            arrays: d.arrays,
+                            data: d.data,
+                  }
 }
