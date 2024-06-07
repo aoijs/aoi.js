5c40246deb69bbeb2abe896fa4d119d21431fd10
berticulousnert <91563714+berticulousnert@users.noreply.github.com>
2024-06-07 10:39:37 -0500

    fixed getarray

--- src/functions/getArray.js
+++ src/functions/getArray.js
@@ -3,10 +3,18 @@ module.exports = async (d) => {
     if (data.err) return d.error(data.err);
 
     const [name] = data.inside.splits;
+    const array = d.data.arrays[name];
+    let ArrayResult;
 
     if (!d.data.arrays?.[name]) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Array with name '" + name + "' does not exist.");
+    
+    if (array.length === 0 || (array.length === 1 && array[0] === '')) {
+        ArrayResult = "[]";
+    } else {
+        ArrayResult = JSON.stringify(array);
+    }
 
-    data.result = JSON.stringify(d.arrays[name]);
+    data.result = ArrayResult;
 
     return {
         code: d.util.setCode(data)
