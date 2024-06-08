36cb93296a4f63a6abead81653792c91cc84cb28
Ayaka <ayakagaming8@gmail.com>
2024-06-09 00:48:26 +0500

    refactor: updated sub to builder

--- lib/aoi.js/src/functions/js/maths/$sub.ts
+++ lib/aoi.js/src/functions/js/maths/$sub.ts
@@ -1,50 +1,44 @@
-import { TranspilerError } from '../../../core/error.js';
+import AoiJSFunction from '../../../structures/AoiJSFunction.js';
+import { escapeMathResult, parseResult } from '../../../util/transpilerHelpers.js';
 import { TranspilerCustoms } from '../../../typings/enums.js';
-import { type FunctionData } from '../../../typings/interfaces.js';
-import {
-	escapeMathResult,
-	parseResult,
-} from '../../../util/transpilerHelpers.js';
 
-export const $sub: FunctionData = {
-	name: '$sub',
-	type: 'getter',
-	brackets: true,
-	optional: false,
-	fields: [
+const sub = new AoiJSFunction()
+	.setName('$sub')
+	.setType('getter')
+	.setBrackets(true)
+	.setOptional(false)
+	.setField(
 		{
 			name: 'numbers',
 			type: 'number',
 			description: 'The numbers to subtract',
 			required: true,
-		},
-	],
-	version: '7.0.0',
-	default: ['void'],
-	returns: 'number',
-	description: 'Returns the sub of the numbers',
-	example: `
-    $sub[1;2] // returns -1
-    $sub[7;4;3] // returns 0
-    `,
-	code: (data, scope) => {
+		}
+	)
+	.setVersion('7.0.0')
+	.setDefault(['void'])
+	.setReturns('number')
+	.setDescription('Returns the sub of the numbers')
+	.setExample(`
+		$sub[1;2] // returns -1
+		$sub[7;4;3] // returns 0
+	`)
+	.setCode((data, scope, thisArg) => {
 		const numbers = data.splits;
 		const currentScope = scope[scope.length - 1];
 		if (
 			data.splits.length === 0 &&
-            !currentScope.name.startsWith('$try_') &&
-            !currentScope.name.startsWith('$catch_')
+			!currentScope.name.startsWith('$try_') &&
+			!currentScope.name.startsWith('$catch_')
 		) {
-			throw new TranspilerError(
-				`${data.name} requires at least 1 argument`,
-			);
+			throw new Error(`${data.name} requires at least 1 argument`);
 		}
 
 		const sub = numbers
 			.map((x) =>
 				x.includes(TranspilerCustoms.FS) ||
-                x.includes('__$DISCORD_DATA$__') ||
-                x.includes(TranspilerCustoms.MFS)
+				x.includes('__$DISCORD_DATA$__') ||
+				x.includes(TranspilerCustoms.MFS)
 					? parseResult(x.trim())
 					: Number(x),
 			)
@@ -52,9 +46,14 @@ export const $sub: FunctionData = {
 
 		const res = escapeMathResult(`(${sub})`);
 		currentScope.update(res, data);
+
 		return {
 			code: res,
 			scope,
 		};
-	},
-};
+	});
+
+export const $sub = sub.build();
+
+
+	
