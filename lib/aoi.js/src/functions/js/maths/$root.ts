fb8b634719911fba0f8af65e79338491f14f2e43
Ayaka <ayakagaming8@gmail.com>
2024-06-09 00:37:07 +0500

    refactor: updated root to builder

--- lib/aoi.js/src/functions/js/maths/$root.ts
+++ lib/aoi.js/src/functions/js/maths/$root.ts
@@ -1,88 +1,62 @@
-import { TranspilerError } from '../../../core/error.js';
+import AoiJSFunction from '../../../structures/AoiJSFunction.js';
+import { escapeMathResult, parseResult } from '../../../util/transpilerHelpers.js';
 import { TranspilerCustoms } from '../../../typings/enums.js';
-import { type FunctionData } from '../../../typings/interfaces.js';
-import {
-	escapeMathResult,
-	escapeResult,
-	parseResult,
-} from '../../../util/transpilerHelpers.js';
 
-export const $root: FunctionData = {
-	name: '$root',
-	type: 'getter',
-	brackets: true,
-	optional: false,
-	fields: [
+const pow = new AoiJSFunction()
+	.setName('$pow')
+	.setType('getter')
+	.setBrackets(true)
+	.setOptional(false)
+	.setField(
 		{
 			name: 'numbers',
 			type: 'number',
-			description: 'The numbers to get the root from',
+			description: 'The numbers to get the power from',
 			required: true,
-		},
-	],
-	version: '7.0.0',
-	default: ['void'],
-	returns: 'number',
-	description: 'Returns the root of the numbers',
-	example: `
-    $root[4;2] // returns 2
-    $root[27;3] // returns 3
-
-    $root[625;4;2] // returns 5
-`,
-	code: (data, scope) => {
+		}
+	)
+	.setVersion('7.0.0')
+	.setDefault(['void'])
+	.setReturns('number')
+	.setDescription('Returns the power of the numbers')
+	.setExample(`
+		$pow[1;2] // returns 1
+		$pow[7;4;3] // returns 2401
+	`)
+	.setCode((data, scope, thisArg) => {
 		const numbers = data.splits;
 		const currentScope = scope[scope.length - 1];
 		if (
 			data.splits.length === 0 &&
-            !currentScope.name.startsWith('$try_') &&
-            !currentScope.name.startsWith('$catch_')
+			!currentScope.name.startsWith('$try_') &&
+			!currentScope.name.startsWith('$catch_')
 		) {
-			throw new TranspilerError(
-				`${data.name} requires at least 1 argument`,
-			);
+			throw new Error(`${data.name} requires at least 1 argument`);
 		}
 
-		if (!currentScope.functions.includes('function nthRoot')) {
-			currentScope.functions += (`function nthRoot(x, n) {
-                try {
-    var negate = n % 2 == 1 && x < 0;
-    if(negate)
-      x = -x;
-    var possible = Math.pow(x, 1 / n);
-    n = Math.pow(possible, n);
-    if(Math.abs(x - n) < 1 && (x > 0 == n > 0))
-      return negate ? -possible : possible;
-  } catch(e){}
-}\n`);
-		}
+		const pow = numbers
+			.map((x) =>
+				x.includes(TranspilerCustoms.FS) ||
+				x.includes('__$DISCORD_DATA$__') ||
+				x.includes(TranspilerCustoms.MFS)
+					? parseResult(x.trim())
+					: Number(x),
+			)
+			.join('**');
 
-		const root = numbers.map((x) =>
-			x.includes(TranspilerCustoms.FS) ||
-            x.includes('__$DISCORD_DATA$__') ||
-            x.includes(TranspilerCustoms.MFS)
-				? parseResult(x.trim())
-				: Number(x),
-		);
-		const rec = (a: string | number, b: string | number) => {
-			let ans = '';
-			let i = 2;
-			while (i <= root.length) {
-				ans = `nthRoot(${a}, ${b})`;
-				i += 1;
-				a = ans;
-				b = root[ i - 1 ];
-			}
-
-			return ans;
-		};
-
-		const res =  escapeResult(escapeMathResult(`(${rec(root[0], root[1])})`));
+		const res = escapeMathResult(`(${pow})`);
 		currentScope.update(res, data);
+
 		return {
 			code: res,
 			scope,
 		};
-	},
-};
+	});
 
+export const $pow = pow.build();
+
+
+	
+
+		
+		
