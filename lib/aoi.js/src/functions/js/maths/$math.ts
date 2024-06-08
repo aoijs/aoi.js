4939cb735ed68bb707c2d131b017ded327750678
Ayaka <ayakagaming8@gmail.com>
2024-06-09 02:22:19 +0500

    refactor: updated  to builder

--- lib/aoi.js/src/functions/js/maths/$math.ts
+++ lib/aoi.js/src/functions/js/maths/$math.ts
@@ -3,7 +3,6 @@ import { escapeMathResult, parseResult, removeF, removeFF, removeMF } from '../.
 
 function isMathExpression(expression: string): boolean {
 	expression = parseResult( removeFF( removeMF( removeF( expression.trim() ) ) ) );
-	const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',', '-'];
 	const mathOperators = ['+', '-', '*', '/', '%', '**', '(', ')', '^', '|', '&', '>>', '<<'];
 	const MathClassFunctions = [
 		'abs',
