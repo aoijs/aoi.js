0e48a858a7223b19f9f077150ef010fc45169219
Ayaka <ayakagaming8@gmail.com>
2024-06-09 15:30:33 +0500

    refactor: updated $root to builder

--- lib/aoi.js/src/functions/js/maths/$root.ts
+++ lib/aoi.js/src/functions/js/maths/$root.ts
@@ -38,17 +38,19 @@ const root = new AoiJSFunction()
 
 		if (!currentScope.functions.includes('function nthRoot')) {
 			currentScope.functions += (`function nthRoot(x, n) {
-				try {
-					var negate = n % 2 == 1 && x < 0;
-					if(negate)
-						x = -x;
-					var possible = Math.pow(x, 1 / n);
-					n = Math.pow(possible, n);
-					if(Math.abs(x - n) < 1 && (x > 0 == n > 0))
-						return negate ? -possible : possible;
-				} catch(e){}
-			}\n`);
-		}
+			
+                function nthRoot(x, n) {
+                 try {
+            var negate = n % 2 == 1 && x < 0;
+            if (negate) x = -x;
+         var possible = Math.pow(x, 1 / n);
+         n = Math.pow(possible, n);
+        if (Math.abs(x - n) < 1 && (x > 0 == n > 0)) return negate ? -possible : possible;
+        } catch (e) {}
+        }
+
+thisArg.updateFunction(currentScope, nthRoot, [])
+
 
 		const root = numbers.map((x) =>
 			x.includes(TranspilerCustoms.FS) ||
