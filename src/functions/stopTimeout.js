84856294ac4caeece8f888f51a655ab5b91e10d8
faf4a <87046111+Faf4a@users.noreply.github.com>
2024-06-28 18:42:24 +0200

    fix: stopTimeout

--- src/functions/stopTimeout.js
+++ src/functions/stopTimeout.js
@@ -3,16 +3,20 @@ module.exports = async (d) => {
     if (data.err) return d.error(data.err);
 
     const [id] = data.inside.splits;
+    const MAX_SAFE_INTEGER = 0x7FFFFFFF;
 
     const timeout = await d.client.db.get(
         "__aoijs_vars__",
         "setTimeout",
         id,
     );
-    if (!timeout) return d.error(`Timeout with ID: ${id} not found.`);
+    if (!timeout) return d.error(`AoiError: Timeout ID ${id} not found.`);
 
-    timeout.value.__timeoutIds__.forEach((x) => clearTimeout(x));
-    timeout.value.__pulseIds__?.forEach((x) => clearInterval(x));
+    if ((timeout.value.__duration__ - Date.now()) <= MAX_SAFE_INTEGER) {
+        clearTimeout(timeout.value.__id__);
+    } else {
+        clearInterval(timeout.value.__id__)
+    }
 
     await d.client.db.delete("__aoijs_vars__", "setTimeout", id);
 
