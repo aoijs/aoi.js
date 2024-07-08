6ea32875a1e2d43b4264202880b7b708e95a98d7
Saia <78155936+SaiaAmako5@users.noreply.github.com>
2024-07-08 08:00:49 +0530

    Update editEmbed.js
    Changes done as asked.

--- src/functions/editEmbed.js
+++ src/functions/editEmbed.js
@@ -1,10 +1,10 @@
 module.exports = async (d) => {
   const data = d.util.aoiFunc(d);
-  var [
+  let [
     channelID = d.channel?.id,
     messageID = d.message?.id,
     index = 1,
-    option = "description",
+    option,
     content,
   ] = data.inside.splits;
   index = index - 1;
@@ -14,11 +14,13 @@ module.exports = async (d) => {
   const message = await d.util.getMessage(channel, messageID);
   if (!message)
     return d.aoiError.fnError(d, "message", {}, { inside: data.inside });
-  if (isNaN(index))
+  if (isNaN(index) && index < 0)
     return d.aoiError.fnError(d, "custom", {}, "Invalid Index");
   if (!content)
     return d.aoiError.fnError(d, "custom", {}, "No Content Provided");
-  var embed = new d.embed(message.embeds[index]);
+  if (!option)
+    return d.aoiError.fnError(d, "custom", {}, "No Content Provided");
+  let embed = new d.embed(message.embeds[index]);
   if (option.startsWith("field")) {
     const fi = option.substring(option.indexOf("field") + 1).match(/\d+/) - 1;
     option = "fields[" + fi + "]." + option.substring(option.indexOf(".") + 1);
@@ -27,9 +29,8 @@ module.exports = async (d) => {
       const ts = new Date(Number(content));
       content = ts.toISOString();
     } catch {
-      d.aoiError.fnError(d, "custom", {}, "Invalid MS Provided");
-      return;
-    }
+     return d.aoiError.fnError(d, "custom", {}, "Invalid MS Provided");
+       }
   } else {
     option = option
       .replaceAll("authorname", "author.name")
@@ -43,12 +44,11 @@ module.exports = async (d) => {
     }
   }
   try {
-    if (!eval(`embed.data.${option}`))
+    if (!embed.data[option])
       return d.aoiError.fnError(d, "custom", {}, "Invalid Option Provided");
-    eval(`embed.data.${option} = content.addBrackets()`);
+    embed.data[option] = content.addBrackets();
   } catch {
-    d.aoiError.fnError(d, "custom", {}, "Option/Index Not Found");
-    return;
+   return d.aoiError.fnError(d, "custom", {}, "Option/Index Not Found");
   }
   message.edit({ embeds: [embed] }).catch((e) => {
     d.aoiError.fnError(d, "custom", {}, e);
