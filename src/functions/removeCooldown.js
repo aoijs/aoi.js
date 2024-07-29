40a0937630e2d00dd9287bf7c5c7bcf6c12f605f
Saia <78155936+SaiaAmako5@users.noreply.github.com>
2024-07-29 22:27:41 +0530

    Create removeCooldown.js
    Removes a cooldown. 
    
    Usage:
    $removeCooldown[command;type;...id?]
    
     Parameters:
    command | required | The name of the command from which you want to remove the cooldown from.
    type | required | The type of the cooldown. (Available types: globalUser, user, guild, and channel)
    ...id?* | optional (auto selects the author) | The id of the User/guild/channel from the cooldown.
    
    * Although the '...id?' is a rest parameter, but it is for the second ID for those type which supports it like 'user' so we can use it like this: `$removeCooldown[...;user;$authorID;$guildID]`

---
+++ src/functions/removeCooldown.js
@@ -0,0 +1,21 @@
+module.exports = async (d) => {
+    const data = d.util.aoiFunc(d);
+    if (data.err) return d.error(data.err);
+    const data = d.util.aoiFunc(d);
+    const [command, type, ...id] = data.inside.splits;
+    const types = {
+        globalUser: id[0] || d.message.author.id,
+        user: `${id[0] || d.message.author.id}_${id[1] || d.message.guild.id || "dm"}`,
+        guild: id[0] || d.message.guild.id,
+        channel: id[0] || d.message.channel.id
+    };
+    if (!types[type]) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Type In");
+    if (await d.client.db.get(type == "guild" ? d.client.db.tables[0] : "__aoijs_vars__", "cooldown", command + "_" + types[type])) {
+        await d.client.db.delete(type == "guild" ? d.client.db.tables[0] : "__aoijs_vars__", "cooldown", command + "_" + types[type]);
+    } else {
+        d.aoiError.fnError(d, "custom", {}, "No Such Cooldown Exists");
+    }
+    return {
+        code: d.util.setCode(data)
+    };
+};
