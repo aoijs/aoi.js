20a39c5f5de415e75c9eadce266b0615dcb1840a
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-08-05 14:38:08 +0200

    feat($usersBanned): add new options

--- src/functions/usersBanned.js
+++ src/functions/usersBanned.js
@@ -1,16 +1,30 @@
-module.exports = async d => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
 
-    const [guildID = d.guild?.id, force = 'false', option = 'id', sep = ' , '] = data.inside.splits;
+    const [guildID = d.guild?.id, force = "false", option = "id", sep = " , "] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildID);
-    if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
+    if (!guild) return d.aoiError.fnError(d, "guild", { inside: data.inside });
 
-    if (force === 'true') await guild.bans.fetch();
+    if (force === "true" || !guild.bans.cache) await guild.bans.fetch();
 
-    data.result = guild.bans.cache.map(x => option === 'mention' ? x.user.toString() : x.user[option]).join(sep);
+    const bannedUsers = guild.bans.cache;
+
+    if (option.includes("{")) {
+        const options = option.match(/{(.*?)}/g);
+        data.result = bannedUsers.map((x) => {
+            let result = option;
+            for (const opt of options) {
+                const n = opt.replace(/{|}/g, "");
+                if (x.user[n]) result = result.replace(opt, x.user[n].toString());
+            }
+            return result;
+        }).join(sep);
+    } else {
+        data.result = bannedUsers.map((x) => (option === "mention" ? x.user.toString() : x.user[option])).join(sep);
+    }
 
     return {
         code: d.util.setCode(data)
-    }
-}
+    };
+};
