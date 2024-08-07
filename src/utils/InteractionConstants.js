e1d4edc1d3a477c9ff586fd8ddcaa3a0d53c8a52
Faf4a <87046111+Faf4a@users.noreply.github.com>
2024-08-07 15:28:47 +0200

    remove: unused methods

--- src/utils/InteractionConstants.js
+++ src/utils/InteractionConstants.js
@@ -1,4 +1,5 @@
 const { ApplicationCommandType, InteractionType, ComponentType } = require("discord.js");
+
 const InteractionTypes = {
     [InteractionType.Ping]: "ping",
     [InteractionType.ApplicationCommand]: "slash",
@@ -17,11 +18,13 @@ const MessageComponentTypes = {
     [ComponentType.MentionableSelect]: "selectMenu",
     [ComponentType.TextInput]: "modal"
 };
+
 const SlashTypes = {
     slash: ApplicationCommandType.ChatInput,
     user: ApplicationCommandType.User,
     message: ApplicationCommandType.Message
 };
+
 module.exports = {
     InteractionTypes: InteractionTypes,
     MessageComponentTypes: MessageComponentTypes,
