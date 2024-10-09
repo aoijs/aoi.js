409bdc978c0c27921d812c5617b0e5f79fc32deb
Fafa <87046111+Faf4a@users.noreply.github.com>
2024-10-09 12:16:08 +0200

    fix: invalid check for get

--- src/functions/get.js
+++ src/functions/get.js
@@ -2,16 +2,18 @@
  * @param {import("..").Data} d
  */
 module.exports = async d => {
-    const data = d.util.aoiFunc(d);
-    if (data.err) return d.error(data.err);
-    
-    const [name] = data.inside.splits;
+  const data = d.util.aoiFunc(d);
+  if (data.err) return d.error(data.err);
+  
+  const [name] = data.inside.splits;
 
-    if (!d.data?.vars?.[name.addBrackets()]) return d.aoiError.fnError(d, "custom", { inside: d.data.inside }, "Invalid variable name");
+  if (!d.data?.vars?.hasOwnProperty(name.addBrackets())) {
+    return d.aoiError.fnError(d, "custom", { inside: d.data.inside }, "Invalid variable name");
+  }
 
-    data.result = d.data.vars[name?.addBrackets()];
+  data.result = d.data.vars[name.addBrackets()];
 
-    return {
-        code: d.util.setCode(data)
-    }
-}
+  return {
+    code: d.util.setCode(data)
+  };
+};
