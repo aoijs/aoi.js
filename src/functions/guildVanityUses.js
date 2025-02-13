5e7ea81910fa0b61f38b2cefc132702aea7efb88
Tyowk <tyowk.personal@gmail.com>
2025-02-13 09:45:38 +0700

    Update guildVanityUses.js

--- src/functions/guildVanityUses.js
+++ src/functions/guildVanityUses.js
@@ -3,12 +3,12 @@
  */
 module.exports = async d => {
     const data = d.util.aoiFunc(d);
-
-    const [guildID = d.guild?.id] = data.inside.splits;
+    const [guildID = d.guild?.id, fetchFirst = 'false'] = data.inside.splits;
 
     const guild = await d.util.getGuild(d, guildID);
     if (!guild) return d.aoiError.fnError(d, 'guild', {inside: data.inside});
 
+    if (fetchFirst == 'true') await guild.fetchVanityData().catch(Boolean);
     data.result = guild.vanityURLUses;
 
     return {
