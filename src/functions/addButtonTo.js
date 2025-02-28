72f8e9f2ec73249a3450a408541e4ea01a2d844c
Tyowk <tyowk.personal@gmail.com>
2025-02-28 11:21:10 +0700

    fix: $addButtonTo

--- src/functions/addButtonTo.js
+++ src/functions/addButtonTo.js
@@ -1,15 +1,25 @@
-const { ActionRow } = require("discord.js");
+const { ButtonBuilder, ActionRowBuilder } = require("discord.js");
 
 /**
  * @param {import("..").Data} d
  */
-module.exports = async (d) => {
+module.exports = async d => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
-    let [channelId = d.channel?.id, messageId = d.message?.id, index = "1", label, style, custom, disabled = "false", emoji] = data.inside.splits;
+    let [
+        channelId = d.channel?.id,
+        messageId = d.message?.id,
+        index = "1",
+        label,
+        style,
+        custom,
+        disabled = "false",
+        emoji
+    ] = data.inside.splits;
 
-    if (isNaN(index) || Number(index) < 1) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Index Provided In");
+    if (isNaN(index) || Number(index) < 1)
+        return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Index Provided In");
 
     const channel = d.client.channels.cache.get(channelId);
     if (!channel) return d.aoiError.fnError(d, "channel", { inside: data.inside });
@@ -21,36 +31,38 @@ module.exports = async (d) => {
     style = isNaN(style) ? d.util.constants.ButtonStyleOptions[style] : Number(style);
     disabled = disabled === "true";
 
-    if (!style || style > 6 || style < 1) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Button Style Provided In");
+    if (!style || style > 6 || style < 1)
+        return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Invalid Button Style Provided In");
 
-    emoji = await d.util.getEmoji(d, emoji.addBrackets()).id;
-    if (!emoji) emoji = emoji.addBrackets().trim();
+    if (emoji && emoji !== "") {
+        const customEmoji = await d.util.getEmoji(d, emoji.addBrackets())?.id;
+        emoji = customEmoji ? customEmoji : emoji.addBrackets().trim();
+    }
 
-    const button = {
+    const button = new ButtonBuilder({
         label,
-        type: 2,
         style,
         disabled,
-        emoji
-    };
-
-    button[style === 5 ? "url" : "customId"] = custom;
+        emoji,
+        type: 2,
+        [style === 5 ? "url" : "customId"]: custom
+    });
 
     const components = d.data.components ? [...d.data.components] : [...message.components];
-
     components[index] = components[index] || { type: 1, components: [] };
+
     components[index].components.push(button);
+    const row = components.map(row => ActionRowBuilder.from(row)).filter(Boolean);
 
-    const raw = components.map((component) => (component instanceof ActionRow ? component.toJSON() : component));
-
-    await message.edit({ components: raw });
+    try {
+        await message.edit({ components: row });
+    } catch (err) {
+        components[index].components.pop();
+        return d.aoiError.fnError(d, "custom", {}, `${err.name}: ${err.message}`);
+    }
 
     return {
         code: d.util.setCode(data),
-        data: {
-            components: {
-                ...raw
-            }
-        }
+        data: { components: row }
     };
 };
