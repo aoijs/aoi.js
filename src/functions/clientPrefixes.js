fa3055ac962feadd19f2677529b10a516a0bae32
Ivaylo <imsamlolll@gmail.com>
2025-03-23 18:34:49 +0300

    forgot to remove

--- src/functions/clientPrefixes.js
+++ src/functions/clientPrefixes.js
@@ -5,7 +5,7 @@ module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
     
-    let [separator = ',', includeSpaces = true] = data.inside.splits;
+    let [separator = ','] = data.inside.splits;
     
     if (Array.isArray(d.client.prefix)) {
         data.result = d.client.prefix.join(" " + separator + " ");
