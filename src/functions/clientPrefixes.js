15333b06739511faf7f15c994d54d15de8bcc2c8
Ivaylo <imsamlolll@gmail.com>
2025-03-23 16:55:58 +0300

    Update clientPrefixes.js

--- src/functions/clientPrefixes.js
+++ src/functions/clientPrefixes.js
@@ -3,9 +3,12 @@
  */
 module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
-
+    if (data.err) return d.error(data.err);
+    
+    let [separator = ','] = data.inside.splits;
+    
     if (Array.isArray(d.client.prefix)) {
-        data.result = d.client.prefix.join(" , ");
+        data.result = d.client.prefix.join(" " + separator + " ");
     } else {
         data.result = d.client.prefix;
     }
