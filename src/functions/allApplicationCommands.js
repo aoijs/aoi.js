d8b67a5133426ac6523276dfe087531376f4cffe
Ivaylo <imsamlolll@gmail.com>
2025-04-02 23:03:24 +0300

    Update allApplicationCommands.js

--- src/functions/allApplicationCommands.js
+++ src/functions/allApplicationCommands.js
@@ -3,17 +3,15 @@
  */  
 module.exports = async (d) => {  
     const data = d.util.aoiFunc(d);  
-  
+
     if (data.err) return d.error(data.err);  
-  
+
     let [separator = ','] = data.inside.splits;  
-    try {  
-        const slashCommands = await d.client.application.commands.fetch();  
-        const formattedSlashCommands = slashCommands.map(command => command.name).join(separator)  
-        data.result = formattedSlashCommands;
-   } catch (e) {  
-         return d.aoiError.fnError(d, 'custom', {}, `Failed to fetch all slash commands with the reason: ${e}`);  
-    }  
+    const slashCommands = await d.client.application.commands.fetch().catch((e) => {
+         d.aoiError.fnError(d, 'custom', {}, 'Failed To Fetch Slash Commands With The Reason: ' + e);  
+         });  
+    data.result = slashCommands.map(command => command.name).join(separator);
+
     return {  
         code: d.util.setCode(data),  
     };  
