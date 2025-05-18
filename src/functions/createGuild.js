47610afb876f590d349429046b3ffc085edc6990
Faf4a <87046111+Faf4a@users.noreply.github.com>
2025-05-18 14:02:05 +0200

    deprecate: createGuild

--- src/functions/createGuild.js
+++ src/functions/createGuild.js
@@ -1,23 +1,27 @@
+const util = require("util");
+
 /**
  * @param {import("..").Data} d
  */
-module.exports = async (d) => {
+const createGuild = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
-    const [ name, icon, returnId = "false" ] = data.inside.splits;
+    const [name, icon, returnId = "false"] = data.inside.splits;
 
-    if (!name) return d.aoiError.fnError(d, "custom", { inside: data.inside } ,"Missing Guild Name")
-    
+    if (!name) return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Missing Guild Name");
+
     try {
         const guild = await d.client.guilds.create({ name: name.addBrackets(), icon: icon.addBrackets() });
 
         data.result = returnId === "true" ? guild.id : null;
     } catch (err) {
-        return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Failed to create Guild with Reason: " + err)
+        return d.aoiError.fnError(d, "custom", { inside: data.inside }, "Failed to create Guild with Reason: " + err);
     }
 
     return {
         code: d.util.setCode(data)
-    }
-}
+    };
+};
+
+module.exports = util.deprecate(createGuild, "createGuild is deprecated and will be removed by Discord in the future.");
