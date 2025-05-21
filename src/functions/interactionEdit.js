eb4f38ea8ab0503546b2c5a4370a6560e15aea60
Faf4a <87046111+Faf4a@users.noreply.github.com>
2025-05-21 17:56:40 +0200

    set content to null for edits

--- src/functions/interactionEdit.js
+++ src/functions/interactionEdit.js
@@ -1,7 +1,9 @@
+const { MessageFlags } = require("discord.js");
+
 /**
  * @param {import("..").Data} d
  */
-module.exports = async d => {
+module.exports = async (d) => {
     const data = d.util.aoiFunc(d);
     if (data.err) return d.error(data.err);
 
@@ -9,20 +11,22 @@ module.exports = async d => {
 
     const parser = await d.util.errorParser(content, d);
 
-    await d.data.interaction?.editReply({
-        content: parser.content?.trim() === "" ? " " : parser.content?.addBrackets(),
-        embeds: parser.embeds,
-        components: parser.components,
-        files: parser.files,
-        flags: parser.flags ?? 0,
-        allowedMentions: {
-            parse: allowedMentions === "all" ? [ "everyone", "users", "roles" ] : (allowedMentions ? allowedMentions?.split(",") : [])
-        }
-    }).catch(e => {
-        d.aoiError.fnError(d, "custom", {}, "Failed to Edit Interaction with Reason: " + e)
-    });
+    await d.data.interaction
+        ?.editReply({
+            content: parser.flags & MessageFlags.IsComponentsV2 ? null : parser.content,
+            embeds: parser.embeds,
+            components: parser.components,
+            files: parser.files,
+            flags: parser.flags ?? 0,
+            allowedMentions: {
+                parse: allowedMentions === "all" ? ["everyone", "users", "roles"] : allowedMentions ? allowedMentions?.split(",") : []
+            }
+        })
+        .catch((e) => {
+            d.aoiError.fnError(d, "custom", {}, "Failed to Edit Interaction with Reason: " + e);
+        });
 
     return {
         code: d.util.setCode(data)
-    }
-}
+    };
+};
