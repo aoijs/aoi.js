0a8fe294841868ad7e4e2f0e13b1440bb2e9c4c5
Faf4a <87046111+Faf4a@users.noreply.github.com>
2025-05-21 13:06:07 +0200

    fix: flags not passed to interaction

--- src/functions/interactionEdit.js
+++ src/functions/interactionEdit.js
@@ -14,6 +14,7 @@ module.exports = async d => {
         embeds: parser.embeds ?? parser.data?.embeds,
         components: parser.components ?? parser.data?.components,
         files: parser.files ?? parser.data?.files,
+        flags: parser.flags ?? 0,
         allowedMentions: {
             parse: allowedMentions === "all" ? [ "everyone", "users", "roles" ] : (allowedMentions ? allowedMentions?.split(",") : [])
         }
