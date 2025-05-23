50cae399b9c21adc4ccae8f34c6ac30bc4465df7
Vishvaas <89300922+Vishvaas06@users.noreply.github.com>
2025-05-23 17:33:54 +0530

    Makes Interaction deferable before $onlyif

--- src/classes/AoiError.js
+++ src/classes/AoiError.js
@@ -88,13 +88,14 @@ class AoiError {
         let msg;
         if (extraOptions.interaction) {
             if (options.content === "" && options.embeds?.length === 0 && options.files?.length === 0 && options.components?.length === 0) return;
-            if (extraOptions?.defer) {
+            if (extraOptions?.defer && !d.data.interaction?.deferred) {
                 await d.data.interaction.deferReply({
                     flags: extraOptions.ephemeral == true
                         ? (options.flags ? (MessageFlags.Ephemeral | options.flags) : MessageFlags.Ephemeral)
                         : options.flags
                 });
-            
+            }
+            if (d.data.interaction?.deferred) {
                 msg = await d.data.interaction.followUp({
                     ...options,
                     flags: extraOptions.ephemeral == true
