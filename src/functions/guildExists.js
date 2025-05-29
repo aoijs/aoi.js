902d9fde429f789c8757654d5b3b50120cf47657
NanotechPikachu <94281228+NanotechPikachu@users.noreply.github.com>
2025-05-29 19:01:14 +0530

    Fix guildExists (#722)

--- src/functions/guildExists.js
+++ src/functions/guildExists.js
@@ -7,7 +7,11 @@ module.exports = async d => {
 
     const guildID = data.inside.inside;
 
-    data.result = !!(await d.util.getGuild(d, guildID));
+    const guild = !!(await d.util.getGuild(d, guildID).catch(() => {
+        return false;
+    }));  
+    
+    data.result = guild;
 
     return {
         code: d.util.setCode(data)
