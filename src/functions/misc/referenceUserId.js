b3ecc70c01d028febfb903a7c36c7992fc3e42dd
YordiGamex <44493931+YordiGamex@users.noreply.github.com>
2025-06-13 13:51:47 -0500

    Update referenceUserId.js
    This way it's much better to use it, since you can get the ID of the user they're replying to no matter what the message is. Its usage is:
    $referenceUserId[$channelID?;$messageID]

--- src/functions/misc/referenceUserId.js
+++ src/functions/misc/referenceUserId.js
@@ -1,18 +1,36 @@
 /**
  * @param {import("..").Data} d
  */
-module.exports = async (d) => {
-   const data = d.util.aoiFunc(d);
-   const [messageID = d.message.id] = data.inside.splits;
+module.exports = async d => {
+    const data = d.util.aoiFunc(d);
+    let [ channelID = d.channel.id, messageID = d.message.id ] = data.inside.splits;
 
-   const message = messageID ? await d.channel.messages.fetch(messageID) : d.message;
-   if (!message) return d.aoiError.fnError(d, 'message', { inside: data.inside });
+    // Obtener el canal
+    const channel = await d.util.getChannel(d, channelID);
+    if (!channel) {
+        data.result = "Canal no encontrado";
+        return { code: d.util.setCode(data) };
+    }
 
-   const id = message.reference?.messageId;
+    const message = await d.util.fetchMessage(channel, messageID);
+    if (!message) {
+        data.result = "Message not found";
+        return { code: d.util.setCode(data) };
+    }
 
-   data.result = id ? (await d.channel.messages.fetch(id))?.author.id : '';
+    const referencedMessageID = message.reference?.messageId;
+    if (!referencedMessageID) {
+        data.result = "No hay referencia";
+        return { code: d.util.setCode(data) };
+    }
 
-   return {
-     code: d.util.setCode(data),
-   }
- }
+    const referencedMessage = await d.util.fetchMessage(channel, referencedMessageID);
+    if (!referencedMessage) {
+        data.result = "Referenced message not found";
+        return { code: d.util.setCode(data) };
+    }
+
+    data.result = referencedMessage.author.id;
+
+    return { code: d.util.setCode(data) };
+};
